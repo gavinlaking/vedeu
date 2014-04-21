2cc080350f5673f6c01557e0967c1b219f7e2152
Gavin Laking <gavinlaking@gmail.com>
2014-04-21 12:29:39 +0100

    Rename 'buffer' to 'composition'.

--- lib/vedeu/renderer.rb
+++ lib/vedeu/renderer.rb
@@ -1,25 +1,37 @@
 module Vedeu
   class Renderer
+    class << self
+      # @param  composition [Array]
+      # @return             [String]
+      def render(composition = [])
+        new(composition).render
+      end
+    end
+
     include Enumerable
 
-    # @param  buffer [Array]
+    # @param  composition [Array]
     # @return        [Vedeu::Renderer]
-    def initialize(buffer = [])
-      @buffer = buffer
+    def initialize(composition = [])
+      @composition = composition
     end
 
     # @return [Enumerator]
     def each(&block)
-      buffer.each(&block)
+      composition.each(&block)
     end
 
     # @return [String]
     def render
-      buffer.join
+      composition.join
     end
 
     private
 
-    attr_reader :buffer
+    attr_reader :composition
+
+    def eol
+      "\n"
+    end
   end
 end
--- test/lib/vedeu/renderer_test.rb
+++ test/lib/vedeu/renderer_test.rb
@@ -3,8 +3,8 @@ require_relative '../../test_helper'
 module Vedeu
   describe Renderer do
     let(:klass)    { Renderer }
-    let(:instance) { klass.new(buffer) }
-    let(:buffer)   {
+    let(:instance) { klass.new(composition) }
+    let(:composition)   {
       [
         ["\e[30;47ma\e[0m", "\e[31;49mb\e[0m", "\e[32;49mc\e[0m"],
         ["\e[33;49md\e[0m", "\e[34;49me\e[0m", "\e[35;49mf\e[0m"],
@@ -25,7 +25,7 @@ module Vedeu
 
       it { subject.must_be_instance_of(String) }
 
-      it 'renders the buffer' do
+      it 'renders the composition' do
         subject
           .must_equal("\e[30;47ma\e[0m\e[31;49mb\e[0m\e[32;49mc\e[0m\e[33;49md\e[0m\e[34;49me\e[0m\e[35;49mf\e[0m\e[36;49mg\e[0m\e[37;49mh\e[0m\e[39;47mi\e[0m")
       end
