7800d8b34143c2105ef51a38a483e555d2c1abaf
Gavin Laking <gavin.laking@onthebeach.co.uk>
2014-05-10 17:29:34 +0100

    Flesh out basic for output.

--- lib/vedeu/compositor.rb
+++ lib/vedeu/compositor.rb
@@ -1,13 +1,35 @@
 module Vedeu
   class Compositor
-    def initialize
+    def self.write(output = [], options = {})
+      new(output, options).write
+    end
+
+    def initialize(output = [], options = {})
+      @output, @options = output, options
+    end
+
+    def write
+      output.map do |line|
+        print line
+      end
+      nil
     end
 
     private
 
+    attr_reader :output
+
     def empty_line(line, width = Terminal.width)
       Esc.set_position(line, 0)
       print " " * width
+    end
+
+    def options
+      defaults.merge!(@options)
+    end
+
+    def defaults
+      {}
     end
   end
 end
