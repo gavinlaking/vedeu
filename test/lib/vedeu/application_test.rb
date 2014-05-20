b8008e512d1db660f5a9cecdfce73469c89cf7f0
Gavin Laking <gavin.laking@onthebeach.co.uk>
2014-05-20 20:43:11 +0100

    Remove commented out tests.

--- test/lib/vedeu/application_test.rb
+++ test/lib/vedeu/application_test.rb
@@ -13,22 +13,6 @@ module Vedeu
       subject { instance.start }
 
       it { skip }
-
-      context 'when the user sets the clear option' do
-        # subject { described_class.clear_screen }
-
-        # it { subject.must_be_instance_of(NilClass) }
-
-        # context 'capturing output' do
-        #   let(:io) { capture_io { subject }.join }
-
-        #   it { io.must_be_instance_of(String) }
-        # end
-      end
-
-      context 'when the user unsets the clear option' do
-
-      end
     end
   end
 end
