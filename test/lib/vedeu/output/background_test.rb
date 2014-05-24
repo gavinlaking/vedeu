320eb7f642cfdbd77c3282b2092bdba134265c69
Gavin Laking <gavin.laking@onthebeach.co.uk>
2014-05-24 11:05:53 +0100

    Add more tests.

--- test/lib/vedeu/output/background_test.rb
+++ test/lib/vedeu/output/background_test.rb
@@ -2,9 +2,46 @@ require_relative '../../../test_helper'
 
 module Vedeu
   describe Background do
-    let(:described_class) { Background }
-    let(:instance) { described_class.new }
+    let(:described_class)    { Background }
+    let(:described_instance) { described_class.new(colour) }
+    let(:colour)             {}
 
-    it { instance.must_be_instance_of(Background) }
+    it { described_instance.must_be_instance_of(Background) }
+
+    describe '#escape_sequence' do
+      subject { described_instance.escape_sequence }
+
+      it { subject.must_be_instance_of(String) }
+
+      context 'with no colour' do
+        it { subject.must_be_instance_of(String) }
+
+        it { subject.must_equal("\e[48;5;49m") }
+      end
+
+      context 'with a named colour' do
+        let(:colour) { :red }
+
+        it { subject.must_be_instance_of(String) }
+
+        it { subject.must_equal("\e[48;5;41m") }
+      end
+
+      context 'with a html colour' do
+        let(:colour) { '#aadd00' }
+
+        it { subject.must_be_instance_of(String) }
+
+        it { subject.must_equal("\e[48;5;148m") }
+      end
+
+      context 'with a default colour' do
+        let(:colour) { :undefined }
+
+        it { subject.must_be_instance_of(String) }
+
+        it { subject.must_equal("\e[48;5;49m") }
+      end
+    end
   end
 end
--- test/lib/vedeu/output/base_test.rb
+++ test/lib/vedeu/output/base_test.rb
@@ -2,9 +2,7 @@ require_relative '../../../test_helper'
 
 module Vedeu
   describe Base do
-    let(:described_class) { Base }
-    let(:instance) { described_class.new }
-
-    it { instance.must_be_instance_of(Base) }
+    let(:described_class)    { Base }
+    let(:described_instance) { described_class.new }
   end
 end
