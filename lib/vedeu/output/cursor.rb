77c922277b710e689e411ef7434d00e7c71b0a5f
Gavin Laking <gavin.laking@onthebeach.co.uk>
2014-06-16 20:59:08 +0100

    Reduce duplication.

--- lib/vedeu/output/cursor.rb
+++ lib/vedeu/output/cursor.rb
@@ -2,56 +2,52 @@ module Vedeu
   class Cursor
     class << self
       def show
-        [esc, '?25h'].join
+        [Esc.esc, '?25h'].join
       end
 
       def hide
-        [esc, '?25l'].join
+        [Esc.esc, '?25l'].join
       end
 
       def home
-        [esc, 'H'].join
+        [Esc.esc, 'H'].join
       end
 
       def up(count = 1)
-        [esc, "#{count || 1}", 'A'].join
+        [Esc.esc, "#{count || 1}", 'A'].join
       end
 
       def down(count = 1)
-        [esc, "#{count || 1}", 'B'].join
+        [Esc.esc, "#{count || 1}", 'B'].join
       end
 
       def right(count = 1)
-        [esc, "#{count || 1}", 'C'].join
+        [Esc.esc, "#{count || 1}", 'C'].join
       end
       alias_method :forward, :right
 
       def left(count = 1)
-        [esc, "#{count || 1}", 'D'].join
+        [Esc.esc, "#{count || 1}", 'D'].join
       end
       alias_method :backward, :left
 
       def save
-        [esc, 's'].join
+        [Esc.esc, 's'].join
       end
 
       def unsave
-        [esc, 'u'].join
+        [Esc.esc, 'u'].join
       end
       alias_method :restore, :unsave
 
       def save_all
-        [esc, '7'].join
+        [Esc.esc, '7'].join
       end
 
       def unsave_all
-        [esc, '8'].join
+        [Esc.esc, '8'].join
       end
       alias_method :restore_all, :unsave_all
-
-      def esc
-        [27.chr, '['].join
-      end
     end
   end
 end
