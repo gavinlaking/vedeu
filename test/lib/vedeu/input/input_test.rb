16852a0224e9c5d262231d44acf5a658f616578e
Gavin Laking <gavin.laking@onthebeach.co.uk>
2014-06-21 22:44:13 +0100

    Convert Input;
    Input only contains behaviour, and should not be instantiated.

--- test/lib/vedeu/input/input_test.rb
+++ test/lib/vedeu/input/input_test.rb
@@ -2,21 +2,16 @@ require_relative '../../../test_helper'
 
 module Vedeu
   describe Input do
-    let(:described_class) { Input }
-    let(:input)           { 'input' }
-    let(:subject)         { described_class.new }
+    let(:described_module) { Input }
+    let(:input)            { 'input' }
 
     before do
       Terminal.stubs(:input).returns(input)
       Queue.stubs(:enqueue).returns([input])
     end
 
-    it 'return an Input instance' do
-      subject.must_be_instance_of(Input)
-    end
-
     describe '.capture' do
-      let(:subject) { described_class.capture }
+      let(:subject) { described_module.capture }
 
       it 'returns an Array' do
         subject.must_be_instance_of(Array)
