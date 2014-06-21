19d1b9aa4fb0b9011936cd1c72fc51284a036b34
Gavin Laking <gavin.laking@onthebeach.co.uk>
2014-06-21 19:43:59 +0100

    Array#map produces [[[[]]]], should be [[]] (use #each instead).

--- lib/vedeu/output/output.rb
+++ lib/vedeu/output/output.rb
@@ -9,8 +9,8 @@ module Vedeu
     def initialize; end
 
     def render
-      InterfaceRepository.refresh.map do |interface|
-        interface.map { |stream| Terminal.output(stream) if stream }
+      InterfaceRepository.refresh.each do |interface|
+        interface.each { |stream| Terminal.output(stream) if stream }
       end
     end
   end
