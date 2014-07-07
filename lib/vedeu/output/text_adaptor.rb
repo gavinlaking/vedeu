5f5b6e821f42470b2791d021d6c029363855b0ed
Gavin Laking <gavin.laking@onthebeach.co.uk>
2014-07-07 22:23:04 +0100

    Add TextAdaptor;
    Converts a block of text containing newlines into Line objects to be used in Compositions.

---
+++ lib/vedeu/output/text_adaptor.rb
@@ -0,0 +1,31 @@
+require_relative '../models/line'
+
+module Vedeu
+  class TextAdaptor
+    def self.adapt(text)
+      new(text).adapt
+    end
+
+    def initialize(text)
+      @text = text
+    end
+
+    def adapt
+      return [] if no_content?
+
+      lines.map { |line| Line.new({ streams: { text: line } }) }
+    end
+
+    private
+
+    attr_reader :text
+
+    def lines
+      text.split(/\n/)
+    end
+
+    def no_content?
+      text.nil? || text.empty?
+    end
+  end
+end
