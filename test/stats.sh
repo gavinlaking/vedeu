3f11a5e0a3f4dff134964d7dcf40526def9018c2
Gavin Laking <gavinlaking@gmail.com>
2014-07-22 11:24:32 +0100

    Add app/test stats script (DAS Ep.01).

---
+++ test/stats.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+function main {
+  for rev in `revisions`; do
+    echo "`number_of_lines ^lib` `number_of_lines ^test` `commit_description`"
+  done
+}
+
+function revisions {
+  git rev-list --reverse HEAD
+}
+
+function commit_description {
+  git log --oneline -1 $rev
+}
+
+function number_of_lines () {
+  git ls-tree -r $rev |
+  awk '{print $4, $3}' |
+  grep "$1" |
+  grep "\.rb" |
+  awk '{print $2}' |
+  xargs git show |
+  wc -l
+}
+
+main
