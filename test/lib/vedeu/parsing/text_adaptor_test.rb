cfd5969c554d27fc3b7ea7fb98244675f4249c08
Gavin Laking <gavinlaking@gmail.com>
2014-07-22 12:52:15 +0100

    Add more tests.

--- test/lib/vedeu/parsing/text_adaptor_test.rb
+++ test/lib/vedeu/parsing/text_adaptor_test.rb
@@ -6,17 +6,43 @@ module Vedeu
     describe '.adapt' do
       it 'returns an empty collection for an empty string' do
         TextAdaptor.adapt('').must_be_empty
+
+        TextAdaptor.adapt('').must_equal([])
       end
 
       it 'returns a single line' do
         text = "This is a single line of text.\n"
         TextAdaptor.adapt(text).size.must_equal(1)
+
+        TextAdaptor.adapt(text).must_equal(
+          [
+            {
+              streams: { text: 'This is a single line of text.' }
+            }
+          ]
+        )
       end
 
       it 'returns multiple lines' do
         text = "Lorem ipm olor sit aet,\nConsctetur adipiscing.\n" \
                "Curitur aiquet, trpis id dui.\n\nCondum elemum.\n"
         TextAdaptor.adapt(text).size.must_equal(5)
+
+        TextAdaptor.adapt(text).must_equal(
+          [
+            {
+              streams: { text: 'Lorem ipm olor sit aet,' }
+            }, {
+              streams: { text: 'Consctetur adipiscing.' }
+            }, {
+              streams: { text: 'Curitur aiquet, trpis id dui.' }
+            }, {
+              streams: { text: '' }
+            }, {
+              streams: { text: 'Condum elemum.' }
+            }
+          ]
+        )
       end
     end
   end
