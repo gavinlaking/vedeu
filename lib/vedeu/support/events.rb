c58db2bfffac08de6c9533e61014974a74af44b0
Gavin Laking <gavin.laking@onthebeach.co.uk>
2014-07-31 11:47:18 +0100

    Remove event specific logging;
    No longer required, plus it was messing the class up.

--- lib/vedeu/support/events.rb
+++ lib/vedeu/support/events.rb
@@ -2,37 +2,22 @@ module Vedeu
   class Events
     def initialize(&block)
       @handlers = Hash.new { |h, k| h[k] = [] }
-
-      log("self: #{self.object_id}")
-
       self.instance_eval(&block) if block_given?
-
       self
     end
 
     def add(object, &block)
-      log("self: #{self.object_id}")
-      log("object: #{object.object_id}")
-
       @self_before_instance_eval = eval 'self', block.binding
 
       self.instance_eval(&block)
     end
 
     def on(event, &block)
-      log("self: #{self.object_id}")
-      log("block: #{block.object_id}")
-      log("storing: #{event.inspect}")
-
       handlers[event] << block
     end
 
     def trigger(event, *args)
       handlers[event].each do |handler|
-        log("self: #{self.object_id}")
-        log("handler: #{handler.object_id}")
-        log("running: #{event.inspect}")
-
         handler.call(*args)
       end
     end
@@ -46,9 +31,5 @@ module Vedeu
     private
 
     attr_reader :handlers
-
-    def log(message)
-      Vedeu.log(sprintf("%42s%s", '', message)) if Vedeu.debug?
-    end
   end
 end
