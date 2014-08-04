69d394fbe01b109547961a1e063f72ad2783f6b8
Gavin Laking <gavinlaking@gmail.com>
2014-08-04 20:51:12 +0100

    Add #width and #align attributes to Stream;
    This provides clients the ability to set a width for a stream, padded with spaces, and align content within left, centre or right.

--- test/lib/vedeu/models/stream_test.rb
+++ test/lib/vedeu/models/stream_test.rb
@@ -10,9 +10,13 @@ module Vedeu
           background: '#000000'
         },
         text:  'Some text',
-        style: 'normal'
+        style: 'normal',
+        width: width,
+        align: align
       })
     }
+    let(:align) { :left }
+    let(:width) { 9 }
 
     it 'has a colour attribute' do
       stream.colour.must_be_instance_of(Colour)
@@ -26,13 +30,59 @@ module Vedeu
       stream.style.must_equal("\e[24m\e[21m\e[27m")
     end
 
+    it 'has a width attribute' do
+      stream.width.must_equal(9)
+    end
+
+    it 'has an align attribute' do
+      stream.align.must_equal(:left)
+    end
+
     describe '#to_s' do
-      it 'returns a String' do
-        stream.to_s.must_equal(
-          "\e[38;5;196m\e[48;5;16m" \
-          "\e[24m\e[21m\e[27m" \
-          "Some text"
-        )
+      describe 'when a width is set and align is default' do
+        let(:width) { 20 }
+
+        it 'returns a String' do
+          stream.to_s.must_equal(
+            "\e[38;5;196m\e[48;5;16m" \
+            "\e[24m\e[21m\e[27m"      \
+            "Some text           "
+          )
+        end
+
+        describe 'and align is :centre' do
+          let(:align) { :centre }
+
+          it 'returns a String' do
+            stream.to_s.must_equal(
+              "\e[38;5;196m\e[48;5;16m" \
+              "\e[24m\e[21m\e[27m"      \
+              "     Some text      "
+            )
+          end
+        end
+
+        describe 'and align is :right' do
+          let(:align) { :right }
+
+          it 'returns a String' do
+            stream.to_s.must_equal(
+              "\e[38;5;196m\e[48;5;16m" \
+              "\e[24m\e[21m\e[27m"      \
+              "           Some text"
+            )
+          end
+        end
+      end
+
+      describe 'when a width is not set' do
+        it 'returns a String' do
+          stream.to_s.must_equal(
+            "\e[38;5;196m\e[48;5;16m" \
+            "\e[24m\e[21m\e[27m"      \
+            "Some text"
+          )
+        end
       end
     end
   end
