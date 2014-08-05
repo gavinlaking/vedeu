8472c198166e823f6923a8b2a407d40c844e40ac
Gavin Laking <gavinlaking@gmail.com>
2014-08-06 00:18:02 +0100

    Continued developing API for views.

--- lib/vedeu/api/view.rb
+++ lib/vedeu/api/view.rb
@@ -11,25 +11,35 @@ module Vedeu
 
       def build(&block)
         self.instance_eval(&block) if block_given?
+
+        attributes.delete_if { |k, v| v.nil? || v.empty? }
       end
 
-      private
+      #private
 
       def colour(*args)
+        attributes[:colour] = {}
       end
 
-      def foreground(*args)
+      def foreground(value = '')
+        attributes[:colour][:foreground] = value
       end
 
-      def background(*args)
+      def background(value = '')
+        attributes[:colour][:background] = value
       end
 
-      def style(*args)
+      def style(values = [])
+        [values].flatten.each { |value| attributes[:style] << value }
+      end
+
+      def attributes
+        @attributes ||= { lines: [], colour: {}, style: [] }
       end
     end
 
     class Line < Interface
-      private
+      # private
 
       def width(value)
         # Stream...
@@ -40,236 +50,52 @@ module Vedeu
       end
 
       def stream(&block)
+        API::Stream.build(&block)
       end
 
       def text(value)
+        attributes[:lines] << { streams: [{ text: value }] }
       end
     end
 
     class Stream < Line
+      # private
+
+      def text(value)
+        { text: value }
+      end
     end
 
     class View
-      def self.build(implicit_name = '', &block)
-        new(implicit_name).build(&block)
+      def self.build(name = '', &block)
+        new(name).build(&block)
       end
 
-      def initialize(implicit_name = '')
-        @implicit_name = implicit_name.to_s
+      def initialize(name = '')
+        fail Vedeu::API::InterfaceNotSpecified if name.nil? || name.empty?
+
+        @name = name.to_s
       end
 
       def build(&block)
         self.instance_eval(&block) if block_given?
 
-        # fail Vedeu::API::InterfaceNotSpecified if name.nil? || name.empty?
         attributes
       end
 
       private
 
-      attr_reader :implicit_name
-
       def line(&block)
-        API::Line.build(&block)
-      end
-
-      def stream(&block)
-        API::Stream.build(&block)
-      end
-
-      def interface(explicit_name = '')
-        name = if (explicit_name.nil? || explicit_name.empty?)
-          if (implicit_name.nil? || implicit_name.empty?)
-            fail Vedeu::API::InterfaceNotSpecified
-          else
-            implicit_name
-          end
-        else
-          explicit_name
-        end
-
-        if InterfaceStore.query(name)
-          attributes[:name] = name
-        end
+        attributes.merge!(API::Line.build(&block))
       end
 
       def attributes
-        @_attributes ||= {}
+        @_attributes ||= { name: name }
       end
 
-      def valid_interface?
-        return true if InterfaceStore.query(name)
+      def name
+        return @name if InterfaceStore.query(@name)
       end
-
-      # def method_missing(method, *args, &block)
-      #   # @self_before_instance_eval.send method, *args, &block
-      # end
     end
   end
 end
-
-require 'vedeu'
-
-class SomeViewClass
-  include Vedeu
-
-  interface 'testing_view' do
-    width  80
-    height 25
-    x      1
-    y      1
-    colour  foreground: '#ffffff', background: '#000000'
-    centred true
-  end
-
-  view 'testing_view' do # argument is target interface
-                         # (interface must be defined)
-    line do
-      text '1. Implicit target interface.'
-    end
-  end
-
-  view do # lack of argument means interface attribute must be provided
-    interface 'testing_view'
-    line do
-      text '1. Explicit target interface.'
-    end
-  end
-
-  view 'testing_view' do
-    line do
-      text '1. A line of text.'
-      text '2. Another line of text.'
-    end
-
-    line do
-      text "3. These lines will split\ninternally into multiple lines."
-    end
-  end
-
-  view 'testing_view' do
-    line do
-      text '1. Line without colours.'
-    end
-
-    line do
-      colour '#ff0000', '#ffff00'
-      text   '2. Line with colours.'
-    end
-
-    line do
-      colour foreground: '#a7ff00', background: '#005700'
-      text   '3. Line with explicit colour declaration.'
-    end
-
-    line do # actually uses streams
-      foreground '#ff00ff' do
-        text '4. Line with only foreground set.'
-      end
-    end
-
-    line do # actually uses streams
-      background '#00ff00' do
-        text '5. Line with only background set.'
-      end
-    end
-  end
-
-  view 'testing_view' do
-    line do
-      style 'normal'
-      text  '1. Line with a normal style.'
-    end
-
-    line do
-      style 'underline'
-      text  '2. Line with an underline style.'
-    end
-
-    line do # actually uses streams
-      style 'normal' do
-        text '3. Line with a normal style.'
-      end
-    end
-
-    line do # actually uses streams
-      style 'underline' do
-        text '4. Line with an underlined style.'
-      end
-    end
-  end
-
-  view 'testing_view' do
-    line do
-      stream do
-        text 'A stream of text.'
-      end
-    end
-
-    line do
-      stream do # Stream is an 'explicit declaration'.
-                # We don't need it unless we want to add colours and styles.
-                # See below.
-        text '1. Two streams of text, these will be'
-      end
-
-      stream do
-        text ' on the same line- note the space.'
-      end
-    end
-
-    line do
-      stream do
-        text '2. Streams can have'
-      end
-
-      stream do
-        colour foreground: '#ff0000', background: '#00ff00'
-        text   ' colours too.'
-      end
-    end
-
-    line do
-      stream do
-        text '3. Streams can have'
-      end
-
-      stream do
-        style 'underline'
-        text  ' styles too.'
-      end
-    end
-  end
-
-  view 'testing_view' do
-    line do
-      width 80
-      text  'This is be aligned left, and padded with spaces.'
-    end
-  end
-
-  view 'testing_view' do
-    line do
-      width 80
-      align :left # explicit
-      text  'This is be aligned left, and padded with spaces.'
-    end
-  end
-
-  view 'testing_view' do
-    line do
-      width 80
-      align :centre
-      text  'This is be aligned centrally, and padded with spaces.'
-    end
-  end
-
-  view 'testing_view' do
-    line do
-      width 80
-      align :right
-      text  'This is be aligned right, and padded with spaces.'
-    end
-  end
-end
-
--- test/lib/vedeu/api/view_test.rb
+++ test/lib/vedeu/api/view_test.rb
@@ -8,23 +8,284 @@ module Vedeu
     describe View do
       before do
         InterfaceStore.reset
-        @api_view = InterfaceTemplate.save('api_view') do
-          width  10
-          height 10
+        InterfaceTemplate.save('testing_view') do
+          width  80
+          height 25
           x      1
           y      1
-          colour  foreground: '#ffff00', background: '#5a5a5a'
-          centred true
+          colour  foreground: '#ffffff', background: '#000000'
+          centred false
         end
       end
 
-      # it 'raises an exception if the interface is not defined' do
-      #   proc { View.build('dummy') { } }.must_raise(Vedeu::API::InterfaceNotSpecified)
-      # end
+      it 'builds a basic view' do
+        Vedeu.view 'testing_view' do
+          line do
+            text '1. A line of text.'
+            text '2. Another line of text.'
+          end
 
-      # it 'returns the interface if defined' do
-      #   View.build('api_view') {}.must_be_instance_of(Interface)
-      # end
+          # line do
+          #   text "3. These lines will split\ninternally into multiple lines."
+          # end
+        end.must_equal(
+          {
+            name:  'testing_view',
+            lines: [
+              { streams: [{ text: '1. A line of text.' }] },
+              { streams: [{ text: '2. Another line of text.' }] }
+            ]
+          }
+        )
+      end
+
+      it 'handles coloured lines' do
+        Vedeu.view 'testing_view' do
+          line do
+            text '1. Line without colours.'
+          end
+
+          line do
+            colour '#ff0000', '#ffff00'
+            text   '2. Line with colours.'
+          end
+
+          line do
+            colour foreground: '#a7ff00', background: '#005700'
+            text   '3. Line with explicit colour declaration.'
+          end
+
+          line do # actually uses streams
+            foreground '#ff00ff' do
+              text '4. Line with only foreground set.'
+            end
+          end
+
+          line do # actually uses streams
+            background '#00ff00' do
+              text '5. Line with only background set.'
+            end
+          end
+        end.must_equal(
+          {
+            name:  'testing_view',
+            lines: [
+              {
+                streams: [{ text: '1. Line without colours.' }]
+              }, {
+                colour:  { background: '#ff0000', foreground: '#ffff00' },
+                streams: [{ text: '2. Line with colours.' }]
+              }, {
+                colour:  { background: '#005700', foreground: '#a7ff00' },
+                streams: [{ text: '3. Line with explicit colour declaration.' }]
+              }, {
+                streams: [{
+                  colour: { background: '', foreground: '#ff00ff' },
+                  text:   '4. Line with only foreground set.'
+                }]
+              }, {
+                streams: [{
+                  colour: { background: '#00ff00', foreground: '' },
+                  text:   '5. Line with only background set.'
+                }]
+              }
+            ]
+          }
+        )
+      end
+
+      it 'handles styles' do
+        Vedeu.view 'testing_view' do
+          line do
+            style 'normal'
+            text  '1. Line with a normal style.'
+          end
+
+          line do
+            style 'underline'
+            text  '2. Line with an underline style.'
+          end
+
+          line do # actually uses streams
+            style 'normal' do
+              text '3. Line with a normal style.'
+            end
+          end
+
+          line do # actually uses streams
+            style 'underline' do
+              text '4. Line with an underlined style.'
+            end
+          end
+        end.must_equal(
+          {
+            name: 'testing_view',
+            lines: [
+              {
+                style:   'normal',
+                streams: [{
+                  text: '1. Line with a normal style.'
+                }]
+              }, {
+                style:   'underline',
+                streams: [{
+                  text: '2. Line with an underline style.'
+                }]
+              }, {
+                streams: [{
+                  style: 'normal',
+                  text:  '3. Line with a normal style.'
+                }]
+              }, {
+                streams: [{
+                  style: 'underline',
+                  text:  '4. Line with an underlined style.'
+                }]
+              }
+            ]
+          }
+        )
+      end
+
+      it 'handles streams, which means sub-line colours and styles' do
+        Vedeu.view 'testing_view' do
+          line do
+            stream do
+              text 'A stream of text.'
+            end
+          end.must_equal(
+
+        )
+      end
+
+      it 'handles streams, which means sub-line colours and styles' do
+        Vedeu.line do
+            stream do # Stream is an 'explicit declaration'.
+                      # We don't need it unless we want to add colours and styles.
+                      # See below.
+              text '1. Two streams of text, these will be'
+            end
+
+            stream do
+              text ' on the same line- note the space.'
+            end
+          end.must_equal(
+
+        )
+      end
+
+      it 'handles streams, which means sub-line colours and styles' do
+        Vedeu.line do
+            stream do
+              text '2. Streams can have'
+            end
+
+            stream do
+              colour foreground: '#ff0000', background: '#00ff00'
+              text   ' colours too.'
+            end
+          end.must_equal(
+
+        )
+      end
+
+      it 'handles streams, which means sub-line colours and styles' do
+        Vedeu.line do
+            stream do
+              text '3. Streams can have'
+            end
+
+            stream do
+              style 'underline'
+              text  ' styles too.'
+            end
+          end
+        end.must_equal(
+
+        )
+      end
+
+      it 'handles alignment' do
+        Vedeu.view 'testing_view' do
+          line do
+            width 80
+            text  'This is aligned left, and padded with spaces.'
+          end
+        end.must_equal(
+          {
+            name:  'testing_view',
+            lines: [{
+              streams: [{
+                width: 80,
+                text:  'This is aligned left, and padded with spaces.'
+              }]
+            }]
+          }
+        )
+      end
+
+      it 'handles alignment' do
+        Vedeu.view 'testing_view' do
+          line do
+            width 80
+            align :left # explicit
+            text  'This is aligned left, and padded with spaces.'
+          end
+        end.must_equal(
+          {
+            name:  'testing_view',
+            lines: [{
+              streams: [{
+                width: 80,
+                align: :left,
+                text:  'This is aligned left, and padded with spaces.'
+              }]
+            }]
+          }
+        )
+      end
+
+      it 'handles alignment' do
+        Vedeu.view 'testing_view' do
+          line do
+            width 80
+            align :centre
+            text  'This is aligned centrally, and padded with spaces.'
+          end
+        end.must_equal(
+          {
+            name:  'testing_view',
+            lines: [{
+              streams: [{
+                width: 80,
+                align: :centre,
+                text:  'This is aligned right, and padded with spaces.'
+              }]
+            }]
+          }
+        )
+      end
+
+      it 'handles alignment' do
+        Vedeu.view 'testing_view' do
+          line do
+            width 80
+            align :right
+            text  'This is aligned right, and padded with spaces.'
+          end
+        end.must_equal(
+          {
+            name:  'testing_view',
+            lines: [{
+              streams: [{
+                width: 80,
+                align: :right,
+                text:  'This is aligned right, and padded with spaces.'
+              }]
+            }]
+          }
+        )
+      end
     end
   end
 end
