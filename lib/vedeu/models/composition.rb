074b413cf61e2a2c6dd0ba13effbce0d9c1370be
Gavin Laking <gavinlaking@gmail.com>
2014-08-10 22:40:12 +0100

    Extract Buffers.

--- lib/vedeu/models/composition.rb
+++ lib/vedeu/models/composition.rb
@@ -1,60 +1,9 @@
-require 'pry'
 require 'virtus'
 
-require 'vedeu/support/terminal'
 require 'vedeu/models/attributes/interface_collection'
+require 'vedeu/output/buffers'
 
 module Vedeu
-  RefreshFailed = Class.new(StandardError)
-
-  module Buffers
-    extend self
-
-    def create(name, sequence)
-      buffers[name][:clear] = sequence
-    end
-
-    def enqueue(name, sequence)
-      buffers[name][:next].unshift(sequence)
-    end
-
-    def refresh(name)
-      data = buffers.fetch(name) do
-        fail RefreshFailed, 'Cannot refresh non-existent interface.'
-      end
-
-      sequence = if data[:next].any?
-        data[:current] = data[:next].pop
-
-      elsif data[:current].empty?
-        data[:clear]
-
-      else
-        data[:current]
-
-      end
-      Terminal.output(sequence)
-    end
-
-    def refresh_all
-      buffers.keys.map do |name|
-        refresh(name)
-      end
-    end
-
-    def reset
-      @buffers = {}
-    end
-
-    private
-
-    def buffers
-      @buffers ||= Hash.new do |hash, key|
-        hash[key] = { current: '', next: [], clear: '' }
-      end
-    end
-  end
-
   class Composition
     include Virtus.model
 
--- test/lib/vedeu/models/interface_test.rb
+++ test/lib/vedeu/models/interface_test.rb
@@ -46,10 +46,6 @@ module Vedeu
       interface.geometry.must_be_instance_of(Geometry)
     end
 
-    it 'has a current attribute' do
-      interface.current.must_equal("\e[1;1H#initialize")
-    end
-
     it 'has a cursor attribute' do
       interface.cursor.must_equal(true)
       Interface.new({ cursor: false }).cursor.must_equal(false)
@@ -57,121 +53,6 @@ module Vedeu
 
     it 'has a delay attribute' do
       interface.delay.must_equal(0.0)
-    end
-
-    describe '#enqueue' do
-      it 'delegates to the Queue class to enqueue itself' do
-        interface = Interface.new({
-          name:   'Interface#enqueue',
-          geometry: {
-            width:  8,
-            height: 2,
-          },
-          lines:  [ { streams: { text: 'a8f39' } } ]
-        })
-        interface.enqueue
-        interface.dequeue.must_equal(
-          "\e[1;1H        \e[1;1H" \
-          "\e[2;1H        \e[2;1H" \
-          "\e[1;1Ha8f39"
-        )
-      end
-    end
-
-    describe '#refresh' do
-      let(:attributes) {
-        {
-          name:   '#refresh',
-          lines:  [],
-          colour: {
-            foreground: '#ff0000',
-            background: '#000000'
-          },
-          geometry: {
-            width:  3,
-            height: 3
-          }
-        }
-      }
-
-      it 'returns a blank interface when there is no content to display (initial state)' do
-        Terminal.stub(:output, nil) do
-          Interface.new(attributes).refresh.must_equal(
-            "\e[38;5;196m\e[48;5;16m" \
-            "\e[1;1H   \e[1;1H" \
-            "\e[2;1H   \e[2;1H" \
-            "\e[3;1H   \e[3;1H"
-          )
-        end
-      end
-
-      it 'returns the fresh content when content is queued up to be displayed' do
-        attributes = {
-          name:   '#refresh',
-          lines:  [
-            { streams: [{ text: '#refresh' }] },
-            { streams: [{ text: '#refresh' }] },
-            { streams: [{ text: '#refresh' }] }
-          ],
-          colour: {
-            foreground: '#ff0000',
-            background: '#000000'
-          },
-          geometry: {
-            width:  11,
-            height: 3
-          }
-        }
-        interface = Interface.new(attributes)
-        diode = interface.enqueue
-
-        Terminal.stub(:output, nil) do
-          interface.refresh.must_equal(
-            "\e[38;5;196m\e[48;5;16m" \
-            "\e[1;1H           \e[1;1H" \
-            "\e[2;1H           \e[2;1H" \
-            "\e[3;1H           \e[3;1H" \
-            "\e[1;1H#refresh" \
-            "\e[2;1H#refresh" \
-            "\e[3;1H#refresh"
-          )
-        end
-      end
-
-      it 'returns the previously shown content when there is stale content from last run' do
-        attributes = {
-          name:   '#refresh',
-          lines:  [],
-          colour: {
-            foreground: '#ff0000',
-            background: '#000000'
-          },
-          geometry: {
-            width:  11,
-            height: 3
-          }
-        }
-        interface = Interface.new(attributes)
-        interface.current = "\e[38;5;196m\e[48;5;16m" \
-                            "\e[1;1H           \e[1;1H" \
-                            "\e[2;1H           \e[2;1H" \
-                            "\e[3;1H           \e[3;1H" \
-                            "\e[1;1H#refresh" \
-                            "\e[2;1H#refresh" \
-                            "\e[3;1H#refresh"
-
-        Terminal.stub(:output, nil) do
-          interface.refresh.must_equal(
-            "\e[38;5;196m\e[48;5;16m" \
-            "\e[1;1H           \e[1;1H" \
-            "\e[2;1H           \e[2;1H" \
-            "\e[3;1H           \e[3;1H" \
-            "\e[1;1H#refresh" \
-            "\e[2;1H#refresh" \
-            "\e[3;1H#refresh"
-          )
-        end
-      end
     end
 
     describe '#to_s' do
