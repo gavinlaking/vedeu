7183824f5bef54a9df6d11aebc851cbaa6cf042f
Gavin Laking <gavinlaking@gmail.com>
2014-08-21 19:48:19 +0100

    Grid is not an API class;
    This class supports the API and therefore has been moved.

--- lib/vedeu/support/grid.rb
+++ lib/vedeu/support/grid.rb
@@ -11,44 +11,42 @@ class Fixnum
   #                     # Terminal is 92 characters wide, maximum value is
   #                     # therefore 84, meaning a column is 7 characters wide.
   def columns
-     Vedeu::API::Grid.columns(self)
+     Vedeu::Grid.columns(self)
   end
 end
 
 module Vedeu
-  module API
-    OutOfRange = Class.new(StandardError)
+  OutOfRange = Class.new(StandardError)
 
-    class Grid
-      def self.columns(value)
-        new(value).columns
-      end
+  class Grid
+    def self.columns(value)
+      new(value).columns
+    end
 
-      def initialize(value)
-        @value = value
-      end
+    def initialize(value)
+      @value = value
+    end
 
-      def columns
-        fail OutOfRange, 'Valid range is 1..12.' if out_of_range?
+    def columns
+      fail OutOfRange, 'Valid range is 1..12.' if out_of_range?
 
-        column * value
-      end
+      column * value
+    end
 
-      private
+    private
 
-      attr_reader :value
+    attr_reader :value
 
-      def column
-        actual / 12
-      end
+    def column
+      actual / 12
+    end
 
-      def actual
-        Terminal.width
-      end
+    def actual
+      Terminal.width
+    end
 
-      def out_of_range?
-        value < 1 || value > 12
-      end
+    def out_of_range?
+      value < 1 || value > 12
     end
   end
 end
