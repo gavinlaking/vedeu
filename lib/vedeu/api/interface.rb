ae1ad25fdfc238aa5b65d5856b354fb2dab4984c
Gavin Laking <gavinlaking@gmail.com>
2014-08-21 19:45:59 +0100

    Add more API documentation.

--- lib/vedeu/api/interface.rb
+++ lib/vedeu/api/interface.rb
@@ -25,81 +25,161 @@ module Vedeu
       end
 
       # @param block [Proc]
+      #
+      # @return []
       def define(&block)
         instance_eval(&block) if block_given?
 
         Vedeu::Store.create(attributes)
       end
 
+      # Define a single line in a view.
+      #
       # @param block [Proc]
+      #
+      # @example
+      #   view 'my_interface' do
+      #     line do
+      #       ... some line attributes ...
+      #     end
+      #   end
+      #
+      # @return []
       def line(&block)
         attributes[:lines] << Line.build(&block)
       end
 
       # @param value [String]
+      #
+      # @see Vedeu::API#use
       def use(value)
         Vedeu.use(value)
       end
 
+      # Define the default colour attributes for an interface.
+      #
       # @param value [Hash]
+      #
+      # @example
+      #   interface 'my_interface' do
+      #     colour background: '#000000', foreground: '#ffffff'
+      #     ... some interface attributes ...
+      #   end
+      #
+      # @return []
       def colour(value)
         attributes[:colour] = value
       end
 
+      # Define the cursor visibility for an interface. A `true` value will show
+      # the cursor, whilst `false` will hide it.
+      #
       # @param value [Boolean]
+      #
+      # @example
+      #   interface 'my_interface' do
+      #     cursor true
+      #     ... some interface attributes ...
+      #   end
+      #
+      # @return []
       def cursor(value)
         attributes[:cursor] = value
       end
 
       # @param value [Fixnum|Float]
+      #
+      # @return []
       def delay(value)
         attributes[:delay] = value
       end
 
+      # Define a group for an interface. Interfaces of the same group can be
+      # targetted together; for example you may want to refresh multiple
+      # interfaces at once.
+      #
       # @param value [String]
+      #
+      # @example
+      #   interface 'my_interface' do
+      #     group 'main_screen' do
+      #     ... some interface attributes ...
+      #   end
+      #
+      # @return []
       def group(value)
         attributes[:group] = value
       end
 
+      # The name of the interface. Used to reference the interface throughout
+      # your application's execution lifetime.
+      #
       # @param value [String]
+      #
+      # @return []
       def name(value)
         attributes[:name] = value
       end
 
+      # Define the starting x position (column) of the interface.
+      #
       # @param value [Fixnum]
+      #
+      # @return []
       def x(value)
         fail XOutOfBounds if x_out_of_bounds?(value)
 
         attributes[:geometry][:x] = value
       end
 
+      # Define the starting y position (row/line) of the interface.
+      #
       # @param value [Fixnum]
+      #
+      # @return []
       def y(value)
         fail YOutOfBounds if y_out_of_bounds?(value)
 
         attributes[:geometry][:y] = value
       end
 
+      # Define the number of characters/columns wide the interface will be.
+      #
       # @param value [Fixnum]
+      #
+      # @return []
       def width(value)
         fail InvalidWidth if x_out_of_bounds?(value)
 
         attributes[:geometry][:width] = value
       end
 
+      # Define the number of characters/rows/lines tall the interface will be.
+      #
       # @param value [Fixnum]
+      #
+      # @return []
       def height(value)
         fail InvalidHeight if y_out_of_bounds?(value)
 
         attributes[:geometry][:height] = value
       end
 
+      # Instructs Vedeu to calculate x and y geometry automatically based on the
+      # centre character of the terminal, the width and the height.
+      #
       # @param value [Boolean]
+      #
+      # @return []
       def centred(value)
         attributes[:geometry][:centred] = value
       end
 
+      # Define the default style attributes for an interface.
+      #
       # @param value [Array|String]
+      #
+      # @return []
       def style(value)
         attributes[:style] = value
       end
---
+++ test/lib/vedeu/api/line_test.rb
@@ -0,0 +1,71 @@
+require 'test_helper'
+
+module Vedeu
+  module API
+    describe Line do
+      describe '#stream' do
+        it 'returns the value assigned' do
+          Line.new.stream do
+            text 'Some text...'
+          end.must_equal(
+            [
+              {
+                colour: {},
+                style: [],
+                text: "Some text...",
+                width: nil,
+                align: :left
+              }
+            ]
+          )
+        end
+      end
+
+      describe '#text' do
+        it 'returns the value assigned' do
+          Line.new.text('Some text...').must_equal(
+            [
+              { text: 'Some text...' }
+            ]
+          )
+        end
+      end
+
+      describe '#foreground' do
+        it 'returns the value assigned' do
+          Line.new.foreground('#ff0000').must_equal(
+            [
+              {
+                colour: {
+                  foreground: "#ff0000"
+                },
+                style: [],
+                text: "",
+                width: nil,
+                align: :left
+              }
+            ]
+          )
+        end
+      end
+
+      describe '#background' do
+        it 'returns the value assigned' do
+          Line.new.background('#00ff00').must_equal(
+            [
+              {
+                colour: {
+                  background: "#00ff00"
+                },
+                style: [],
+                text: "",
+                width: nil,
+                align: :left
+              }
+            ]
+          )
+        end
+      end
+    end
+  end
+end
