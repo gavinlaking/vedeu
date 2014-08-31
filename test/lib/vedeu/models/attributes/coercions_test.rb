4c95f7ec2e149c5d571f2767de65b5588d1ffa10
Gavin Laking <gavinlaking@gmail.com>
2014-08-31 21:53:51 +0100

    Rename coercer_test.rb to coercions_test.rb;
    Also, fix tests.

--- test/lib/vedeu/models/attributes/coercions_test.rb
+++ test/lib/vedeu/models/attributes/coercions_test.rb
@@ -4,20 +4,17 @@ module Vedeu
   class TestClass
     include Coercions
 
+    attr_reader :attributes
+
     def initialize(attributes = {})
       @attributes = attributes
       @name       = attributes[:name]
+      @parent     = attributes[:parent]
     end
   end
 
   describe Coercions do
     describe '.coercer' do
-      let(:klass) { mock('TestClass') }
-
-      before do
-        TestClass.stubs(:new).returns(klass)
-      end
-
       [nil, [], {}].each do |empty_value|
         it 'returns an empty collection when nil or empty' do
           TestClass.coercer(empty_value).must_equal([])
@@ -25,13 +22,15 @@ module Vedeu
       end
 
       it 'returns when an object' do
+        klass = TestClass.new
+
         TestClass.coercer([klass]).must_equal([klass])
       end
 
       it 'returns a collection of models when a single hash' do
         coerced = TestClass.coercer({ :name => 'test1' })
-        coerced.size.must_equal(1)
-        coerced.must_equal([klass])
+        coerced.must_be_instance_of(Array)
+        coerced.first.attributes.must_equal({ name: "test1", parent: nil })
       end
 
       it 'returns a collection of models when multiple hashes' do
@@ -39,13 +38,15 @@ module Vedeu
           { :name => 'test1' }, { :name => 'test2' }
         ])
         coerced.size.must_equal(2)
-        coerced.must_equal([klass, klass])
+        coerced.map(&:attributes).must_equal(
+          [{ name: "test1", parent: nil }, { name: "test2", parent: nil }]
+        )
       end
 
       it 'returns a collection of models when a single array' do
         coerced = TestClass.coercer([{ :name => 'test3' }])
         coerced.size.must_equal(1)
-        coerced.must_equal([klass])
+        coerced.first.attributes.must_equal({ name: "test3", parent: nil })
       end
     end
   end
