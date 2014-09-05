1e55be3cbacf598a19355cf25e1f19eaad0908f1
Gavin Laking <gavinlaking@gmail.com>
2014-09-05 20:00:24 +0100

    Still trying to write integration tests.

---
+++ test/integration/defining_interfaces_test.rb
@@ -0,0 +1,27 @@
+require 'test_helper'
+
+class MyFirstApplication
+  include Vedeu
+
+  interface 'hydrogen' do
+    x       2
+    y       2
+    width   40
+    height  4
+    centred false
+    cursor  true
+    delay   0.25
+    group   'elements'
+  end
+
+  def initialize; end
+end
+
+describe 'Define an interface' do
+  it 'defines an interface' do
+    skip
+    MyFirstApplication.new
+
+    Vedeu.defined.interfaces.must_equal(['hydrogen'])
+  end
+end
