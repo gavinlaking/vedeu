228aff70bdfcf640f464baa2243d08baa582b3c8
Gavin Laking <gavinlaking@gmail.com>
2014-09-06 22:09:53 +0100

    Reorder tests to match method definition.

--- test/lib/vedeu/repositories/groups_test.rb
+++ test/lib/vedeu/repositories/groups_test.rb
@@ -2,8 +2,16 @@ require 'test_helper'
 
 module Vedeu
   describe Groups do
-    before { Groups.reset }
-    before { Groups.reset }
+    describe '#add' do
+      it 'returns false if the group name is empty' do
+        Groups.add({ group: '', name: 'gallium', delay: 0.0 }).must_equal(false)
+      end
+
+      it 'adds the interface name to the group in storage' do
+        Groups.add({ group: 'elements', name: 'germanium', delay: 0.0 })
+        Groups.all.must_equal({ 'elements' => Set['germanium'] })
+      end
+    end
 
     describe '#all' do
       before do
@@ -16,6 +24,18 @@ module Vedeu
         Groups.all.must_equal({
           "elements" => Set["cobalt", "nickel", "copper"]
         })
+      end
+    end
+
+    describe '#find' do
+      before { Groups.add({ group: 'elements', name: 'zinc', delay: 0.0 }) }
+
+      it 'raises an exception if the group cannot be found' do
+        proc { Groups.find('not_found') }.must_raise(GroupNotFound)
+      end
+
+      it 'returns a collection of interface names belonging to the group' do
+        Groups.find('elements').must_equal(Set['zinc'])
       end
     end
 
@@ -32,26 +52,15 @@ module Vedeu
       end
     end
 
-    describe '#find' do
-      before { Groups.add({ group: 'elements', name: 'zinc', delay: 0.0 }) }
+    describe '#registered?' do
+      it 'returns true when the group is registered' do
+        Groups.add({ group: 'registered' })
 
-      it 'raises an exception if the group cannot be found' do
-        proc { Groups.find('not_found') }.must_raise(GroupNotFound)
+        Groups.registered?('registered').must_equal(true)
       end
 
-      it 'returns a collection of interface names belonging to the group' do
-        Groups.find('elements').must_equal(Set['zinc'])
-      end
-    end
-
-    describe '#add' do
-      it 'returns false if the group name is empty' do
-        Groups.add({ group: '', name: 'gallium', delay: 0.0 }).must_equal(false)
-      end
-
-      it 'adds the interface name to the group in storage' do
-        Groups.add({ group: 'elements', name: 'germanium', delay: 0.0 })
-        Groups.all.must_equal({ 'elements' => Set['germanium'] })
+      it 'returns false when the group is not registered' do
+        Groups.registered?('not_registered').must_equal(false)
       end
     end
 
