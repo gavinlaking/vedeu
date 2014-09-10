486068a85cb961b9e378966e19bacc36827f4677
Gavin Laking <gavinlaking@gmail.com>
2014-09-10 20:31:31 +0100

    Add tests for Common.

--- test/lib/vedeu/support/common_test.rb
+++ test/lib/vedeu/support/common_test.rb
@@ -1,6 +1,25 @@
 require 'test_helper'
 
+class VedeuCommonClass
+  include Vedeu::Common
+
+  def defined_value_test(variable)
+    defined_value?(variable)
+  end
+end
+
 module Vedeu
   describe Common do
+
+    describe '#defined_value?' do
+      it 'return true when the variable is not nil or empty' do
+        VedeuCommonClass.new.defined_value_test('not empty').must_equal(true)
+      end
+
+      it 'returns false when the variable is nil or empty' do
+        VedeuCommonClass.new.defined_value_test([]).must_equal(false)
+      end
+    end
+
   end
 end
