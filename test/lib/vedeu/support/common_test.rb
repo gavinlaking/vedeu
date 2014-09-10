75f2a985573a2910e240758959a704be57204c4c
Gavin Laking <gavinlaking@gmail.com>
2014-09-10 21:17:46 +0100

    Add an attributes test for Common.

--- test/lib/vedeu/support/common_test.rb
+++ test/lib/vedeu/support/common_test.rb
@@ -19,6 +19,13 @@ module Vedeu
       it 'returns false when the variable is nil or empty' do
         VedeuCommonClass.new.defined_value_test([]).must_equal(false)
       end
+
+      it 'returns false when dealing with hash keys which may not exist or ' \
+         'have a value' do
+        attributes = {}
+
+        VedeuCommonClass.new.defined_value_test(attributes[:misc]).must_equal(false)
+      end
     end
 
   end
