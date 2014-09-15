6415a41ffcce632b156f51a77e1cdaec97038bde
Gavin Laking <gavinlaking@gmail.com>
2014-09-15 22:47:27 +0100

    Handle Fixnum as a defined value.

--- lib/vedeu/support/common.rb
+++ lib/vedeu/support/common.rb
@@ -7,9 +7,10 @@ module Vedeu
 
     # Returns a boolean indicating whether a variable has a useful value.
     #
-    # @param variable [String|Symbol|Array] The variable to check.
+    # @param variable [String|Symbol|Array|Fixnum] The variable to check.
     # @return [Boolean]
     def defined_value?(variable)
+      return true if variable.is_a?(Fixnum)
       return true unless variable.nil? || variable.empty?
 
       false
