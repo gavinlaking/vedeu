d4d333e8eb5c6bcc88b240684ae5cd411ccd4c95
Gavin Laking <gavinlaking@gmail.com>
2014-10-14 21:26:49 +0100

    Cursors are hidden by default; fix tests to reflect.

--- test/lib/vedeu/output/compositor_test.rb
+++ test/lib/vedeu/output/compositor_test.rb
@@ -27,7 +27,7 @@ module Vedeu
             "\e[1;1H     \e[1;1H" \
             "\e[2;1H     \e[2;1H" \
             "\e[3;1H     \e[3;1H" \
-            "\e[1;1H\e[?25h"
+            "\e[1;1H\e[?25l"
           ])
         end
       end
@@ -62,7 +62,7 @@ module Vedeu
             "\e[1;1Hargon" \
             "\e[2;1Hboron" \
             "\e[3;1Hradon" \
-            "\e[1;1H\e[?25h"
+            "\e[1;1H\e[?25l"
           ])
         end
       end
--- test/lib/vedeu/output/render_test.rb
+++ test/lib/vedeu/output/render_test.rb
@@ -37,7 +37,7 @@ module Vedeu
           "\e[1;1Hthis is the first" \
           "\e[2;1Hthis is the second and it is lon" \
           "\e[3;1Hthis is the third, it is even lo" \
-          "\e[1;1H\e[?25h"
+          "\e[1;1H\e[?25l"
         )
       end
 
@@ -55,7 +55,7 @@ module Vedeu
           "\e[1;1H                                \e[1;1H" \
           "\e[2;1H                                \e[2;1H" \
           "\e[3;1H                                \e[3;1H" \
-          "\e[1;1H\e[?25h"
+          "\e[1;1H\e[?25l"
         )
       end
 
@@ -89,7 +89,7 @@ module Vedeu
           "\e[1;1Hthis is the first" \
           "\e[2;1H" \
           "\e[3;1Hthis is the third, it is even lo" \
-          "\e[1;1H\e[?25h"
+          "\e[1;1H\e[?25l"
         )
       end
 
