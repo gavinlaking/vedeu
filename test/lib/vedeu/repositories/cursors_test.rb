d210df058f1229454f2c8abcbcd1ab4aa3ab6e0d
Gavin Laking <gavinlaking@gmail.com>
2014-10-19 19:13:36 +0100

    Add test coverage for #update, #hide and #show.

--- test/lib/vedeu/repositories/cursors_test.rb
+++ test/lib/vedeu/repositories/cursors_test.rb
@@ -5,11 +5,15 @@ module Vedeu
 
     before do
       Cursors.reset
-      Interfaces.add({ name: 'chromium' })
+      Registrar.record({ name: 'chromium' })
     end
 
     describe '#add' do
       let(:attributes) { { name: 'chromium' } }
+
+      it 'returns a new Cursor after registering' do
+        Cursors.add(attributes).must_be_instance_of(Cursor)
+      end
 
       it 'returns a new Cursor after updating' do
         Cursors.update(attributes).must_be_instance_of(Cursor)
@@ -21,6 +25,24 @@ module Vedeu
         it 'raises an exception' do
           proc { Cursors.update(attributes) }.must_raise(MissingRequired)
         end
+
+        it 'raises an exception' do
+          proc { Cursors.add(attributes) }.must_raise(MissingRequired)
+        end
+      end
+    end
+
+    describe '#hide' do
+      it 'sets the state attribute of the Cursor to :hide' do
+        cursor = Cursors.hide
+        cursor.state.must_equal(:hide)
+      end
+    end
+
+    describe '#show' do
+      it 'sets the state attribute of the Cursor to :show' do
+        cursor = Cursors.show
+        cursor.state.must_equal(:show)
       end
     end
 
