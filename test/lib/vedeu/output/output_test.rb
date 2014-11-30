d51e3edca253279d7249e10f3422e0a6d57545f9
Gavin Laking <gavinlaking@gmail.com>
2014-11-30 17:45:18 +0000

    Refactor Output tests.

--- test/lib/vedeu/output/output_test.rb
+++ test/lib/vedeu/output/output_test.rb
@@ -3,71 +3,79 @@ require 'test_helper'
 module Vedeu
 
   describe Output do
+    let(:interface) {
+      Interface.new({
+        name:   'flourine',
+        geometry: {
+          width:  32,
+          height: 3,
+        },
+        lines:    lines,
+        colour:   colour
+      })
+    }
+    let(:lines) {
+      [
+        {
+          streams: [{ text: 'this is the first' }]
+        },
+        {
+          streams: [{ text: 'this is the second and it is long' }]
+        },
+        {
+          streams: [{ text: 'this is the third, it is even longer and still truncated' }]
+        },
+        {
+          streams: [{ text: 'this should not render' }]
+        }
+      ]
+    }
+    let(:colour) { {} }
 
     before do
       Buffers.reset
       Cursors.reset
       Interfaces.reset
 
+      Terminal.console.stubs(:winsize).returns([5, 40])
       Terminal.console.stubs(:print)
-
-      Vedeu.interface('fluorine') do
-        width  32
-        height 3
-        line 'this is the first'
-        line 'this is the second and it is long'
-        line 'this is the third, it is even longer and still truncated'
-        line 'this should not render'
-      end
     end
 
     describe '#initialize' do
       it 'returns an instance of itself' do
-        interface = mock('Interface')
         Output.new(interface).must_be_instance_of(Output)
       end
     end
 
     describe '.clear' do
       it 'returns the escape sequence to clear the whole interface' do
-        interface = Interface.new({
-          name:   'Output.clear',
-          geometry: {
-            width:  5,
-            height: 2
-          }
-        })
         Output.clear(interface).must_equal([
-          "\e[1;1H     \e[1;1H" \
-          "\e[2;1H     \e[2;1H"
+          "\e[1;1H                                \e[1;1H" \
+          "\e[2;1H                                \e[2;1H" \
+          "\e[3;1H                                \e[3;1H" \
         ])
       end
 
-      it 'returns the escape sequence to clear the whole interface with specified colours' do
-        interface = Interface.new({
-          name:   'Output.clear',
-          geometry: {
-            width:  5,
-            height: 2,
-          },
-          colour: {
+      context 'when colours are specified' do
+        let(:colour) {
+          {
             foreground: '#00ff00',
             background: '#ffff00'
           }
-        })
-        Output.clear(interface).must_equal([
-          "\e[38;2;0;255;0m\e[48;2;255;255;0m" \
-          "\e[1;1H     \e[1;1H" \
-          "\e[2;1H     \e[2;1H"
-        ])
+        }
+
+        it 'returns the escape sequences to clear the whole interface with colours' do
+          Output.clear(interface).must_equal([
+            "\e[38;2;0;255;0m\e[48;2;255;255;0m" \
+            "\e[1;1H                                \e[1;1H" \
+            "\e[2;1H                                \e[2;1H" \
+            "\e[3;1H                                \e[3;1H" \
+          ])
+        end
       end
     end
 
     describe '.render' do
-      let(:interface) { Vedeu.use('fluorine') }
-
-      before { interface.stubs(:in_focus?).returns(true) }
-
       it 'returns the content for the interface' do
         Output.render(interface).must_equal(
           "\e[1;1H                                \e[1;1H" \
@@ -79,31 +87,21 @@ module Vedeu
         )
       end
 
-      it 'returns a blank interface if there are no streams of text' do
-        interface = Interface.new({
-          name:     'fluorine',
-          geometry: {
-            width:  32,
-            height: 3,
-          },
-          lines:    []
-        })
+      context 'when there are no streams of text' do
+        let(:lines) { [] }
 
-        Output.render(interface).must_equal(
-          "\e[1;1H                                \e[1;1H" \
-          "\e[2;1H                                \e[2;1H" \
-          "\e[3;1H                                \e[3;1H"
-        )
+        it 'returns a blank interface' do
+          Output.render(interface).must_equal(
+            "\e[1;1H                                \e[1;1H" \
+            "\e[2;1H                                \e[2;1H" \
+            "\e[3;1H                                \e[3;1H"
+          )
+        end
       end
 
-      it 'skips lines which have streams with no content' do
-        interface = Interface.new({
-          name:     'fluorine',
-          geometry: {
-            width:  32,
-            height: 3,
-          },
-          lines:    [
+      context 'when lines have streams which are empty' do
+        let(:lines) {
+          [
             {
               streams: [{ text: 'this is the first' }]
             }, {
@@ -118,15 +116,18 @@ module Vedeu
               streams: [{ text: 'this should not render' }]
             }
           ]
-        })
-        Output.render(interface).must_equal(
-          "\e[1;1H                                \e[1;1H" \
-          "\e[2;1H                                \e[2;1H" \
-          "\e[3;1H                                \e[3;1H" \
-          "\e[1;1Hthis is the first" \
-          "\e[2;1H" \
-          "\e[3;1Hthis is the third, it is even lo"
-        )
+        }
+
+        it 'skips lines' do
+          Output.render(interface).must_equal(
+            "\e[1;1H                                \e[1;1H" \
+            "\e[2;1H                                \e[2;1H" \
+            "\e[3;1H                                \e[3;1H" \
+            "\e[1;1Hthis is the first" \
+            "\e[2;1H" \
+            "\e[3;1Hthis is the third, it is even lo"
+          )
+        end
       end
     end
 
