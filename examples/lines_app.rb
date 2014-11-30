dce22b9cb4e974432704fc4e1d3bb31d48aca9d5
Gavin Laking <gavinlaking@gmail.com>
2014-11-30 11:36:16 +0000

    Move CursorApp and LinesApp into root of examples directory.

--- examples/lines_app.rb
+++ examples/lines_app.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
-lib_dir = File.dirname(__FILE__) + '/../../lib'
+lib_dir = File.dirname(__FILE__) + '/../lib'
 $LOAD_PATH.unshift(lib_dir) unless $LOAD_PATH.include?(lib_dir)
 
 -> { its -> { a } }
