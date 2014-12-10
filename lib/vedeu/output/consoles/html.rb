4e55727abe2d706ffcc8f2e609a8e036404034ad
Gavin Laking <gavinlaking@gmail.com>
2014-12-10 20:49:45 +0000

    Start work on HTML console.

---
+++ lib/vedeu/output/consoles/html.rb
@@ -0,0 +1,18 @@
+module Vedeu
+
+  module Consoles
+
+    # The HTML console takes output from the terminal and converts it to HTML.
+    class HTML
+
+      def write
+        # do nothing
+      end
+
+      private
+
+    end # HTML
+
+  end # Console
+
+end # Vedeu
