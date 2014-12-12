08db9ef9ba72d1b5713a9c17b7cec2d1361879e3
Gavin Laking <gavinlaking@gmail.com>
2014-12-12 15:59:30 +0000

    Add DSL::Stream.

---
+++ lib/vedeu/dsl/stream.rb
@@ -0,0 +1,92 @@
+module Vedeu
+
+  module DSL
+
+    # Provides methods to be used to define views.
+    #
+    # @api public
+    class Stream
+
+      include DSL::Colour
+      include DSL::Style
+
+      # Returns an instance of DSL::Stream.
+      #
+      # @param model [Stream]
+      def initialize(model = Vedeu::Stream.new)
+        @model = model
+      end
+
+      # Align a string (or object responding to `to_s`), and add it to the
+      # Stream.
+      #
+      # @note If using the convenience methods; {left}, {centre}, {center} or
+      #   {right}, then a specified anchor will be ignored.
+      #
+      # @example
+      #   left 'This will be left aligned.', width: 35
+      #   # => 'This will be left aligned.         '
+      #
+      #   centre 'This will be aligned centrally.', width: 35
+      #   # => '  This will be aligned centrally.  '
+      #   # centre is also aliased to center
+      #
+      #   right 'This will be right aligned.', width: 35
+      #   # => '        This will be right aligned.'
+
+      #   right 'This will be right aligned.', width: 35, anchor: centre
+      #   # => '        This will be right aligned.'
+      #
+      #   align 'This will be truncated here. More text here.', width: 28
+      #   # => 'This will be truncated here.'
+      #
+      #   align 'Padded with hyphens.', width: 25, pad: '-', anchor: :right
+      #   # => '-----Padded with hyphens.'
+      #
+      # @param value [String|Object] A string or object that responds to `to_s`.
+      # @param options [Hash] Alignment options.
+      # @option options :anchor [Symbol] One of `:left`, `:centre`/`:center`, or
+      #   `:right`.
+      # @option options :width [Integer|NilClass] The width of the text stream
+      #   to add. If the `string` provided is longer than this value, the string
+      #   will be truncated.
+      # @option options :pad [String] The character to use to pad the width, by
+      #   default uses an empty space (0x20). Only when the string is shorter
+      #   than the specified width.
+      # @return [String]
+      def align(value = '', options = {})
+        model.add_text(Vedeu::Align.with(value,
+          options.merge({ anchor: __callee__ })))
+      end
+      alias_method :left,   :align
+      alias_method :center, :align
+      alias_method :centre, :align
+      alias_method :right,  :align
+
+      # Add textual data to the stream via this method.
+      #
+      # @note Calls `to_s` on the 'value' param; meaning any object which
+      #   responds to `to_s` can be provided.
+      #
+      # @param value [String|undefined] The text to be added to this Stream.
+      # @return [String] The param 'value' as a String.
+      def text(value = '')
+        model.add_text(value.to_s)
+      end
+
+      # @deprecated
+      def width(value)
+        warn "Vedeu::API::Stream#width is now deprecated, and will be " \
+             "removed in version 0.3.0. Use: Vedeu::DSL::Stream#align " \
+             "(#{DOCS_URL}/Vedeu/DSL/Stream#align-instance_method)"
+      end
+
+      private
+
+      attr_reader :model
+
+    end # Stream
+
+  end # DSL
+
+end # Vedeu
---
+++ test/lib/vedeu/dsl/stream_test.rb
@@ -0,0 +1,86 @@
+require 'test_helper'
+
+module Vedeu
+
+  module DSL
+
+    describe Stream do
+
+      describe '#align' do
+        it 'returns the string aligned and added to the Stream' do
+          stream = Vedeu::Stream.build do
+            align 'This is added to the stream.', width: 30
+          end
+
+          stream.text.must_equal('This is added to the stream.  ')
+        end
+
+        context 'alias_method' do
+          context ':left' do
+            it 'returns the string aligned and added to the Stream' do
+              stream = Vedeu::Stream.build do
+                left 'This is added to the stream.', width: 30
+              end
+
+              stream.text.must_equal('This is added to the stream.  ')
+            end
+          end
+
+          context ':centre/:center' do
+            it 'returns the string aligned and added to the Stream' do
+              stream = Vedeu::Stream.build do
+                centre 'This is added to the stream.', width: 30
+              end
+
+              stream.text.must_equal(' This is added to the stream. ')
+            end
+          end
+
+          context ':right' do
+            it 'returns the string aligned and added to the Stream' do
+              stream = Vedeu::Stream.build do
+                right '  This is added to the stream.', width: 30
+              end
+
+              stream.text.must_equal('  This is added to the stream.')
+            end
+
+            it 'ignores the anchor option when provided' do
+              stream = Vedeu::Stream.build do
+                right 'This is added to the stream.', width: 30, anchor: :left
+              end
+
+              stream.text.must_equal('  This is added to the stream.')
+            end
+          end
+        end
+      end
+
+      describe '#text' do
+        it 'adds the string to the Stream' do
+          stream = Vedeu::Stream.build do
+            text 'This is added to the stream.'
+          end
+
+          stream.text.must_equal('This is added to the stream.')
+        end
+
+        context 'multiple calls' do
+          it 'adds the strings to the Stream' do
+            stream = Vedeu::Stream.build do
+              text 'This is added to the stream. '
+              text 'This is also added to the stream.'
+            end
+
+            stream.text.must_equal(
+              'This is added to the stream. This is also added to the stream.'
+            )
+          end
+        end
+      end
+
+    end # Stream
+
+  end # DSL
+
+end # Vedeu
