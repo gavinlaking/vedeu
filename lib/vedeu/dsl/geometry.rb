176e75d5c4d2690912d6aa88f0e85ef60c1f0df1
Gavin Laking <gavinlaking@gmail.com>
2014-12-12 15:58:42 +0000

    Add DSL::Geometry.

---
+++ lib/vedeu/dsl/geometry.rb
@@ -0,0 +1,121 @@
+module Vedeu
+
+  module DSL
+
+    class Geometry
+
+      # Returns an instance of DSL::Geometry.
+      #
+      # @param model [Geometry]
+      def initialize(model = Vedeu::Geometry.new)
+        @model = model
+      end
+
+      # Instructs Vedeu to calculate x and y geometry automatically based on the
+      # centre character of the terminal, the width and the height.
+      #
+      # @param value [Boolean] Any value other than nil or false will evaluate
+      #   to true.
+      #
+      # @example
+      #   interface 'my_interface' do
+      #     centred!
+      #
+      #   interface 'my_interface' do
+      #     centred false
+      #     ...
+      #
+      # @return [Boolean]
+      def centred(value = true)
+        model.set_centred(!!value)
+      end
+      alias_method :centred!, :centred
+
+      # Specify the number of characters/rows/lines tall the interface will be.
+      #
+      # @param value [Fixnum]
+      #
+      # @example
+      #   interface 'my_interface' do
+      #     height 8
+      #     ...
+      #
+      # @return [Fixnum]
+      def height(value)
+        Vedeu.log(out_of_bounds('height')) if y_out_of_bounds?(value)
+
+        model.set_height(value)
+      end
+
+      # Specify the number of characters/columns wide the interface will be.
+      #
+      # @param value [Fixnum]
+      #
+      # @example
+      #   interface 'my_interface' do
+      #     width 25
+      #     ...
+      #
+      # @return [Fixnum]
+      def width(value)
+        Vedeu.log(out_of_bounds('width')) if x_out_of_bounds?(value)
+
+        model.set_width(value)
+      end
+
+      # Specify the starting x position (column) of the interface.
+      #
+      # @param value [Fixnum]
+      # @param block [Proc]
+      #
+      # @example
+      #   interface 'my_interface' do
+      #     x 7 # start on column 7.
+      #
+      #   interface 'other_interface' do
+      #     x { use('my_interface').east } # start on column 8, if
+      #                                    # `my_interface` changes position,
+      #                                    # `other_interface` will too.
+      #
+      # @return [Fixnum]
+      def x(value = 0, &block)
+        return model.x = block if block_given?
+
+        Vedeu.log(out_of_bounds('x')) if x_out_of_bounds?(value)
+
+        model.set_x(value)
+      end
+
+      # Specify the starting y position (row/line) of the interface.
+      #
+      # @param value [Fixnum]
+      # @param block [Proc]
+      #
+      # @example
+      #   interface 'my_interface' do
+      #     y  4
+      #     ...
+      #
+      #   interface 'other_interface' do
+      #     y  { use('my_interface').north } # start on row/line 3, if
+      #     ...                              # `my_interface` changes position,
+      #                                      # `other_interface` will too.
+      #
+      # @return [Fixnum]
+      def y(value = 0, &block)
+        return model.y = block if block_given?
+
+        Vedeu.log(out_of_bounds('y')) if y_out_of_bounds?(value)
+
+        model.set_y(value)
+      end
+
+      private
+
+      attr_reader :model
+
+    end # Geometry
+
+  end # DSL
+
+end # Vedeu
---
+++ test/lib/vedeu/dsl/geometry_test.rb
@@ -0,0 +1,80 @@
+require 'test_helper'
+
+module Vedeu
+
+  module DSL
+
+    describe Geometry do
+
+      describe '#centred' do
+        it 'returns false if the value is false or nil' do
+          skip
+          interface = Vedeu.interface('iron') { centred(false) }
+          interface.geometry.centred.must_equal(false)
+        end
+
+        it 'returns true' do
+          skip
+          interface = Vedeu.interface('iron') { centred! }
+          interface.geometry.centred.must_equal(true)
+        end
+      end
+
+      describe '#height' do
+        it 'sets the attribute to the value' do
+          Vedeu.interface 'iron' do
+            height 6
+          end
+
+          Vedeu.use('iron').attributes[:geometry][:height].must_equal(6)
+        end
+      end
+
+      describe '#width' do
+        it 'sets the attribute to the value' do
+          skip
+          Vedeu.interface 'iron' do
+            width 25
+          end
+
+          Vedeu.use('iron').attributes[:geometry][:width].must_equal(25)
+        end
+      end
+
+      describe '#x' do
+        it 'sets the attribute to the block if a block is given' do
+          Vedeu.interface 'iron' do
+            x { 9 }
+          end
+
+          Vedeu.use('iron').geometry.x.must_equal(9)
+        end
+
+        it 'sets the attribute to the value if a block is not given' do
+          Vedeu.interface 'iron' do
+            x 7
+          end
+
+          Vedeu.use('iron').attributes[:geometry][:x].must_equal(7)
+        end
+      end
+
+      describe '#y' do
+        it 'sets the attribute to the block if a block is given' do
+          Vedeu.interface 'iron' do
+            y { 6 }
+          end.y.must_equal(6)
+        end
+
+        it 'sets the attribute to the value if a block is not given' do
+          Vedeu.interface 'iron' do
+            y 4
+          end.y.must_equal(4)
+        end
+      end
+
+    end # Geometry
+
+  end # DSL
+
+end # Vedeu
