48e72efd02c168f31a988f7cd616c9c24c19f287
Gavin Laking <gavinlaking@gmail.com>
2014-12-12 15:58:24 +0000

    Add DSL::Colour.

---
+++ lib/vedeu/dsl/colour.rb
@@ -0,0 +1,100 @@
+module Vedeu
+
+  module DSL
+
+    # Provides colour helpers for use in the {DSL::Interface}, {DSL::Line} and
+    # {DSL::Stream} classes.
+    #
+    # @api public
+    module Colour
+
+      # Define the background colour for an interface, line, or a stream. When
+      # called with a block, will create a new stream with the background colour
+      # specified; see {Vedeu::API::Stream} for directives which are valid
+      # within the block. When the block terminates, the background will return
+      # to that of the parent.
+      #
+      # @todo Reimplement block.
+      #
+      # @note The last defined background colour for a particular interface,
+      #   line or stream overrides previously defined entries in the same block.
+      #
+      # @param value [String]
+      # @param block [Proc]
+      #
+      # @example
+      #   interface 'my_interface' do
+      #     background '#0022ff' # /or/ (blue)
+      #     bgcolor    '#22ff00' # /or/ (blue is overridden to green)
+      #     bg         '#ff0022' #      (green is overridden to red)
+      #     ...
+      #
+      #     line do
+      #       background '#2200ff'
+      #       ...
+      #
+      #       stream do
+      #         background '#22ff00'
+      #         ...
+      #
+      #   background('#0022ff') { 'This will be blue.' }
+      #
+      # @raise [InvalidSyntax] When the value is not defined.
+      # @return [Hash]
+      def background(value = '')
+        set_colour({ background: value })
+      end
+      alias_method :bg,      :background
+      alias_method :bgcolor, :background
+
+      # @see Vedeu::DSL::Colour#background
+      def foreground(value = '')
+        set_colour({ foreground: value })
+      end
+      alias_method :fg,      :foreground
+      alias_method :fgcolor, :foreground
+
+      # Define either or both foreground and background colours for an
+      # interface, line or a stream. At least one attribute is required.
+      #
+      # @note Rejects invalid keys and empty/nil values. Also, the last defined
+      #   colour for a particular interface, line or stream overrides previously
+      #   defined entries in the same block.
+      #
+      # @param values [Hash]
+      #
+      # @example
+      #   interface 'my_interface' do
+      #     colour background: '#ff00ff', foreground: '#ffff00'
+      #     colour bgcolor:    '#ff00ff', fgcolor:    '#ffff00'
+      #     colour bg:         '#ff00ff', fg:         '#ffff00'
+      #     ...
+      #
+      #     line do
+      #       colour background: '#000000', foreground: '#ffffff'
+      #       ...
+      #
+      #       stream do
+      #         colour background: '#000000', foreground: '#ffffff'
+      #         ...
+      #
+      # @return [Hash]
+      def set_colour(values = {})
+        values.delete_if do |k, v|
+          valid_keys.include?(k) == false || v.nil? || v.empty?
+        end
+
+        self.colour.merge(values)
+      end
+
+      private
+
+      def valid_keys
+        [:background, :bgcolor, :bg, :foreground, :fgcolor, :fg]
+      end
+
+    end # Colour
+
+  end # DSL
+
+end # Vedeu
