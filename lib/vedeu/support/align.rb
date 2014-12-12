257dfde6cc154c0fbe71ac9b402a2bed2bcf1def
Gavin Laking <gavinlaking@gmail.com>
2014-12-12 16:01:06 +0000

    Add Align class to align strings left, centre, or right justified.

---
+++ lib/vedeu/support/align.rb
@@ -0,0 +1,86 @@
+module Vedeu
+
+  # Align a string (or object responding to `to_s`).
+  #
+  class Align
+
+    # @see Vedeu::DSL::Stream#align
+    def self.with(value = '', options = {})
+      new(value, options).aligned
+    end
+
+    # Returns a new instance of Align.
+    #
+    # @return [Align]
+    def initialize(value = '', options = {})
+      @value   = value
+      @options = defaults.merge(options)
+    end
+
+    # Aligns the value.
+    #
+    # @return [String]
+    def aligned
+      return string unless width
+
+      return truncated if truncate?
+
+      case anchor
+      when :align           then left
+      when :centre, :center then centre
+      when :right           then right
+      else left
+      end
+    end
+
+    private
+
+    attr_reader :value, :options
+
+    def anchor
+      options[:anchor]
+    end
+
+    def centre
+      string.center(width, pad)
+    end
+
+    def defaults
+      {
+        anchor: :left,
+        pad:    ' ',
+        width:  nil,
+      }
+    end
+
+    def left
+      string.ljust(width, pad)
+    end
+
+    def pad
+      options[:pad]
+    end
+
+    def right
+      string.rjust(width, pad)
+    end
+
+    def string
+      value.to_s
+    end
+
+    def truncate?
+      string.size > width
+    end
+
+    def truncated
+      string.slice(0, width)
+    end
+
+    def width
+      options[:width]
+    end
+
+  end # Alignment
+
+end # Vedeu
---
+++ test/lib/vedeu/support/align_test.rb
@@ -0,0 +1,76 @@
+require 'test_helper'
+
+module Vedeu
+
+  describe Align do
+    let(:value)   { 'Testing the Align class with various options' }
+    let(:options) {
+      {
+        anchor: anchor,
+        pad:    pad,
+        width:  width,
+      }
+    }
+    let(:anchor) { :left }
+    let(:pad)    { ' ' }
+    let(:width)  { nil }
+
+    describe '.with' do
+
+      context 'when a width is provided' do
+        context 'when value longer than the width' do
+          let(:width) { 23 }
+
+          it 'returns the value truncated' do
+            Align.with(value, options).must_equal('Testing the Align class')
+          end
+        end
+
+        context 'when value is shorter or equal to the width' do
+          let(:width) { 48 }
+
+          context 'and an anchor is not set' do
+            it 'returns the value left aligned' do
+              Align.with(value, options)
+                .must_equal('Testing the Align class with various options    ')
+            end
+          end
+
+          context 'and the anchor is set to :left' do
+            it 'returns the value left aligned' do
+              Align.with(value, options)
+                .must_equal('Testing the Align class with various options    ')
+            end
+          end
+
+          context 'and the anchor is set to centre' do
+            let(:anchor) { :centre }
+
+            it 'returns the value centre aligned' do
+              Align.with(value, options)
+                .must_equal('  Testing the Align class with various options  ')
+            end
+          end
+
+          context 'and the anchor is set to right' do
+            let(:anchor) { :right }
+
+            it 'returns the value right aligned' do
+              Align.with(value, options)
+                .must_equal('    Testing the Align class with various options')
+            end
+          end
+        end
+      end
+
+      context 'when a width is not provided' do
+        it 'returns the value as a string' do
+          Align.with(:some_value).must_equal('some_value')
+        end
+      end
+
+    end
+
+  end # Align
+
+end # Vedeu
