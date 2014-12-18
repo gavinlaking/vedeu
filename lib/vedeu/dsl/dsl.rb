609af4e3c9a543c33c9aae1d29dde64c750aac37
Gavin Laking <gavinlaking@gmail.com>
2014-12-18 19:10:48 +0000

    Remove instance variables.

--- lib/vedeu/dsl/dsl.rb
+++ lib/vedeu/dsl/dsl.rb
@@ -18,7 +18,7 @@ module Vedeu
 
   class Node
     attr_accessor :name, :parent
-    attr_reader :children
+    attr_reader   :children
 
     def initialize(name)
       @name     = name
@@ -26,23 +26,23 @@ module Vedeu
     end
 
     def add_child(node)
-      @children << node
+      children << node
 
       node.parent = self
     end
     alias :<< :add_child
 
     def remove_child(node)
-      @children.delete(node)
+      children.delete(node)
     end
 
     def [](index)
-      @children[index]
+      children[index]
     end
 
     def []=(index, node)
       replaced_child        = @children[index]
-      @children[index]      = node
+      children[index]       = node
       replaced_child.parent = nil
       node.parent           = self
     end
