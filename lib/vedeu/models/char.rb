7a9dba89fdc58e355d0ef7305c786a934197c84a
Gavin Laking <gavinlaking@gmail.com>
2014-12-30 00:33:46 +0000

    Refactor Char;
    - Add Position: independently place characters in the terminal.
    - Remove Presentation module: Inline behaviour.
    - Remove attributes hash: prefer individual attributes via #initialize.

--- lib/vedeu/models/char.rb
+++ lib/vedeu/models/char.rb
@@ -1,42 +1,97 @@
 module Vedeu
 
+  #
   # A Char represents a single character of the terminal. It is a container for
   # the a single character in a {Vedeu::Stream}.
   #
-  # @note The parent of a Char is a Line.
-  # @todo Colour and style is not being correctly reset. (GL 2014-10-19)
+  # Though a multi-character String can be passed as a value, only the first
+  # character is returned in the escape sequence.
   #
   # @api private
+  #
   class Char
 
-    include Presentation
-
-    attr_accessor :parent, :value
-    attr_reader :attributes
-    alias_method :data, :value
-
+    #
     # Returns a new instance of Char.
     #
-    # @param attributes [Hash]
+    # @param  value    [String]
+    # @param  parent   [Line]
+    # @param  colour   [Colour]
+    # @param  style    [Style]
+    # @param  position [Position]
     # @return [Char]
-    def initialize(attributes = {})
-      @attributes = defaults.merge(attributes)
-      @parent     = @attributes[:parent]
-      @value      = @attributes[:value]
+    #
+    def initialize(value, parent, colour, style, position)
+      @value    = value
+      @parent   = parent
+      @colour   = colour
+      @style    = style
+      @position = position
+    end
+
+    #
+    # @return [String] An escape sequence with value interpolated.
+    #
+    def to_s
+      render_position { render_colour { render_style { value } } }
     end
 
     private
 
-    # The default values for a new instance of Char.
-    #
-    # @return [Hash]
-    def defaults
-      {
-        colour: {},
-        parent: nil,
-        style:  [],
-        value:  '',
-      }
+    attr_reader :parent, :position
+
+    def colour
+      return '' unless @colour
+
+      @colour
+    end
+
+    def style
+      return '' unless @style
+
+      @style
+    end
+
+    def value
+      @value.nil? ? '' : @value[0]
+    end
+
+    def parent_colour
+      return '' unless parent
+
+      parent.colour
+    end
+
+    def parent_style
+      return '' unless parent
+
+      parent.style
+    end
+
+    def render_colour
+      [
+        colour,
+        yield,
+        parent_colour
+      ].join
+    end
+
+    def render_position
+      if position
+        position.to_s { yield }
+
+      else
+        yield
+
+      end
+    end
+
+    def render_style
+      [
+        style,
+        yield,
+        parent_style
+      ].join
     end
 
   end # Char
--- test/lib/vedeu/models/char_test.rb
+++ test/lib/vedeu/models/char_test.rb
@@ -4,45 +4,96 @@ module Vedeu
 
   describe Char do
 
-    let(:described) { Char.new(attributes) }
-    let(:parent) {
-      {
-        colour: {
-          foreground: '#00ff00',
-          background: '#ff00ff'
-        },
-        style: []
-      }
-    }
-    let(:attributes) {
-      {
-        colour: { foreground: '#ffff00', background: '#0000ff' },
-        parent: parent,
-        style:  [],
-        value:  value,
-      }
-    }
-    let(:value)   { 'a' }
+    let(:described)     { Char.new(value, parent, colour, style, position) }
+    let(:value)         { 'a' }
+    let(:parent)        { Line.new([], nil, parent_colour, parent_style) }
+    let(:colour)        { nil }
+    let(:style)         { nil }
+    let(:position)      { nil }
+    let(:parent_colour) { nil }
+    let(:parent_style)  { nil }
+
 
     describe '#initialize' do
       it { return_type_for(described, Char) }
-      it { assigns(described, '@attributes', attributes) }
+      it { assigns(described, '@colour', colour) }
       it { assigns(described, '@parent', parent) }
+      it { assigns(described, '@style', style) }
       it { assigns(described, '@value', value) }
+      it { assigns(described, '@position', position) }
     end
 
     describe '#to_s' do
-      it { return_type_for(described.to_s, String) }
+      subject { Char.new(value, parent, colour, style, position).to_s }
 
-      it 'returns the escape sequences and content' do
-        # - char colours and style as set in Stream#chars.
-        # - the value.
-        # - the colours and style of the line which the Char belongs to.
-        described.to_s.must_equal(
-          "\e[38;2;255;255;0m\e[48;2;0;0;255m" \
-          "a" \
-          "\e[38;2;0;255;0m\e[48;2;255;0;255m"
-        )
+      it { return_type_for(subject, String) }
+
+      context 'when a position is specified' do
+        let(:position) { Position.new(17, 2) }
+
+        it { return_value_for(subject, "\e[17;2Ha\e[17;2H") }
+      end
+
+      context 'when a position is not specified' do
+        let(:position) {}
+
+        it { return_value_for(subject, "a") }
+      end
+
+      context 'when a colour is specified' do
+        let(:colour) { Colour.new({ foreground: '#00ff00', background: '#005500' }) }
+
+        context 'when a parent colour is specified' do
+          let(:parent_colour) { Colour.new({ foreground: '#ff0000', background: '#550000' }) }
+
+          it { return_value_for(subject, "\e[38;2;0;255;0m\e[48;2;0;85;0ma\e[38;2;255;0;0m\e[48;2;85;0;0m") }
+        end
+
+        context 'when a parent colour is not specified' do
+          let(:parent_colour) {}
+
+          it { return_value_for(subject, "\e[38;2;0;255;0m\e[48;2;0;85;0ma") }
+        end
+      end
+
+      context 'when a colour is not specified' do
+        let(:colour) {}
+
+        it { return_value_for(subject, "a") }
+      end
+
+      context 'when a style is specified' do
+        let(:style) { Style.new('underline') }
+
+        context 'when a parent style is specified' do
+          let(:parent_style) { Style.new('bold') }
+
+          it { return_value_for(subject, "\e[4ma\e[1m") }
+        end
+
+        context 'when a parent style is not specified' do
+          let(:parent_style) {}
+
+          it { return_value_for(subject, "\e[4ma") }
+        end
+      end
+
+      context 'when a style is not specified' do
+        let(:style) {}
+
+        it { return_value_for(subject, "a") }
+      end
+
+      context 'when the value is nil' do
+        let(:value) {}
+
+        it { return_value_for(subject, "") }
+      end
+
+      context 'when the value is more than one character' do
+        let(:value) { 'multi' }
+
+        it { return_value_for(subject, "m") }
       end
     end
 
