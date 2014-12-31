6169b54ebdefe7e08ab274a6800bad5957826e76
Gavin Laking <gavinlaking@gmail.com>
2014-12-31 17:05:51 +0000

    Add DSL::Shared for shared methods.

---
+++ lib/vedeu/dsl/shared.rb
@@ -0,0 +1,42 @@
+module Vedeu
+
+  module DSL
+
+    module Shared
+
+      extend self
+
+      # Use the specified interface; useful for sharing attributes with other
+      # interfaces. Any public method of #{Vedeu::Interface} is available.
+      #
+      # @example
+      #   interface 'my_interface' do
+      #     delay use('my_other_interface').delay # use the delay of another
+      #     ...                                   # interface
+      #
+      #   interface 'my_interface' do
+      #     geometry do
+      #       width use('my_other_interface').width # use the width of another
+      #       ...                                   # interface
+      #
+      #   Vedeu.use('my_other_interface').width # can be used in your code to
+      #                                         # get this value
+      #
+      # @param value [String] The name of the interface you wish to use. Typically
+      #   used when defining interfaces to share geometry.
+      # @return [Vedeu::Interface]
+      def use(value)
+        if Vedeu::Interfaces.registered?(value) == false
+          fail ModelNotFound, "The properties of this interface (#{value}) " \
+                              "cannot be used, since the interface has not " \
+                              "been defined."
+        end
+
+        Vedeu::Interface.new(Vedeu::Interfaces.find(value))
+      end
+
+    end # Shared
+
+  end # DSL
+
+end # Vedeu
---
+++ test/lib/vedeu/dsl/shared_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+module Vedeu
+
+  module DSL
+
+    describe Shared do
+
+      describe '.use' do
+        it { skip }
+
+        # before { Vedeu::Interfaces.reset }
+
+        # context 'when an interface has not been defined' do
+        #   it { proc { Vedeu.use('unknown') }.must_raise(ModelNotFound) }
+        # end
+
+        # it 'returns an instance of the named interface' do
+        #   Vedeu.interface('aluminium')
+
+        #   Vedeu.use('aluminium').must_be_instance_of(Vedeu::Interface)
+        # end
+      end
+
+    end # Shared
+
+  end # DSL
+
+end # Vedeu
