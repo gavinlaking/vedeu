49d00db5be3c423dabefa9d5c9d6c5865d362ad9
Gavin Laking <gavinlaking@gmail.com>
2015-01-01 21:34:06 +0000

    Move API.log into Log.

--- lib/vedeu/support/log.rb
+++ lib/vedeu/support/log.rb
@@ -1,3 +1,5 @@
+require 'vedeu/configuration/configuration'
+
 module Vedeu
 
   # Provides the ability to log anything to the Vedeu log file.
@@ -7,9 +9,24 @@ module Vedeu
 
     class << self
 
+      # Write a message to the Vedeu log file.
+      #
+      # @param message [String] The message you wish to emit to the log
+      #   file, useful for debugging.
+      # @param force   [Boolean] When evaluates to true will attempt to
+      #   write to the log file regardless of the Configuration setting.
+      #
+      # @example
+      #   Vedeu.log('A useful debugging message: Error!')
+      #
+      # @return [TrueClass]
+      def log(message, force = false)
+        logger.debug(message) if enabled? || force
+      end
+
       # @return [TrueClass]
       def logger
-        Logger.new(Configuration.log).tap do |log|
+        Logger.new(log_file).tap do |log|
           log.formatter = proc do |_, time, _, message|
             [timestamp(time.utc.iso8601), message, "\n"].join
           end
@@ -17,6 +34,16 @@ module Vedeu
       end
 
       private
+
+      # @return [Boolean]
+      def enabled?
+        Vedeu::Configuration.debug?
+      end
+
+      # @return [String]
+      def log_file
+        Vedeu::Configuration.log
+      end
 
       # Returns a formatted (red, underlined) UTC timestamp,
       # eg. 2014-10-24T12:34:56Z
