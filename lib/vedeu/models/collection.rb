1d4ab71a5f4931bc9c28fc12a42c7d9a80c7eb74
Gavin Laking <gavinlaking@gmail.com>
2015-01-03 22:17:18 +0000

    Remove duplicate code.

--- lib/vedeu/models/collection.rb
+++ lib/vedeu/models/collection.rb
@@ -2,13 +2,6 @@ module Vedeu
 
   module Model
 
-    attr_reader :repository
-
-    # @return [void] The model instance stored in the repository.
-    def store
-      repository.store(self)
-    end
-
     class Collection
 
       include Enumerable
