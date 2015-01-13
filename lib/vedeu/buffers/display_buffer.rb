7cf4642b63fff1a8f231ff8cefa1732b44c7e94b
Gavin Laking <gavinlaking@gmail.com>
2015-01-13 19:03:48 +0000

    Add more documentation.

--- lib/vedeu/buffers/display_buffer.rb
+++ lib/vedeu/buffers/display_buffer.rb
@@ -2,8 +2,15 @@ require 'vedeu/support/repository'
 
 module Vedeu
 
+  # This module is included in Vedeu::Interface to provide means to store client
+  # application views created with {Vedeu.renders} or {Vedeu.views}.
+  #
   module DisplayBuffer
 
+    # Store the view and immediate refresh it; causing to be pushed to the
+    # Terminal.
+    #
+    # @return [Interface]
     def store_immediate
       store_deferred
 
@@ -12,6 +19,10 @@ module Vedeu
       self
     end
 
+    # Store the view. This view will be shown next time the refresh event is
+    # triggered for the interface.
+    #
+    # @return [Interface]
     def store_deferred
       return store_new_buffer unless Vedeu.buffers_repository.registered?(name)
 
@@ -24,6 +35,10 @@ module Vedeu
 
     private
 
+    # Registers a set of buffers for the interface.
+    #
+    # @see Vedeu::Buffer
+    # @return [Interface]
     def store_new_buffer
       store_new_interface
 
@@ -36,8 +51,10 @@ module Vedeu
       self
     end
 
+    # Registers the interface.
+    #
+    # @return [Interface]
     def store_new_interface
-
       unless Vedeu.interfaces_repository.registered?(name)
         Vedeu.log("Registering interface: '#{name}'")
 
