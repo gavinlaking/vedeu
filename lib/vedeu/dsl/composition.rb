506e503c8b563eefd3eb540f1d525a8673bf1aa1
Gavin Laking <gavinlaking@gmail.com>
2015-01-23 18:58:29 +0000

    Remove Vedeu::Common from here as no longer used.

--- lib/vedeu/dsl/composition.rb
+++ lib/vedeu/dsl/composition.rb
@@ -1,5 +1,3 @@
-require 'vedeu/support/common'
-
 module Vedeu
 
   module DSL
@@ -7,8 +5,6 @@ module Vedeu
     # DSL for creating collections of interfaces.
     #
     class Composition
-
-      include Vedeu::Common
 
       # Returns an instance of DSL::Composition.
       #
@@ -22,8 +18,8 @@ module Vedeu
       # A view is just an Interface object.
       #
       # @todo More documentation required.
-      # @param interface_name [String] The name of the interface you are
-      #   targetting for this view.
+      # @param name [String] The name of the interface you are targetting for
+      #   this view.
       # @param block [Proc] The directives you wish to send to this interface.
       #
       # @example
@@ -32,15 +28,14 @@ module Vedeu
       #
       # @raise [InvalidSyntax] The required block was not given.
       # @return [Vedeu::Model::Collection<Vedeu::Interface>]
-      def view(interface_name = '', &block)
+      def view(name = '', &block)
         unless block_given?
           fail InvalidSyntax, "'#{__callee__}' requires a block."
         end
 
-        interface = Vedeu::Interface
-          .build({ name: interface_name, parent: model }, &block)
+        view = Vedeu::Interface.build({ name: name, parent: model }, &block)
 
-        model.interfaces.add(interface)
+        model.interfaces.add(view)
       end
 
       private
