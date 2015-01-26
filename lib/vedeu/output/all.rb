5717155bf6eee776d8381537cc7196c47f1327f7
Gavin Laking <gavinlaking@gmail.com>
2015-01-26 17:50:35 +0000

    Remove placeholder development files.

--- lib/vedeu/output/all.rb
+++ lib/vedeu/output/all.rb
@@ -1,7 +1,6 @@
 require 'vedeu/output/border'
 require 'vedeu/output/compositor'
 require 'vedeu/output/output'
-require 'vedeu/output/pipe'
 require 'vedeu/output/viewport'
 require 'vedeu/output/writer'
 
--- lib/vedeu/output/consoles/all.rb
+++
@@ -1,4 +0,0 @@
-require 'vedeu/output/consoles/file'
-require 'vedeu/output/consoles/html'
-require 'vedeu/output/consoles/null'
-require 'vedeu/output/consoles/screen'
