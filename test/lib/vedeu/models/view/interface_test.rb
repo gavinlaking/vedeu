aeaed4f7b880f85d6e94072fed48064a7a1a4bfd
Gavin Laking <gavinlaking@gmail.com>
2015-01-30 18:22:35 +0000

    Add more tests.

--- test/lib/vedeu/models/view/interface_test.rb
+++ test/lib/vedeu/models/view/interface_test.rb
@@ -4,133 +4,62 @@ module Vedeu
 
   describe Interface do
 
-    let(:described)  { Vedeu::Interface }
-    let(:instance)   { described.new(attributes) }
-    let(:attributes) { {} }
+    let(:described) { Vedeu::Interface }
+    let(:instance)  { described.new(_name, lines, parent, colour, style) }
+    let(:_name)     { 'hydrogen' }
+    let(:lines)     { [] }
+    let(:parent)    {}
+    let(:colour)    {}
+    let(:style)     {}
 
-    # let(:attributes) {
-    #   {
-    #     colour: {
-    #       background: '#000000',
-    #       foreground: '#ff0000',
-    #     },
-    #     group: 'my_group',
-    #     name: 'francium',
-    #     geometry: {
-    #       x: 5,
-    #       y: 3,
-    #       width: 10,
-    #       height: 15,
-    #     }
-    #   }
-    # }
+    describe '#initialize' do
+      subject { instance }
 
-    # # before do
-    # #   Interfaces.reset
-    # #   Vedeu.interface('#initialize') do
-    # #     group 'my_group'
-    # #     colour foreground: '#ff0000', background: '#000000'
-    # #     geometry do
-    # #       y 3
-    # #       x 5
-    # #       width 10
-    # #       height 15
-    # #     end
-    # #   end
-    # # end
-
-    # describe '#initialize' do
-    #   it { described.must_be_instance_of(Interface) }
-    #   it { subject.instance_variable_get('@attributes').must_equal({
-    #       border:   {},
-    #       colour:   {
-    #         background: '#000000',
-    #         foreground: '#ff0000',
-    #       },
-    #       delay:    0.0,
-    #       geometry: {
-    #         x:      5,
-    #         y:      3,
-    #         width:  10,
-    #         height: 15,
-    #       },
-    #       group:    'my_group',
-    #       lines:    [],
-    #       name:     'francium',
-    #       parent:   nil,
-    #       style:    ''
-    #     })
-    #   }
-    #   it { described.instance_variable_get('@delay').must_equal(0.0) }
-    #   it { described.instance_variable_get('@group').must_equal('my_group') }
-    #   it { described.instance_variable_get('@name').must_equal('francium') }
-    #   it { described.instance_variable_get('@parent').must_equal(nil) }
-    #   it { described.instance_variable_get('@repository').must_equal(Vedeu.interfaces) }
-
-    #   context 'with default attributes' do
-    #     let(:attributes) { {} }
-
-    #     it { subject.instance_variable_get('@attributes').must_equal({
-    #         border:   {},
-    #         colour:   {},
-    #         delay:    0.0,
-    #         geometry: {},
-    #         group:    '',
-    #         lines:    [],
-    #         name:     '',
-    #         parent:   nil,
-    #         style:    ''
-    #       })
-    #     }
-    #   end
-    # end
-
-    # describe '#attributes' do
-    #   it 'returns the value' do
-    #     described.attributes.must_equal(
-    #       {
-    #         border:     {},
-    #         colour:     {
-    #           foreground: '#ff0000',
-    #           background: '#000000'
-    #         },
-    #         delay:      0.0,
-    #         geometry:   {
-    #           y:          3,
-    #           x:          5,
-    #           width:      10,
-    #           height:     15,
-    #         },
-    #         group:      'my_group',
-    #         lines:      [],
-    #         name:       'francium',
-    #         parent:     nil,
-    #         style:      '',
-    #       }
-    #     )
-    #   end
-    # end
+      it { subject.instance_variable_get('@name').must_equal(_name) }
+      #it { subject.instance_variable_get('@lines').must_equal(lines) }
+      it { subject.instance_variable_get('@parent').must_equal(parent) }
+      it { subject.instance_variable_get('@colour').must_equal(colour) }
+      it { subject.instance_variable_get('@style').must_equal(style) }
+      it { subject.instance_variable_get('@border').must_equal(nil) }
+      it { subject.instance_variable_get('@delay').must_equal(0.0) }
+      it { subject.instance_variable_get('@geometry').must_equal(nil) }
+      it { subject.instance_variable_get('@group').must_equal('') }
+      it { subject.instance_variable_get('@repository').must_equal(Vedeu.interfaces) }
+    end
 
     describe '#cursor' do
       subject { instance.cursor }
 
-      it { skip }
+      it { subject.must_be_instance_of(Vedeu::Cursor) }
     end
 
     describe '#lines' do
       it { instance.lines.must_be_instance_of(Vedeu::Model::Lines) }
     end
 
+    # TODO: when the interface does not have any geometry defined?
     describe '#render' do
       subject { instance.render }
 
-      it { skip }
+      context 'when the interface has a border' do
+        let(:border) { Vedeu::Border.new(instance, { enabled: true }) }
+
+        before { instance.stubs(:border).returns(border) }
+
+        it { skip }
+      end
+
+      context 'when the interface does not have a border' do
+        it { skip }
+      end
     end
 
     describe '#store' do
       subject { instance.store }
 
       context 'when the interface has no name' do
+        let(:_name) {}
+
         it { proc { subject }.must_raise(MissingRequired) }
       end
 
@@ -157,6 +86,7 @@ module Vedeu
       end
     end
 
+    # TODO: when the interface does not have any geometry defined?
     describe '#viewport' do
       subject { instance.viewport }
 
