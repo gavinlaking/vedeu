c5f0d14f10765cdd4599b9dd96ec2ad55fe84a7d
Gavin Laking <gavinlaking@gmail.com>
2015-02-07 20:55:07 +0000

    Move integration/dsl/borders_test into dsl/components/border_test.

--- test/lib/vedeu/dsl/components/border_test.rb
+++ test/lib/vedeu/dsl/components/border_test.rb
@@ -9,16 +9,30 @@ module Vedeu
       let(:described)  { Vedeu::DSL::Border }
       let(:instance)   { described.new(model) }
       let(:model)      { Vedeu::Border.new(interface, attributes) }
+      let(:client)     {}
       let(:interface)  { mock('Interface') }
       let(:attributes) { {} }
       let(:boolean)    { true }
+
+      before do
+        Vedeu.interfaces.reset
+        Vedeu.interface 'borders' do
+          geometry do
+            height 3
+            width  3
+          end
+          lines do
+            line ''
+          end
+        end
+      end
 
       describe '#initialize' do
         subject { instance }
 
         it { subject.must_be_instance_of(Vedeu::DSL::Border) }
-
         it { subject.instance_variable_get('@model').must_equal(model) }
+        it { subject.instance_variable_get('@client').must_equal(client) }
       end
 
       describe '#bottom_left' do
@@ -28,6 +42,25 @@ module Vedeu
 
         it { subject.must_be_instance_of(String) }
         it { subject.must_equal('C') }
+
+        context 'DSL #bottom_left' do
+          subject {
+            Vedeu.interface 'borders' do
+              border do
+                bottom_left '<'
+              end
+            end
+          }
+
+          it { subject.must_be_instance_of(Vedeu::Interface) }
+
+          it 'allows the use of bottom_left within border' do
+            subject.border.to_s.must_equal(
+              "\e(0l\e(B\e(0q\e(B\e(0k\e(B\n" \
+              "\e(0<\e(B\e(0q\e(B\e(0j\e(B"
+            )
+          end
+        end
       end
 
       describe '#bottom_right' do
@@ -37,6 +70,25 @@ module Vedeu
 
         it { subject.must_be_instance_of(String) }
         it { subject.must_equal('D') }
+
+        context 'DSL #bottom_right' do
+          subject {
+            Vedeu.interface 'borders' do
+              border do
+                bottom_right '>'
+              end
+            end
+          }
+
+          it { subject.must_be_instance_of(Vedeu::Interface) }
+
+          it 'allows the use of bottom_right within border' do
+            subject.border.to_s.must_equal(
+              "\e(0l\e(B\e(0q\e(B\e(0k\e(B\n" \
+              "\e(0m\e(B\e(0q\e(B\e(0>\e(B"
+            )
+          end
+        end
       end
 
       describe '#horizontal' do
@@ -46,6 +98,25 @@ module Vedeu
 
         it { subject.must_be_instance_of(String) }
         it { subject.must_equal('H') }
+
+        context 'DSL #horizontal' do
+          subject {
+            Vedeu.interface 'borders' do
+              border do
+                horizontal '~'
+              end
+            end
+          }
+
+          it { subject.must_be_instance_of(Vedeu::Interface) }
+
+          it 'allows the use of horizontal within border' do
+            subject.border.to_s.must_equal(
+              "\e(0l\e(B\e(0~\e(B\e(0k\e(B\n" \
+              "\e(0m\e(B\e(0~\e(B\e(0j\e(B"
+            )
+          end
+        end
       end
 
       describe '#show_bottom' do
@@ -57,6 +128,25 @@ module Vedeu
           let(:boolean) { false }
 
           it { subject.must_be_instance_of(FalseClass) }
+        end
+
+        context 'DSL #show_bottom' do
+          subject {
+            Vedeu.interface 'borders' do
+              border do
+                show_bottom ''
+              end
+            end
+          }
+
+          it { subject.must_be_instance_of(Vedeu::Interface) }
+
+          it 'allows the use of show_bottom within border' do
+            subject.border.to_s.must_equal(
+              "\e(0l\e(B\e(0q\e(B\e(0k\e(B\n" \
+              "\e(0m\e(B\e(0q\e(B\e(0j\e(B"
+            )
+          end
         end
       end
 
@@ -70,6 +160,25 @@ module Vedeu
 
           it { subject.must_be_instance_of(FalseClass) }
         end
+
+        context 'DSL #show_left' do
+          subject {
+            Vedeu.interface 'borders' do
+              border do
+                show_left ''
+              end
+            end
+          }
+
+          it { subject.must_be_instance_of(Vedeu::Interface) }
+
+          it 'allows the use of show_left within border' do
+            subject.border.to_s.must_equal(
+              "\e(0l\e(B\e(0q\e(B\e(0k\e(B\n" \
+              "\e(0m\e(B\e(0q\e(B\e(0j\e(B"
+            )
+          end
+        end
       end
 
       describe '#show_right' do
@@ -81,6 +190,25 @@ module Vedeu
           let(:boolean) { false }
 
           it { subject.must_be_instance_of(FalseClass) }
+        end
+
+        context 'DSL #show_right' do
+          subject {
+            Vedeu.interface 'borders' do
+              border do
+                show_right ''
+              end
+            end
+          }
+
+          it { subject.must_be_instance_of(Vedeu::Interface) }
+
+          it 'allows the use of show_right within border' do
+            subject.border.to_s.must_equal(
+              "\e(0l\e(B\e(0q\e(B\e(0k\e(B\n" \
+              "\e(0m\e(B\e(0q\e(B\e(0j\e(B"
+            )
+          end
         end
       end
 
@@ -94,6 +222,25 @@ module Vedeu
 
           it { subject.must_be_instance_of(FalseClass) }
         end
+
+        context 'DSL #show_top' do
+          subject {
+            Vedeu.interface 'borders' do
+              border do
+                show_top ''
+              end
+            end
+          }
+
+          it { subject.must_be_instance_of(Vedeu::Interface) }
+
+          it 'allows the use of show_top within border' do
+            subject.border.to_s.must_equal(
+              "\e(0l\e(B\e(0q\e(B\e(0k\e(B\n" \
+              "\e(0m\e(B\e(0q\e(B\e(0j\e(B"
+            )
+          end
+        end
       end
 
       describe '#top_left' do
@@ -103,6 +250,25 @@ module Vedeu
 
         it { subject.must_be_instance_of(String) }
         it { subject.must_equal('A') }
+
+        context 'DSL #top_left' do
+          subject {
+            Vedeu.interface 'borders' do
+              border do
+                top_left '{'
+              end
+            end
+          }
+
+          it { subject.must_be_instance_of(Vedeu::Interface) }
+
+          it 'allows the use of top_left within border' do
+            subject.border.to_s.must_equal(
+              "\e(0{\e(B\e(0q\e(B\e(0k\e(B\n" \
+              "\e(0m\e(B\e(0q\e(B\e(0j\e(B"
+            )
+          end
+        end
       end
 
       describe '#top_right' do
@@ -112,6 +278,25 @@ module Vedeu
 
         it { subject.must_be_instance_of(String) }
         it { subject.must_equal('B') }
+
+        context 'DSL #top_right' do
+          subject {
+            Vedeu.interface 'borders' do
+              border do
+                top_right '}'
+              end
+            end
+          }
+
+          it { subject.must_be_instance_of(Vedeu::Interface) }
+
+          it 'allows the use of top_right within border' do
+            subject.border.to_s.must_equal(
+              "\e(0l\e(B\e(0q\e(B\e(0}\e(B\n" \
+              "\e(0m\e(B\e(0q\e(B\e(0j\e(B"
+            )
+          end
+        end
       end
 
       describe '#vertical' do
@@ -121,6 +306,25 @@ module Vedeu
 
         it { subject.must_be_instance_of(String) }
         it { subject.must_equal('V') }
+
+        context 'DSL #vertical' do
+          subject {
+            Vedeu.interface 'borders' do
+              border do
+                vertical ':'
+              end
+            end
+          }
+
+          it { subject.must_be_instance_of(Vedeu::Interface) }
+
+          it 'allows the use of vertical within border' do
+            subject.border.to_s.must_equal(
+              "\e(0l\e(B\e(0q\e(B\e(0k\e(B\n" \
+              "\e(0m\e(B\e(0q\e(B\e(0j\e(B"
+            )
+          end
+        end
       end
 
     end # Border
