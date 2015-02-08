eeaeca92dcf9ed2f1aad95a5cb77bba5645749e0
Gavin Laking <gavinlaking@gmail.com>
2015-02-08 12:02:13 +0000

    Remove commented out test.

--- test/lib/vedeu/dsl/shared/text_test.rb
+++ test/lib/vedeu/dsl/shared/text_test.rb
@@ -6,18 +6,7 @@ module Vedeu
 
     describe Text do
 
-      # describe '#align' do
-      #   let(:value)   { 'silicon' }
-      #   let(:options) { {} }
 
-      #   subject { described.align(value, options) }
-
-      #   it { subject.must_be_instance_of(String) }
-
-      #   it 'aligns the value according to the options provided' do
-      #     subject.must_equal('siliconsilicon')
-      #   end
-      # end
 
     end # Text
 
