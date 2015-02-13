e9e51456305dfcc2babb52a5033f6ae6f85e27dc
Gavin Laking <gavinlaking@gmail.com>
2015-02-13 21:01:30 +0000

    Move definition of an interface's border to the view;
    At the moment, due to the way that interfaces and views are constructed, the definition of a border on an interface will not show on the corresponding view. The temporary workaround is to define the border on a per view basis.
    
    - Also; add more documentation for borders.

--- lib/vedeu/dsl/components/border.rb
+++ lib/vedeu/dsl/components/border.rb
@@ -9,6 +9,13 @@ module Vedeu
     # Allows customisation for the border's sides and corners; a custom
     # foreground and background, style, and whether a particular side should be
     # drawn or not.
+    #
+    # @example
+    #   Vedeu.renders do
+    #     view 'border_demo' do
+    #       border do
+    #         # ...
+    #
     class Border
 
       include Vedeu::DSL
@@ -23,6 +30,16 @@ module Vedeu
         @client = client
       end
 
+      # Set the character to be used to draw the bottom left corner of the
+      # border.
+      #
+      # @example
+      #   Vedeu.renders do
+      #     view 'border_demo' do
+      #       border do
+      #         bottom_left '+'
+      #         # ...
+      #
       # @param char [String] Character to be used as the bottom left border
       #   character.
       # @return [String]
@@ -30,6 +47,16 @@ module Vedeu
         model.attributes[:bottom_left] = char
       end
 
+      # Set the character to be used to draw the bottom right corner of the
+      # border.
+      #
+      # @example
+      #   Vedeu.renders do
+      #     view 'border_demo' do
+      #       border do
+      #         bottom_right '+'
+      #         # ...
+      #
       # @param char [String] Character to be used as the bottom right border
       #   character.
       # @return [String]
@@ -37,6 +64,15 @@ module Vedeu
         model.attributes[:bottom_right] = char
       end
 
+      # Set the character to be used to draw a horizontal part of the border.
+      #
+      # @example
+      #   Vedeu.renders do
+      #     view 'border_demo' do
+      #       border do
+      #         horizontal '-'
+      #         # ...
+      #
       # @param char [String] Character to be used as the horizontal border
       #   character.
       # @return [String]
@@ -44,6 +80,15 @@ module Vedeu
         model.attributes[:horizontal] = char
       end
 
+      # Enable/disable the bottom border.
+      #
+      # @example
+      #   Vedeu.renders do
+      #     view 'border_demo' do
+      #       border do
+      #         show_bottom false
+      #         # ...
+      #
       # @param boolean [Boolean] All values evaluate as true except nil and
       #   false.
       # @return [Boolean]
@@ -51,6 +96,15 @@ module Vedeu
         model.attributes[:show_bottom] = !!boolean
       end
 
+      # Enable/disable the left border.
+      #
+      # @example
+      #   Vedeu.renders do
+      #     view 'border_demo' do
+      #       border do
+      #         show_left false
+      #         # ...
+      #
       # @param boolean [Boolean] All values evaluate as true except nil and
       #   false.
       # @return [Boolean]
@@ -58,6 +112,15 @@ module Vedeu
         model.attributes[:show_left] = !!boolean
       end
 
+      # Enable/disable the right border.
+      #
+      # @example
+      #   Vedeu.renders do
+      #     view 'border_demo' do
+      #       border do
+      #         show_right false
+      #         # ...
+      #
       # @param boolean [Boolean] All values evaluate as true except nil and
       #   false.
       # @return [Boolean]
@@ -65,6 +128,15 @@ module Vedeu
         model.attributes[:show_right] = !!boolean
       end
 
+      # Enable/disable the top border.
+      #
+      # @example
+      #   Vedeu.renders do
+      #     view 'border_demo' do
+      #       border do
+      #         show_top false
+      #         # ...
+      #
       # @param boolean [Boolean] All values evaluate as true except nil and
       #   false.
       # @return [Boolean]
@@ -72,6 +144,15 @@ module Vedeu
         model.attributes[:show_top] = !!boolean
       end
 
+      # Set the character to be used to draw the top left corner of the border.
+      #
+      # @example
+      #   Vedeu.renders do
+      #     view 'border_demo' do
+      #       border do
+      #         top_left '+'
+      #         # ...
+      #
       # @param char [String] Character to be used as the top left border
       #   character.
       # @return [String]
@@ -79,6 +160,15 @@ module Vedeu
         model.attributes[:top_left] = char
       end
 
+      # Set the character to be used to draw the top right corner of the border.
+      #
+      # @example
+      #   Vedeu.renders do
+      #     view 'border_demo' do
+      #       border do
+      #         top_right '+'
+      #         # ...
+      #
       # @param char [String] Character to be used as the top right border
       #   character.
       # @return [String]
@@ -86,6 +176,15 @@ module Vedeu
         model.attributes[:top_right] = char
       end
 
+      # Set the character to be used to draw a vertical part of the border.
+      #
+      # @example
+      #   Vedeu.renders do
+      #     view 'border_demo' do
+      #       border do
+      #         vertical '|'
+      #         # ...
+      #
       # @param char [String] Character to be used as the vertical border
       #   character.
       # @return [String]
