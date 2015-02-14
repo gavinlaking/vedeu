f8d4a307a173f0aab4c1b43db305d5dabb6d2e52
Gavin Laking <gavinlaking@gmail.com>
2015-02-14 22:55:38 +0000

    Move Geometries and Menus into repositories/all.

--- lib/vedeu/repositories/all.rb
+++ lib/vedeu/repositories/all.rb
@@ -1,6 +1,40 @@
 require 'vedeu/repositories/repository'
-require 'vedeu/repositories/geometries'
-require 'vedeu/repositories/menus'
 
 module Vedeu
+
+  # Allows the storing of interface/view borders independent of the interface
+  # instance.
+  #
+  class Borders < Repository
+
+    # @return [Vedeu::Borders]
+    def self.borders
+      @_borders ||= Vedeu::Borders.new(Vedeu::Border)
+    end
+
+  end # Borders
+
+  # Allows the storing of interface/view geometry independent of the interface
+  # instance.
+  #
+  class Geometries < Repository
+
+    # @return [Vedeu::Geometries]
+    def self.geometries
+      @_geometries ||= Vedeu::Geometries.new(Vedeu::Geometry)
+    end
+
+  end # Geometries
+
+  # Allows the storing of menus by name.
+  #
+  class Menus < Repository
+
+    # @return [Vedeu::Menus]
+    def self.menus
+      @_menus ||= Vedeu::Menus.new(Vedeu::Menu)
+    end
+
+  end # Menus
+
 end # Vedeu
