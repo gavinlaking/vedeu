47954d4ce9319c012bda2ba8bda2b326fac2360b
Gavin Laking <gavinlaking@gmail.com>
2015-03-01 20:21:54 +0000

    Add MonoLogger back in;
    Resizing the terminal with debug! enabled, may yield an error:
    
    'log writing failed. can't be called from trap context'
    
    Using MonoLogger by Steve Klabnik prevents this.

--- lib/vedeu/support/log.rb
+++ lib/vedeu/support/log.rb
@@ -2,6 +2,92 @@ require 'vedeu/configuration/configuration'
 
 module Vedeu
 
+  # Allows the creation of a lock-less log device.
+  #
+  # @api private
+  class MonoLogger < Logger
+
+    # Create a trappable Logger instance.
+    #
+    # @param logdev [String|IO] The filename (String) or IO object (typically
+    #   STDOUT, STDERR or an open file).
+    # @param shift_age [] Number of old log files to keep, or frequency of
+    #   rotation (daily, weekly, monthly).
+    # @param shift_size [] Maximum log file size (only applies when shift_age
+    #   is a number).
+    #
+    # @example
+    #   Logger.new(name, shift_age = 7, shift_size = 1048576)
+    #   Logger.new(name, shift_age = 'weekly')
+    #
+    def initialize(logdev, shift_age=nil, shift_size=nil)
+      @level = DEBUG
+      @default_formatter = Formatter.new
+      @formatter = nil
+      @logdev = nil
+      if logdev
+        @logdev = LocklessLogDevice.new(logdev)
+      end
+    end
+
+    # Ensures we can always write to the log file by creating a lock-less
+    # log device.
+    class LocklessLogDevice < LogDevice
+
+      # @param log []
+      # @return []
+      def initialize(log = nil)
+        @dev = nil
+        @filename = nil
+        @shift_age = nil
+        @shift_size = nil
+
+        if log.respond_to?(:write) && log.respond_to?(:close)
+          @dev = log
+
+        else
+          @dev = open_logfile(log)
+          @dev.sync = true
+          @filename = log
+
+        end
+      end
+
+      # @param message []
+      # @return []
+      def write(message)
+        @dev.write(message)
+
+      rescue Exception => ignored
+        warn("log writing failed. #{ignored}")
+
+      end
+
+      # @return []
+      def close
+        @dev.close rescue nil
+      end
+
+      private
+
+      # @param filename []
+      # @return []
+      def open_logfile(filename)
+        if (FileTest.exist?(filename))
+          open(filename, (File::WRONLY | File::APPEND))
+
+        else
+          logdev = open(filename, (File::WRONLY | File::APPEND | File::CREAT))
+          logdev.sync = true
+          logdev
+
+        end
+      end
+
+    end # LocklessLogDevice
+
+  end # MonoLogger
+
   # Provides the ability to log anything to the Vedeu log file.
   #
   # @api private
@@ -29,7 +115,7 @@ module Vedeu
 
       # @return [TrueClass]
       def logger
-        Logger.new(log_file).tap do |log|
+        MonoLogger.new(log_file).tap do |log|
           log.formatter = proc do |_, time, _, message|
             formatted_message(message, time)
           end
