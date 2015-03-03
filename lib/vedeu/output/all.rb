a7a4022da39e2059863f6743ac883453e63a245b
Gavin Laking <gavinlaking@gmail.com>
2015-03-03 19:30:59 +0000

    This code is currently unused, but I want to use it in the future.

--- lib/vedeu/output/all.rb
+++ lib/vedeu/output/all.rb
@@ -24,7 +24,6 @@ require 'vedeu/output/output'
 require 'vedeu/output/viewport'
 require 'vedeu/output/virtual_terminal'
 require 'vedeu/output/wordwrap'
-require 'vedeu/output/writer'
 
 module Vedeu
 end # Vedeu
--- lib/vedeu/support/all.rb
+++ lib/vedeu/support/all.rb
@@ -1,14 +1,10 @@
 require 'vedeu/support/coercions'
 require 'vedeu/support/common'
-require 'vedeu/support/console'
-require 'vedeu/support/log'
-require 'vedeu/support/read'
 require 'vedeu/support/sentence'
 require 'vedeu/support/template'
 require 'vedeu/support/terminal'
 require 'vedeu/support/trace'
 require 'vedeu/support/visible'
-require 'vedeu/support/write'
 
 module Vedeu
 end # Vedeu
