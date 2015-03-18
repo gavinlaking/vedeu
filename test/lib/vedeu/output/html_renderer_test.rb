4c426148bc1cbb7a50b8459adb8d51fb163a9b60
Gavin Laking <gavinlaking@gmail.com>
2015-03-18 22:19:39 +0000

    Add more tests/placeholder tests.

--- test/lib/vedeu/output/html_renderer_test.rb
+++ test/lib/vedeu/output/html_renderer_test.rb
@@ -37,6 +37,35 @@ module Vedeu
       end
     end
 
+    describe '#html_body' do
+      subject { instance.html_body }
+
+      it { subject.must_be_instance_of(String) }
+
+      it { subject.must_equal('') }
+
+      context 'when there is output' do
+        let(:output) {
+          [
+            [
+              Vedeu::Char.new({ value: 't' }),
+              Vedeu::Char.new({ value: 'e' }),
+              Vedeu::Char.new({ value: 's' }),
+              Vedeu::Char.new({ value: 't' }),
+            ]
+          ]
+        }
+
+        it { subject.must_equal(
+          "<tr>\n" \
+          "<td style='background:#000;color:#222;border:1px #000 solid;'>t</td>\n" \
+          "<td style='background:#000;color:#222;border:1px #000 solid;'>e</td>\n" \
+          "<td style='background:#000;color:#222;border:1px #000 solid;'>s</td>\n" \
+          "<td style='background:#000;color:#222;border:1px #000 solid;'>t</td>\n" \
+          "</tr>\n") }
+      end
+    end
+
   end # HTMLRenderer
 
 end # Vedeu
