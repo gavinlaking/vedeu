09e50343a45a5f69cf8e9c565741b0470baaec2f
Gavin Laking <gavinlaking@gmail.com>
2015-05-24 02:21:38 +0100

    Measure the duration of the refreshing action and output as debugging to log.

--- lib/vedeu/output/refresh.rb
+++ lib/vedeu/output/refresh.rb
@@ -10,7 +10,13 @@ module Vedeu
     #
     # @return [Array]
     def all
-      Vedeu.interfaces.registered.each { |name| by_name(name) }
+      message = "Refreshing all interfaces"
+
+      Vedeu.log(type: :info, message: message)
+
+      timer(message) do
+        Vedeu.interfaces.registered.each { |name| by_name(name) }
+      end
     end
 
     # Refresh the interface which is currently focussed.
@@ -26,9 +32,13 @@ module Vedeu
     # @return [Array|ModelNotFound] A collection of the names of interfaces
     #   refreshed, or an exception if the group was not found.
     def by_group(group_name)
-      Vedeu.log(type: :info, message: "Refreshing group: '#{group_name}'")
+      message = "Refreshing group: '#{group_name}'"
 
-      Vedeu.groups.find!(group_name).members.each { |name| by_name(name) }
+      Vedeu.log(type: :info, message: message)
+
+      timer(message) do
+        Vedeu.groups.find!(group_name).members.each { |name| by_name(name) }
+      end
     end
 
     # Refresh an interface by name.
@@ -37,9 +47,27 @@ module Vedeu
     #   named buffer.
     # @return [Array|ModelNotFound]
     def by_name(name)
-      Vedeu.log(type: :info, message: "Refreshing interface: '#{name}'")
+      message = "Refreshing interface: '#{name}'"
 
-      Vedeu.buffers.by_name(name).render
+      Vedeu.log(type: :info, message: message)
+
+      timer(message) { Vedeu.buffers.by_name(name).render }
+    end
+
+    # @param message [String]
+    # @param block [Proc]
+    # @return [Array|ModelNotFound]
+    def timer(message = '', &block)
+      started  = Time.now.to_f
+
+      work = yield
+
+      finished = Time.now.to_f
+      elapsed  = ((finished - started) * 1000).to_i
+
+      Vedeu.log(type: :debug, message: "#{message} took #{elapsed}ms.")
+
+      work
     end
 
   end # Refresh
