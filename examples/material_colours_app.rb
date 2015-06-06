e38809602af51fa94c7bc08bebfef4efb4c06c30
Gavin Laking <gavinlaking@gmail.com>
2015-06-07 00:41:06 +0100

    Don't write to file for material_colours_app.

--- examples/material_colours_app.rb
+++ examples/material_colours_app.rb
@@ -14,7 +14,7 @@ class VedeuMaterialColoursApp
   configure do
     debug!
     log '/tmp/vedeu_material_colours_app.log'
-    renderers Vedeu::Renderers::File
+    # renderers Vedeu::Renderers::File
   end
 
   interface 'main_interface' do
