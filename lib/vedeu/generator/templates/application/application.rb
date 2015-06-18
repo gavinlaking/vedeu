73ebe22e706a71f011eab32b8f45ff7039720631
Gavin Laking <gavinlaking@gmail.com>
2015-06-18 23:22:43 +0100

    Create Bootstrap class to initialize a generated application.

--- lib/vedeu/generator/templates/application/application.rb
+++ lib/vedeu/generator/templates/application/application.rb
@@ -1,59 +1,13 @@
 #!/usr/bin/env ruby
 
-# require 'vedeu'
+require 'vedeu'
 
 module YourAppName
 
   class Application
 
-    def self.start(argv = ARGV)
-      new(argv).start
-    end
-
-    def initialize(argv)
-      @argv = argv
-    end
-
-    def start
-      [:configuration_path,
-       :interface_path,
-       :keymap_path].each do |path|
-        load(path)
-      end
-
-      Vedeu::Launcher.execute!(argv)
-    end
-
-    protected
-
-    attr_reader :argv
-
-    private
-
-    def configuration_path
-      File.dirname(__FILE__) + '/config/**/*'
-    end
-
-    def interface_path
-      File.dirname(__FILE__) + '/app/views/interfaces/**/*'
-    end
-
-    def keymap_path
-      File.dirname(__FILE__) + '/app/models/keymaps/**/*'
-    end
-
-    def load(path)
-      files = send(path)
-
-      Dir.glob(files).select do |file|
-        File.file?(file) && File.extname(file) == '.rb'
-      end.each do |file|
-        Kernel.load(file)
-      end
-    end
+    Vedeu::Bootstrap.start(ARGV)
 
   end
 
 end
-
-YourAppName::Application.start(ARGV)
---
+++ test/lib/vedeu/bootstrap_test.rb
@@ -0,0 +1,30 @@
+require 'test_helper'
+
+module Vedeu
+
+  describe Bootstrap do
+
+    let(:described) { Vedeu::Bootstrap }
+    let(:instance)  { described.new(argv) }
+    let(:argv)      { [] }
+
+    describe '#initialize' do
+      subject { instance }
+
+      it { subject.must_be_instance_of(described) }
+      it { subject.instance_variable_get('@argv').must_equal(argv) }
+    end
+
+    describe '.start' do
+      it { described.must_respond_to(:start) }
+    end
+
+    describe '#start'
+      subject { instance.start }
+
+
+    end
+
+  end # Bootstrap
+
+end # Vedeu
