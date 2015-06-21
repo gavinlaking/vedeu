eb672bfd86f9dc3a4dd6bdb090e08251fce73b3b
Gavin Laking <gavinlaking@gmail.com>
2015-06-21 21:56:23 +0100

    Remove log directory.

--- lib/vedeu/generator/application.rb
+++ lib/vedeu/generator/application.rb
@@ -45,7 +45,6 @@ module Vedeu
         FileUtils.mkdir_p(name + '/bin')
         FileUtils.mkdir_p(name + '/config')
         FileUtils.mkdir_p(name + '/lib')
-        FileUtils.mkdir_p(name + '/log')
         FileUtils.mkdir_p(name + '/test')
       end
 
