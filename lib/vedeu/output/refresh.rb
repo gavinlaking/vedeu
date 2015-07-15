30394985f7d8a4dbbe8caf9e484bc4274466a01d
Gavin Laking <gavinlaking@gmail.com>
2015-07-15 23:54:26 +0100

    Extract Refresh#by_group to RefreshGroup class.

--- lib/vedeu/output/refresh.rb
+++ lib/vedeu/output/refresh.rb
@@ -1,3 +1,5 @@
+require_relative 'refresh_group'
+
 module Vedeu
 
   # Refreshes the terminal.
@@ -34,11 +36,8 @@ module Vedeu
     # @return [Array|ModelNotFound] A collection of the names of interfaces
     #   refreshed, or an exception if the group was not found.
     def by_group(group_name)
-      Vedeu.timer("Refreshing group: '#{group_name}'") do
-        members    = Vedeu.groups.find!(group_name).members
-        interfaces = members.each { |name| Vedeu.interfaces.by_name(name) }
-        by_zindex  = interfaces.sort { |a, b| a.zindex <=> b.zindex }
-        by_zindex.each { |name| by_name(name) }
+      Vedeu.timer("Refresh Group: '#{group_name}'") do
+        Vedeu::RefreshGroup.by_name(group_name)
       end
     end
 
