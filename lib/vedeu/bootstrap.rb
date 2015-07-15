8a70d3f978eb4c59a135edffdd634129b19515b1
Gavin Laking <gavinlaking@gmail.com>
2015-07-15 23:55:11 +0100

    Require views and templates first.

--- lib/vedeu/bootstrap.rb
+++ lib/vedeu/bootstrap.rb
@@ -42,6 +42,8 @@ module Vedeu
       end
 
       [
+        'app/views/templates/**/*',
+        'app/views/interfaces/**/*',
         'app/controllers/**/*',
         'app/helpers/**/*',
         'app/views/**/*',
