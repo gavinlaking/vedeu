a126a0690966c728e7632d9caff07c55e5e1c091
Gavin Laking <gavinlaking@gmail.com>
2015-07-18 20:47:23 +0100

    Add more tests.

---
+++ test/lib/vedeu/repositories/all_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+
+module Vedeu
+
+	describe Repositories do
+
+    describe '.register' do
+      # it { skip! }
+    end
+
+    describe '.registered' do
+      # it { skip! }
+    end
+
+    describe '.reset!' do
+      # it { skip! }
+    end
+
+	end # Repositories
+
+end # Vedeu
