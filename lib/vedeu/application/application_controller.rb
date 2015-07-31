49e8f1349c06462fb12f20285f0a4bbeb3436c00
Gavin Laking <gavinlaking@gmail.com>
2015-07-31 17:43:30 +0100

    Move initialize method of controllers into ApplicationController; Also, add #redirect_to method which client application controllers can call to redirect to a different controller/action.

--- lib/vedeu/application/application_controller.rb
+++ lib/vedeu/application/application_controller.rb
@@ -8,6 +8,28 @@ module Vedeu
 
     include Vedeu::Controller
 
+    # @param params [Hash] The named parameters provided to the controller which
+    #   will be used by the actions within the controller.
+    # @return [Vedeu::ApplicationController]
+    def initialize(**params)
+      @params = params
+    end
+
+    # @param controller [Symbol] The name of controller to be redirected to.
+    # @param action [Symbol] The name of the action within the controller to be
+    #   called.
+    # @param params [Hash] Any named parameter which need to be passed to the
+    #   action.
+    def redirect_to(controller, action, **params)
+      Vedeu.trigger(:_goto_, controller, action, params)
+    end
+
+    protected
+
+    # @!attribute [rw] params
+    # @return [Hash] The named parameters passed to the controller.
+    attr_accessor :params
+
   end # ApplicationController
 
 end # Vedeu
--- test/lib/vedeu/application/application_controller_test.rb
+++ test/lib/vedeu/application/application_controller_test.rb
@@ -5,9 +5,36 @@ module Vedeu
   describe ApplicationController do
 
     let(:described) { Vedeu::ApplicationController }
-    let(:instance)  { described.new }
+    let(:instance)  { described.new(params) }
+    let(:params)    {
+      {
 
-    # @todo Add more tests.
+      }
+    }
+
+    describe '#initialize' do
+      subject { instance }
+
+      it { subject.must_be_instance_of(described) }
+      it { subject.instance_variable_get('@params').must_equal(params) }
+    end
+
+    describe '#redirect_to' do
+      let(:controller) {}
+      let(:action)     {}
+      let(:params)     {
+        {}
+      }
+
+      before { Vedeu.stubs(:trigger) }
+
+      subject { instance.redirect_to(controller, action, params) }
+
+      it {
+        Vedeu.expects(:trigger).with(:_goto_, controller, action, params)
+        subject
+      }
+    end
 
   end # ApplicationController
 
