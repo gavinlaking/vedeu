3f060df9635898bfe6a4d40068f98c719623c517
Gavin Laking <gavinlaking@gmail.com>
2015-08-02 17:12:53 +0100

    Move view related collections into own directory.

---
+++ test/lib/vedeu/models/views/collections/view_collection_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+module Vedeu
+
+  module Views
+
+  	describe ViewCollection do
+
+      let(:described) { Vedeu::Views::ViewCollection }
+      let(:instance)  { described.new }
+
+      it { described.superclass.must_equal(Vedeu::Collection) }
+
+      describe '#initialize' do
+        it { instance.must_be_instance_of(described) }
+      end
+
+  	end # ViewCollection
+
+  end # Views
+
+end # Vedeu
