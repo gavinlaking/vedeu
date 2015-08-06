0194f445f1c95981ed08850d11ba4172acc12142
Gavin Laking <gavinlaking@gmail.com>
2015-08-06 22:15:20 +0100

    Remove #duplicate method.

--- lib/vedeu/dsl/use.rb
+++ lib/vedeu/dsl/use.rb
@@ -6,40 +6,6 @@ module Vedeu
     #
     module Use
 
-      # Duplicate a stored model.
-      #
-      # This DSL method copies the attributes of a stored model, changes the
-      # name to that of the current model, and stores the duplicate as a new
-      # model in model's repository.
-      #
-      # @example
-      #   # Here the attributes of 'my_geometry' are used to create the
-      #   # new model 'my_other_geometry'.
-      #   Vedeu.geometry 'my_other_geometry' do
-      #     duplicate('my_geometry')
-      #   end
-      #
-      # @note
-      #   - Only models of the same repository can be used in this way.
-      #   - If the stored model cannot be found, the duplicate may not have
-      #     the values you required.
-      #   - Values defined before will be overwritten by the new values from
-      #     the stored model.
-      #   - Values defined after will overwrite the values of the new
-      #     duplicate model.
-      #
-      # @param name [String] The name of the model to duplicate.
-      # @return [void] The new model based on the original.
-      def duplicate(name)
-        original = use(name)
-
-        unless original.respond_to?(:null?)
-          duplicated      = original.dup
-          duplicated.name = model.name
-          duplicated.store
-        end
-      end
-
       # Use the attribute of stored model.
       #
       # This DSL method provides access to a stored model by name. You can
