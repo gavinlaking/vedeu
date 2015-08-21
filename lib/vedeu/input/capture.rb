cd706fc04ebd786c5e2054579035745f34a1b7d9
Gavin Laking <gavinlaking@gmail.com>
2015-08-21 20:02:20 +0100

    Whitespace changes.

--- lib/vedeu/input/capture.rb
+++ lib/vedeu/input/capture.rb
@@ -31,25 +31,27 @@ module Vedeu
 
         if keys.ord == 27 # \e
           # Vedeu.log(type: :debug, message: "#keys: Escape detected...")
-
           @chars = 3
 
           begin
             # Vedeu.log(type:    :debug,
             #           message: "#keys: Attempting read_nonblock(#{@chars})")
             keys << console.read_nonblock(@chars)
+
           rescue IO::WaitReadable
             # Vedeu.log(type:    :debug,
             #           message: "#keys: (#{@chars}) Rescuing IO::WaitReadable")
             IO.select([console])
             @chars -= 1
             retry
+
           rescue IO::WaitWritable
             # Vedeu.log(type:    :debug,
             #           message: "#keys: (#{@chars}) Rescuing IO::WaitWritable")
             IO.select(nil, [console])
             @chars -= 1
             retry
+
           end
         end
         # Vedeu.log(type:    :debug,
