093d88b1e44ebc05ab22c7d47161ccec6a15e086
Gavin Laking <gavinlaking@gmail.com>
2015-08-22 18:57:35 +0100

    Refactor LocklessLogDevice.

--- lib/vedeu/log/lockless_log_device.rb
+++ lib/vedeu/log/lockless_log_device.rb
@@ -7,53 +7,65 @@ module Vedeu
 
     # Returns a new instance of Vedeu::LocklessLogDevice.
     #
-    # @param log [void]
+    # @param file_or_filename [void]
     # @return [Vedeu::LocklessLogDevice]
-    def initialize(log = nil)
-      @dev      = nil
-      @filename = nil
-
-      if log.respond_to?(:write) && log.respond_to?(:close)
-        @dev = log
-
-      else
-        @dev      = open_logfile(log)
-        @dev.sync = true
-        @filename = log
-
-      end
+    def initialize(file_or_filename = nil)
+      @file_or_filename = file_or_filename
     end
 
     # @param message [String]
     # @return [void]
     def write(message)
-      @dev.write(message)
+      device.write(message)
 
     rescue StandardError => exception
-      warn("log writing failed. #{exception}")
+      warn("Log writing failed. #{exception}")
+
     end
 
     # @return [void]
     def close
-      @dev.close
+      device.close
+
     rescue
       nil
+
     end
+
+    protected
+
+    # @!attribute [r] file_or_filename
+    # @return [String]
+    attr_reader :file_or_filename
+    alias_method :file,     :file_or_filename
+    alias_method :filename, :file_or_filename
 
     private
 
-    # @param filename [String]
-    # @return [void]
-    def open_logfile(filename)
-      if FileTest.exist?(filename)
+    # @return [File]
+    def device
+      if file?
+        file
+
+      elsif exists?
         open(filename, (File::WRONLY | File::APPEND))
 
       else
-        logdev = open(filename, (File::WRONLY | File::APPEND | File::CREAT))
-        logdev.sync = true
-        logdev
+        device      = open(filename, (File::WRONLY | File::APPEND | File::CREAT))
+        device.sync = true
 
       end
+    end
+
+    # @return [Boolean]
+    def exists?
+      FileTest.exist?(filename)
+    end
+
+    # @return [Boolean]
+    def file?
+      file_or_filename.respond_to?(:write) &&
+      file_or_filename.respond_to?(:close)
     end
 
   end # LocklessLogDevice
--- test/lib/vedeu/log/lockless_log_device_test.rb
+++ test/lib/vedeu/log/lockless_log_device_test.rb
@@ -4,13 +4,25 @@ module Vedeu
 
   describe LocklessLogDevice do
 
-    # let(:described) { Vedeu::LocklessLogDevice }
-    # let(:instance)  { described.new }
-    # # let(:log)       {}
+    let(:described)        { Vedeu::LocklessLogDevice }
+    let(:instance)         { described.new(file_or_filename) }
+    let(:file_or_filename) {}
 
-    # describe '#initialize' do
-    #   it { instance.must_be_instance_of(described) }
-    # end
+    describe '#initialize' do
+      it { instance.must_be_instance_of(described) }
+      it {
+        instance.instance_variable_get('@file_or_filename').
+          must_equal(file_or_filename)
+      }
+    end
+
+    describe '#write' do
+      subject { instance.write(message) }
+    end
+
+    describe '#close' do
+      subject { instance.close }
+    end
 
   end # LocklessLogDevice
 
