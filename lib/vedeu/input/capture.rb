09795d83f2fe29f0b83fec6b9cb326edb9bacc1f
Gavin Laking <gavinlaking@gmail.com>
2015-09-04 20:45:28 +0100

    Remove unnecessary parens and commented code.

--- lib/vedeu/input/capture.rb
+++ lib/vedeu/input/capture.rb
@@ -34,32 +34,23 @@ module Vedeu
         keys = console.getch
 
         if keys.ord == ESCAPE_KEY_CODE
-          # Vedeu.log(type: :debug, message: "#keys: Escape detected...")
           @chars = 3
 
           begin
-            # Vedeu.log(type:    :debug,
-            #           message: "#keys: Attempting read_nonblock(#{@chars})")
             keys << console.read_nonblock(@chars)
 
           rescue IO::WaitReadable
-            # Vedeu.log(type:    :debug,
-            #           message: "#keys: (#{@chars}) Rescuing IO::WaitReadable")
             IO.select([console])
             @chars -= 1
             retry
 
           rescue IO::WaitWritable
-            # Vedeu.log(type:    :debug,
-            #           message: "#keys: (#{@chars}) Rescuing IO::WaitWritable")
             IO.select(nil, [console])
             @chars -= 1
             retry
 
           end
         end
-        # Vedeu.log(type:    :debug,
-        #           message: "#keys: Sending result: #{keys.inspect}")
 
         keys
       end
