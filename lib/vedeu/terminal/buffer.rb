8d119999a3dece294e981a6ec52ca156c1e6da0e
Gavin Laking <gavinlaking@gmail.com>
2015-09-08 23:09:00 +0100

    Continue work on Terminal::Buffer.

--- lib/vedeu/terminal/buffer.rb
+++ lib/vedeu/terminal/buffer.rb
@@ -2,19 +2,31 @@ module Vedeu
 
   module Terminal
 
-    class Buffer
+    # All output will be written to this singleton, and #render will be called
+    # at the end of each run of {Vedeu::MainLoop}; effectively rendering this
+    # buffer to each registered renderer. This buffer is not cleared after this
+    # action though, as subsequent actions will modify the contents. This means
+    # that individual parts of Vedeu can write content here at various points
+    # and only at the end of each run of {Vedeu::MainLoop} will it be actually
+    # output 'somewhere'.
+    #
+    module Buffer
 
-      # private
+      extend self
 
-      def column(y = 1, x = 1)
-        return false if y < 1 || y > Vedeu.height
-        return false if x < 1 || x > Vedeu.width
-
+      # @return [Array<Array<void>>]
+      def output
+        @output ||= terminal
       end
 
-      def row(y = 1)
-        return false if y < 1 || y > Vedeu.height
+      # @return [void|NilClass]
+      def render
+        Vedeu.renderers.render(output) if Vedeu.ready?
+      end
 
+      # @return [Array<Array<void>>]
+      def reset
+        @output = terminal
       end
 
       # @return [Array<Array<Vedeu::Cell>>]
@@ -26,12 +38,18 @@ module Vedeu
         end
       end
 
+      # @param value [void]
+      # @param y [Fixnum]
+      # @param x [Fixnum]
+      # @return [Boolean]
       def write(value, y = 1, x = 1)
         return false if value.nil?
         return false if y < 1 || y > Vedeu.height
         return false if x < 1 || x > Vedeu.width
 
-        terminal[y][x] = value
+        output[y][x] = value
+
+        true
       end
 
     end # Buffer
--- test/lib/vedeu/terminal/buffer_test.rb
+++ test/lib/vedeu/terminal/buffer_test.rb
@@ -6,76 +6,76 @@ module Vedeu
 
     describe Buffer do
 
-      let(:described) { Vedeu::Terminal::Buffer }
-      let(:instance)  { described.new }
+      let(:described)    { Vedeu::Terminal::Buffer }
+      let(:empty_buffer) {
+        Array.new(2) do |y|
+          Array.new(3) do |x|
+            Vedeu::Cell.new(y: y + 1, x: x + 1)
+          end
+        end
+      }
 
       before do
         Vedeu.stubs(:height).returns(2)
         Vedeu.stubs(:width).returns(3)
+        Vedeu::Terminal::Buffer.reset
       end
 
-      describe '#column' do
-        let(:y) { 1 }
-        let(:x) { 2 }
+      describe '#output' do
+        subject { described.output }
 
-        subject { instance.column(y, x) }
-
-        context 'when y < 1' do
-          let(:y) { 0 }
-
-          it { subject.must_equal(false) }
+        context 'when nothing has been written to the buffer' do
+          it {
+            described.instance_variable_get('@output').must_equal(empty_buffer)
+          }
         end
 
-        context 'when y > height' do
-          let(:y) { 3 }
+        context 'when something has been written to the buffer' do
+          let(:expected) {
+            exp = empty_buffer
+            exp[1][2] = Vedeu::Views::Char.new(value: 'a', position: [1, 2])
+            exp
+          }
 
-          it { subject.must_equal(false) }
-        end
+          before do
+            described.write(Vedeu::Views::Char.new(value: 'a',
+                                                   position: [1, 2]), 1, 2)
+          end
 
-        context 'when x < 1' do
-          let(:x) { 0 }
-
-          it { subject.must_equal(false) }
-        end
-
-        context 'when x > width' do
-          let(:x) { 4 }
-
-          it { subject.must_equal(false) }
+          it { described.instance_variable_get('@output').must_equal(expected) }
         end
       end
 
-      describe '#row' do
-        let(:y) {}
+      describe '#render' do
+        subject { described.render }
 
-        subject { instance.row(y) }
+        context 'when Vedeu is not ready' do
+          before do
+            Vedeu.stubs(:ready?).returns(false)
+          end
 
-        context 'when y < 1' do
-          let(:y) { 0 }
-
-          it { subject.must_equal(false) }
+          it { subject.must_equal(nil) }
         end
 
-        context 'when y > height' do
-          let(:y) { 3 }
-
-          it { subject.must_equal(false) }
+        context 'when Vedeu is ready' do
+          # it { skip }
         end
+      end
+
+      describe '#reset' do
+        subject { described.reset }
+
+        it {
+          described.instance_variable_get('@output').must_equal(empty_buffer)
+        }
+        it { subject.must_equal(empty_buffer) }
       end
 
       describe '#terminal' do
-        let(:expected) {
-          Array.new(2) do |y|
-            Array.new(3) do |x|
-              Vedeu::Cell.new(y: y + 1, x: x + 1)
-            end
-          end
-        }
-
-        subject { instance.terminal }
+        subject { described.terminal }
 
         it { subject.must_be_instance_of(Array) }
-        it { subject.must_equal(expected) }
+        it { subject.must_equal(empty_buffer) }
       end
 
       describe '#write' do
@@ -83,7 +83,7 @@ module Vedeu
         let(:x)      { 0 }
         let(:_value) {}
 
-        subject { instance.write(_value, y, x) }
+        subject { described.write(_value, y, x) }
 
         context 'when the value is nil' do
           it { subject.must_equal(false) }
@@ -116,9 +116,20 @@ module Vedeu
         context 'when y and x are within the height and width' do
           let(:y)      { 1 }
           let(:x)      { 2 }
-          let(:_value) { Vedeu::Cell.new(y: y, x: x, value: 'a') }
+          let(:_value) { Vedeu::Views::Char.new(y: y, x: x, value: 'a') }
+          let(:expected) {
+            exp = empty_buffer
+            exp[y][x] = Vedeu::Views::Char.new(value: 'a', position: [y, x])
+            exp
+          }
 
-          it { subject.must_equal(_value) }
+          before do
+            described.write(Vedeu::Views::Char.new(value: 'a',
+                                                   position: [y, x]), y, x)
+          end
+
+          it { subject.must_equal(true) }
+          it { described.output.must_equal(expected) }
         end
       end
 
