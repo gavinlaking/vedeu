ed5d254e9d332853f1a2c262f91fcaa5f60ec487
Gavin Laking <gavinlaking@gmail.com>
2015-09-21 22:07:45 +0100

    Remove HTML renderer from example application.

--- test/support/examples/material_colours_app.rb
+++ test/support/examples/material_colours_app.rb
@@ -26,7 +26,6 @@ class VedeuMaterialColoursApp
   Vedeu.configure do
     debug!
     log '/tmp/vedeu_material_colours_app.log'
-    renderers Vedeu::Renderers::HTML.new(filename: '/tmp/material', timestamp: true)
   end
 
   Vedeu.interface 'main_interface' do
