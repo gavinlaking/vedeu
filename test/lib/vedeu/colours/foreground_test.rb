47f60ac48d8c2179993c3aa5c05639ecae4d7c9f
Gavin Laking <gavinlaking@gmail.com>
2015-09-26 03:15:28 +0100

    Remove unnecessary tests.

--- test/lib/vedeu/colours/foreground_test.rb
+++ test/lib/vedeu/colours/foreground_test.rb
@@ -24,9 +24,6 @@ module Vedeu
         context 'when the colour is named (3-bit / 8 colours)' do
           {
             red:     "\e[31m",
-            yellow:  "\e[33m",
-            magenta: "\e[35m",
-            white:   "\e[97m",
             default: "\e[39m",
             unknown: '',
           }.map do |colour, result|
@@ -38,12 +35,7 @@ module Vedeu
 
         context 'when the colour is numbered (8-bit / 256 colours)' do
           {
-            24  => "\e[38;5;24m",
-            56  => "\e[38;5;56m",
             82  => "\e[38;5;82m",
-            118 => "\e[38;5;118m",
-            143 => "\e[38;5;143m",
-            219 => "\e[38;5;219m",
             -2  => '',
             442 => ''
           }.map do |colour, result|
@@ -57,12 +49,7 @@ module Vedeu
           before { Configuration.stubs(:colour_mode).returns(8) }
 
           {
-            '#5f0000' => "\e[38;5;52m",
-            '#008700' => "\e[38;5;28m",
-            '#0000d7' => "\e[38;5;20m",
-            '#afafaf' => "\e[38;5;145m",
             '#afd700' => "\e[38;5;148m",
-            '#af005f' => "\e[38;5;125m",
             '999999'  => '',
             '#12121'  => '',
             '#h11111' => '',
@@ -75,12 +62,7 @@ module Vedeu
 
         context 'when the colour is a CSS value (24-bit / 16.8mil colours)' do
           {
-            '#5f0000' => "\e[38;2;95;0;0m",
-            '#008700' => "\e[38;2;0;135;0m",
-            '#0000d7' => "\e[38;2;0;0;215m",
-            '#afafaf' => "\e[38;2;175;175;175m",
             '#afd700' => "\e[38;2;175;215;0m",
-            '#af005f' => "\e[38;2;175;0;95m",
             '999999'  => '',
             '#12121'  => '',
             '#h11111' => '',
