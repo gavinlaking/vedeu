b21f44b2369c470cff101fe3e2b3775ea24dd6c6
Gavin Laking <gavinlaking@gmail.com>
2015-10-04 21:13:16 +0100

    Wrap benchmark-ips gem.

--- lib/vedeu/logging/all.rb
+++ lib/vedeu/logging/all.rb
@@ -12,5 +12,6 @@ end # Vedeu
 require 'vedeu/logging/lockless_log_device'
 require 'vedeu/logging/mono_logger'
 require 'vedeu/logging/log'
+require 'vedeu/logging/ips'
 require 'vedeu/logging/debug'
 require 'vedeu/logging/timer'
---
+++ lib/vedeu/logging/ips.rb
@@ -0,0 +1,64 @@
+require 'benchmark/ips'
+
+module Benchmark
+  module IPS
+    class Report
+      class Entry
+        def display
+          Vedeu.log(type: :debug, message: to_s)
+        end
+      end
+    end
+  end
+end
+
+module Vedeu
+
+  module Logging
+
+    module Debug
+
+      class IPS
+
+        attr_accessor :samples
+        attr_accessor :benchmark
+
+        def initialize(&block)
+          @old_stdout = $stdout
+          $stdout     = StringIO.new
+          @samples    = {}
+          @benchmark  = Benchmark::IPS::Job.new
+          @count      = 0
+        end
+
+        def add_item(label = '', &blk)
+          samples[label] = blk
+          @count += 1
+
+          benchmark.item(label, &blk)
+        end
+
+        def execute!
+          benchmark.compare!
+          benchmark.run_warmup
+          benchmark.run
+
+          $stdout.sync = true
+          benchmark.run_comparison
+          benchmark.full_report
+
+          Vedeu.log(type: :debug, message: "IPS:\n#{$stdout.string}")
+          $stdout = @old_stdout
+
+          key = samples.keys.sample
+          Vedeu.log(type: :debug, message: "Running: #{key}")
+          samples[key].call
+        end
+
+      end # IPS
+
+    end # Debug
+
+  end # Logging
+
+end # Vedeu
