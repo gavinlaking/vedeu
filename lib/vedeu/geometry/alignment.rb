41c44bfcc77e17285dadd836fd5d991ecd8a219f
Gavin Laking <gavinlaking@gmail.com>
2015-10-21 20:02:41 +0100

    Remove HorizontalAlignment and VerticalAlignment classes.

--- lib/vedeu/geometry/alignment.rb
+++ lib/vedeu/geometry/alignment.rb
@@ -71,42 +71,6 @@ module Vedeu
 
     end # Alignment
 
-    # Provides the mechanism to align an interface/view horizontally
-    # within the available terminal space.
-    #
-    class HorizontalAlignment < Vedeu::Geometry::Alignment
-
-      # @raise [Vedeu::Error::InvalidSyntax] When the value is not one
-      #   of :bottom, :centre, :left, :middle, :right, :top.
-      # @return [Symbol]
-      def align
-        return value if valid?
-
-        fail Vedeu::Error::InvalidSyntax,
-             'No horizontal alignment value given. Valid values are :center, ' \
-             ':centre, :left, :none, :right.'.freeze
-      end
-
-    end # HorizontalAlignment
-
-    # Provides the mechanism to align an interface/view vertically
-    # within the available terminal space.
-    #
-    class VerticalAlignment < Vedeu::Geometry::Alignment
-
-      # @raise [Vedeu::Error::InvalidSyntax] When the value is not one
-      #   of :bottom, :centre, :left, :middle, :right, :top.
-      # @return [Symbol]
-      def align
-        return value if valid?
-
-        fail Vedeu::Error::InvalidSyntax,
-             'No vertical alignment value given. Valid values are :bottom, ' \
-             ':middle, :none, :top.'.freeze
-      end
-
-    end # VerticalAlignment
-
   end # Geometry
 
 end # Vedeu
