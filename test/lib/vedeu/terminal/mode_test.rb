d949a8348be49339663d198668ae765fb1f0a675
Gavin Laking <gavinlaking@gmail.com>
2015-10-23 23:05:40 +0100

    Start adding mechanism for the :_mode_switch_ event to accept an optional mode parameter.

--- test/lib/vedeu/terminal/mode_test.rb
+++ test/lib/vedeu/terminal/mode_test.rb
@@ -51,25 +51,43 @@ module Vedeu
       end
 
       describe '.switch_mode!' do
-        subject { described.switch_mode! }
+        let(:mode) {}
+
+        subject { described.switch_mode!(mode) }
 
         it 'returns a Symbol' do
           subject.must_be_instance_of(Symbol)
         end
 
-        it 'returns :fake if previously :raw' do
-          described.raw_mode!
-          subject.must_equal(:fake)
+        context 'when the mode is not given' do
+          it 'returns :fake if previously :raw' do
+            described.raw_mode!
+            subject.must_equal(:fake)
+          end
+
+          it 'returns :cooked if previously :fake' do
+            described.fake_mode!
+            subject.must_equal(:cooked)
+          end
+
+          it 'returns :raw if previously :cooked' do
+            described.cooked_mode!
+            subject.must_equal(:raw)
+          end
         end
 
-        it 'returns :cooked if previously :fake' do
-          described.fake_mode!
-          subject.must_equal(:cooked)
-        end
+        context 'when the mode is given' do
+          let(:mode) { :cooked }
 
-        it 'returns :raw if previously :cooked' do
-          described.cooked_mode!
-          subject.must_equal(:raw)
+          it { subject.must_equal(:cooked) }
+
+          context 'when the mode given is not valid' do
+            let(:mode) { :invalid }
+
+            before { described.cooked_mode! }
+
+            it { subject.must_equal(:raw) }
+          end
         end
       end
 
@@ -85,6 +103,12 @@ module Vedeu
 
         it 'returns the configured terminal mode' do
           subject.must_equal(:raw)
+        end
+
+        context 'when the mode has been changed' do
+          before { described.fake_mode! }
+
+          it { subject.must_equal(:fake) }
         end
       end
 
