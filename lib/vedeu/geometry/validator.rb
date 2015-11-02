f66a44c5d9e504b40f1cc7bae259b2391c1428d4
Gavin Laking <gavinlaking@gmail.com>
2015-11-02 22:48:50 +0000

    Reduce duplication by introducing a Validator module for the Geometry DSL.

---
+++ lib/vedeu/geometry/validator.rb
@@ -0,0 +1,56 @@
+module Vedeu
+
+  module Geometry
+
+    module Validator
+
+      include Vedeu::Common
+
+      # @param value [Fixnum] The number of lines/rows.
+      # @raise [Vedeu::Error::InvalidSyntax] When the value is nil.
+      # @return [TrueClass]
+      def validate_height!(value)
+        fail Vedeu::Error::InvalidSyntax,
+             'No height given.'.freeze if absent?(value)
+
+        true
+      end
+
+      # @param value [Symbol] One of :center, :centre, :left,
+      #   :none, :right.
+      # @raise [Vedeu::Error::InvalidSyntax] When the value is nil.
+      # @return [TrueClass]
+      def validate_horizontal_alignment!(value)
+        fail Vedeu::Error::InvalidSyntax,
+             'No horizontal alignment given. Valid values are :center, ' \
+             ':centre, :left, :none, :right.'.freeze unless present?(value)
+
+        true
+      end
+
+      # @param vertical [Symbol] One of :bottom, :middle, :none, :top.
+      # @raise [Vedeu::Error::InvalidSyntax] When the value is nil.
+      # @return [TrueClass]
+      def validate_vertical_alignment!(value)
+        fail Vedeu::Error::InvalidSyntax,
+             'No vertical alignment given. Valid values are :bottom, ' \
+             ':middle, :none, :top.'.freeze unless present?(value)
+
+        true
+      end
+
+      # @param value [Fixnum] The number of characters/columns.
+      # @raise [Vedeu::Error::InvalidSyntax] When the value is nil.
+      # @return [TrueClass]
+      def validate_width!(value)
+        fail Vedeu::Error::InvalidSyntax,
+             'No width given.'.freeze if absent?(value)
+
+        true
+      end
+
+    end # Validator
+
+  end # Geometry
+
+end # Vedeu
