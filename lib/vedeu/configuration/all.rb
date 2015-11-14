f9c942286ea9c9ad4a0e1acb3ed68e9c83cc8e70
Gavin Laking <gavinlaking@gmail.com>
2015-11-14 20:03:19 +0000

    Add all.rb to both configuration/ and output/;
    Also remove output/refresh/; moving contents to output/.

---
+++ lib/vedeu/configuration/all.rb
@@ -0,0 +1,26 @@
+module Vedeu
+
+  # Namespace for configuration classes.
+  #
+  module Config
+
+    module_function
+
+    # Custom log for configuration.
+    #
+    # @param from [String] Which configuration set the options.
+    # @param options [Hash<Symbol => void>] The configuration options set.
+    # @return [Hash<Symbol => void>] The options param.
+    def log(from, options)
+      options.each do |option, value|
+        Vedeu.log(type:    :config,
+                  message: "#{from} #{option}: #{value}".freeze)
+      end
+    end
+
+  end
+
+end # Vedeu
+
+require 'vedeu/configuration/api'
+require 'vedeu/configuration/configuration'
--- lib/vedeu/configuration/configuration.rb
+++ lib/vedeu/configuration/configuration.rb
@@ -1,25 +1,5 @@
 module Vedeu
 
-  # Namespace for configuration classes.
-  #
-  module Config
-
-    module_function
-
-    # Custom log for configuration.
-    #
-    # @param from [String] Which configuration set the options.
-    # @param options [Hash<Symbol => void>] The configuration options set.
-    # @return [Hash<Symbol => void>] The options param.
-    def log(from, options)
-      options.each do |option, value|
-        Vedeu.log(type:    :config,
-                  message: "#{from} #{option}: #{value}".freeze)
-      end
-    end
-
-  end
-
   # Allows the customisation of Vedeu's behaviour through the
   # configuration API.
   #
