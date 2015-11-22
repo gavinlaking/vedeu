1c364a6b7515ced69d8aa380cc0fcd01e7525aa6
Gavin Laking <gavinlaking@gmail.com>
2015-11-22 18:54:06 +0000

    Fix intermittent test failure;
    Fixes #322.

--- test/lib/vedeu/logging/ips_test.rb
+++ test/lib/vedeu/logging/ips_test.rb
@@ -13,7 +13,7 @@ module Vedeu
 
         describe '#initialize' do
           it { instance.must_be_instance_of(described) }
-          it { instance.instance_variable_get('@old_stdout').must_be_instance_of(StringIO) }
+          it { instance.instance_variable_get('@old_stdout').wont_equal(nil) }
           it { instance.instance_variable_get('@samples').must_equal({}) }
           it { instance.instance_variable_get('@benchmark').must_be_instance_of(Benchmark::IPS::Job) }
           it { instance.instance_variable_get('@count').must_equal(0) }
