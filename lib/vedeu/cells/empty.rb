35e73a2aa1a9128ffc01891e4a586c27763ee212
Gavin Laking <gavinlaking@gmail.com>
2015-11-30 23:30:21 +0000

    Flesh out Cells::Empty class with some behaviour.

--- lib/vedeu/cells/empty.rb
+++ lib/vedeu/cells/empty.rb
@@ -4,6 +4,45 @@ module Vedeu
 
     class Empty
 
+      include Comparable
+      include Vedeu::Common
+      include Vedeu::Presentation
+      include Vedeu::Repositories::Defaults
+
+      # @!attribute [r] value
+      # @return [String]
+      attr_reader :value
+
+      # An object is equal when its values are the same.
+      #
+      # @param other [void]
+      # @return [Boolean]
+      def eql?(other)
+        self.class == other.class    &&
+          position == other.position &&
+          value    == other.value    &&
+          colour   == other.colour
+      end
+      alias_method :==, :eql?
+
+      private
+
+      # @return [Hash<Symbol => Hash<void>|NilClass|String>]
+      def defaults
+        {
+          colour:   {},
+          name:     '',
+          position: nil,
+          style:    '',
+          value:    '',
+        }
+      end
+
+      # @return [Vedeu::Interfaces::Interface]
+      def interface
+        @interface ||= Vedeu.interfaces.by_name(name)
+      end
+
     end # Empty
 
   end # Cells
--- test/lib/vedeu/cells/empty_test.rb
+++ test/lib/vedeu/cells/empty_test.rb
@@ -6,6 +6,41 @@ module Vedeu
 
     describe Empty do
 
+      let(:described)  { Vedeu::Cells::Empty }
+      let(:instance)   { described.new(attributes) }
+      let(:attributes) {
+        {
+          colour:   colour,
+          name:     _name,
+          position: position,
+          style:    style,
+          value:    _value,
+        }
+      }
+      let(:colour)   { {} }
+      let(:_name)    { '' }
+      let(:position) {}
+      let(:style)    { '' }
+      let(:_value)   { '' }
+
+      describe 'accessors' do
+        it { instance.must_respond_to(:value) }
+      end
+
+      describe '#eql?' do
+        let(:other) { instance }
+
+        subject { instance.eql?(other) }
+
+        it { subject.must_equal(true) }
+
+        context 'when different to other' do
+          let(:other) { described.new(value: 'b') }
+
+          it { subject.must_equal(false) }
+        end
+      end
+
     end # Empty
 
   end # Cells
