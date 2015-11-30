1b35caa7e9dec0dba189c65d8782942315378d7a
Gavin Laking <gavinlaking@gmail.com>
2015-11-30 23:30:49 +0000

    Flesh out Cells::Border class with some behaviour.

--- lib/vedeu/cells/border.rb
+++ lib/vedeu/cells/border.rb
@@ -2,7 +2,23 @@ module Vedeu
 
   module Cells
 
-    class Border
+    class Border < Empty
+
+      include Vedeu::Common
+
+      # @return [String]
+      def value
+        return '' unless present?(@value)
+
+        Vedeu::EscapeSequences::Esc.border { @value }
+      end
+
+      private
+
+      # @return [Hash<Symbol => Hash<void>>]
+      def defaults
+        super.merge!(border: {})
+      end
 
     end # Border
 
--- test/lib/vedeu/cells/border_test.rb
+++ test/lib/vedeu/cells/border_test.rb
@@ -6,6 +6,41 @@ module Vedeu
 
     describe Border do
 
+      let(:described)  { Vedeu::Cells::Border }
+      let(:instance)   { described.new(attributes) }
+      let(:attributes) {
+        {
+          value: _value
+        }
+      }
+      let(:_value) {}
+
+      describe '#value' do
+        subject { instance.value }
+
+        context 'when the value is a horizontal border character' do
+          let(:_value) { "\x71" }
+
+          it { subject.must_equal("\e(0q\e(B") }
+        end
+
+        context 'when the value is a vertical border character' do
+          let(:_value) { "\x78" }
+
+          it { subject.must_equal("\e(0x\e(B") }
+        end
+
+        context 'when the value is a corner border character' do
+          let(:_value) { "\x6A" }
+
+          it { subject.must_equal("\e(0j\e(B") }
+        end
+
+        context 'when the value is nil or empty' do
+          it { subject.must_equal('') }
+        end
+      end
+
     end # Border
 
   end # Cells
