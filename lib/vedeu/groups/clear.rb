720c63c3db61c035721d6c44904348a62fa3fb39
Gavin Laking <gavinlaking@gmail.com>
2015-12-05 23:00:47 +0000

    Fix some Rubocop warnings.

--- lib/vedeu/groups/clear.rb
+++ lib/vedeu/groups/clear.rb
@@ -67,7 +67,9 @@ module Vedeu
   # :nocov:
 
   # See {file:docs/events/visibility.md#\_clear_group_}
-  Vedeu.bind(:_clear_group_) { |name| Vedeu.clear_by_group(name) if Vedeu.ready? }
+  Vedeu.bind(:_clear_group_) do |name|
+    Vedeu.clear_by_group(name) if Vedeu.ready?
+  end
 
   # :nocov:
 
