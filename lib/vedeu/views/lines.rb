3f5ab707f5cce7879d58cb0221f29f083bb67ec7
Gavin Laking <gavinlaking@gmail.com>
2015-12-17 22:47:32 +0000

    Add custom .coerce method for Vedeu::Views::Lines.

--- lib/vedeu/views/lines.rb
+++ lib/vedeu/views/lines.rb
@@ -8,6 +8,35 @@ module Vedeu
     #
     class Lines < Vedeu::Repositories::Collection
 
+      # @param See Vedeu::Repositories::Collection#initialize
+      # @raise [Vedeu::Error::InvalidSyntax] When the collection
+      #   cannot be coerced since it is unrecognised or unsupported.
+      # @return [Vedeu::Views::Lines]
+      def self.coerce(collection = [], parent = nil, name = nil)
+        if collection.is_a?(Vedeu::Views::Lines)
+          collection
+
+        elsif collection.is_a?(Array)
+          return new(collection, parent, name) if collection.empty?
+
+          coerced_collection = []
+          collection.each do |element|
+            coerced_collection << element if element.is_a?(Vedeu::Views::Line)
+          end
+
+          new(coerced_collection, parent, name)
+
+        elsif collection.is_a?(Vedeu::Views::Line)
+          new([collection], parent, name)
+
+        else
+          fail Vedeu::Error::InvalidSyntax,
+               'Cannot coerce for Vedeu::View::Lines as collection ' \
+               'unrecognised.'.freeze
+
+        end
+      end
+
     end # Lines
 
   end # Views
--- test/lib/vedeu/views/lines_test.rb
+++ test/lib/vedeu/views/lines_test.rb
@@ -6,6 +6,51 @@ module Vedeu
 
     describe Lines do
 
+      let(:described)  { Vedeu::Views::Lines }
+      let(:instance)   { described.new }
+      let(:collection) { Vedeu::Views::Lines.new }
+      let(:parent)     {}
+      let(:_name)      {}
+
+      describe '.coerce' do
+        let(:line) { Vedeu::Views::Line.new }
+
+        subject { described.coerce(collection, parent, _name) }
+
+        context 'when the collection is a Vedeu::Views::Lines' do
+          it { subject.must_equal(collection) }
+        end
+
+        context 'when the collection is an Array' do
+          context 'when the collection is not empty' do
+            let(:collection) { [line, nil, line] }
+            let(:expected)   { Vedeu::Views::Lines.new([line, line]) }
+
+            it { subject.must_equal(expected) }
+          end
+
+          context 'when the collection is empty' do
+            let(:collection) { [] }
+            let(:expected)   { Vedeu::Views::Lines.new }
+
+            it { subject.must_equal(expected) }
+          end
+        end
+
+        context 'when the collection is a Vedeu::Views::Line' do
+          let(:collection) { line }
+          let(:expected)   { Vedeu::Views::Lines.new([line]) }
+
+          it { subject.must_equal(expected) }
+        end
+
+        context 'when the collection is something else' do
+          let(:collection) { :invalid }
+
+          it { proc { subject }.must_raise(Vedeu::Error::InvalidSyntax) }
+        end
+      end
+
     end # Lines
 
   end # Views
