2e17c0de99a22dc4acb19b41bd22b62bde738864
Gavin Laking <gavinlaking@gmail.com>
2015-12-17 18:51:43 +0000

    DSL::Text is being repurposed to be the manipulator of strings.

--- lib/vedeu/dsl/text.rb
+++ lib/vedeu/dsl/text.rb
@@ -1,95 +1,64 @@
-
 module Vedeu
 
   module DSL
 
-    # Provides helper methods for building views.
+    # Manipulates text values based on given options when building
+    # views.
     #
-    module Text
+    # @api private
+    #
+    class Text
 
-      # Specify the content for a view. Provides the means to align a
-      # string (or object responding to `to_s`), and add it as a Line
-      # or to the Stream.
-      #
-      # @note If using the convenience methods; left, centre, center
-      #   or right, then a specified anchor will be ignored.
-      #
-      # @example
-      #   lines do
-      #     centre '...'
-      #   end
-      #
-      #   line do
-      #     right '...'
-      #   end
-      #
-      #   line do
-      #     stream do
-      #       text '...'
-      #     end
-      #   end
-      #
-      #   left 'This will be left aligned.', width: 35
-      #   # => 'This will be left aligned.         '
-      #
-      #   centre 'This will be aligned centrally.', width: 35
-      #   # => '  This will be aligned centrally.  '
-      #   # centre is also aliased to center
-      #
-      #   right 'This will be right aligned.', width: 35
-      #   # => '        This will be right aligned.'
-      #
-      #   right 'This will be right aligned.', width: 35,
-      #     anchor: centre
-      #
-      #   text 'This will be truncated here. More text here.',
-      #     width: 28 # => 'This will be truncated here.'
-      #
-      #   text 'Padded with hyphens.', width: 25, pad: '-',
-      #     anchor: :right # => '-----Padded with hyphens.'
-      #
-      # @param value [String|Object] A string or object that responds
-      #   to `to_s`.
-      # @param options [Hash<Symbol => void>] Text options.
-      # @option options :anchor [Symbol] One of `:left`,
-      #   `:centre`/`:center`, or `:right`.
-      # @option options :width [Integer|NilClass] The width of the
-      #   text stream to add. If the `string` provided is longer than
-      #   this value, the string will be truncated. If no width is
-      #   provided in the context of 'lines', then the interface width
-      #   is used. If no width is provided in the context of a
-      #   'stream', then no alignment will occur.
-      # @option options :pad [String] The character to use to pad the
-      #   width, by default uses an empty space (0x20). Only when the
-      #   string is shorter than the specified width.
-      # @return [String]
-      def text(value = '', options = {})
-        options.merge!(text_options).merge!(anchor: __callee__)
+      extend Forwardable
 
-        Vedeu::Output::Text.add(value, options)
+      def_delegators :align,
+                     :centre_aligned?,
+                     :left_aligned?,
+                     :right_aligned?,
+                     :unaligned?
+
+      # @param value [String]
+      # @param options [Vedeu::DSL::ViewOptions]
+      # @return [Vedeu::DSL::Text]
+      def initialize(value = '', options = Vedeu::DSL::ViewOptions.coerce({}))
+        fail Vedeu::Error::Fatal unless options.is_a?(Vedeu::DSL::ViewOptions)
+
+        @value   = value || ''
+        @options = options
       end
-      alias_method :text=,   :text
-      alias_method :align,   :text
-      alias_method :center,  :text
-      alias_method :centre,  :text
-      alias_method :left,    :text
-      alias_method :right,   :text
-      alias_method :align=,  :text
-      alias_method :center=, :text
-      alias_method :centre=, :text
-      alias_method :left=,   :text
-      alias_method :right=,  :text
 
-      private
+      # def align
+      #   options[:align]
+      # end
 
-      # @return [Hash<Symbol => void>]
-      def text_options
-        {
-          client: client,
-          model:  model,
-          name:   model.name,
-        }
-      end
+      # def alignment
+      #   # Vedeu::DSL::Align.new(value, options)
+
+      #   if truncate?
+
+
+      #   end
+      # end
+
+      # def truncate?
+      #   options[:truncate]
+      # end
+
+      # def truncate
+      #   return Vedeu::DSL::Truncate.new(value, options) if truncate?
+
+      #   value
+      # end
+
+      # def wordwrap
+      #   if truncate?
+      #     value
+
+      #   # else
+      #     # wordwrap
+
+      #   end
+      # end
 
     end # Text
 
--- test/lib/vedeu/dsl/text_test.rb
+++ test/lib/vedeu/dsl/text_test.rb
@@ -6,126 +6,41 @@ module Vedeu
 
     describe Text do
 
-      # let(:described) { Vedeu::DSL::Text }
+      let(:described) { Vedeu::DSL::Text }
+      let(:instance)  { described.new(_value, options) }
+      let(:_value)    { 'Some text...' }
+      let(:options)   {
+        Vedeu::DSL::ViewOptions.coerce({})
+      }
 
-      # describe '#text' do
-      #   let(:_value)  {}
-      #   let(:options) { {} }
-      #   let(:modified_options) {
-      #     {
-      #       anchor: anchor,
-      #       model:  model,
-      #       name:   model.name,
-      #       client: nil,
-      #     }
-      #   }
-      #   let(:anchor)   { :text }
-      #   let(:model)    { Vedeu::Views::Line.new }
-      #   let(:instance) { Vedeu::DSL::Line.new(model) }
+      describe '#initialize' do
+        subject { instance }
 
-      #   subject { instance.text(_value, options) }
+        context 'when the options is a Vedeu::DSL::ViewOptions object' do
+          it { subject.instance_variable_get('@options').must_equal(options) }
+        end
 
-      #   it { instance.must_respond_to(:text=) }
-      #   it { instance.must_respond_to(:align=) }
-      #   it { instance.must_respond_to(:center=) }
-      #   it { instance.must_respond_to(:centre=) }
-      #   it { instance.must_respond_to(:left=) }
-      #   it { instance.must_respond_to(:right=) }
+        context 'when the options is not a Vedeu::DSL::ViewOptions object' do
+          let(:options) { {} }
 
-      #   context 'when the model is a Vedeu::Views::View' do
-      #     let(:model)    { Vedeu::Views::View.new }
-      #     let(:instance) { Vedeu::Interfaces::DSL.new(model) }
+          it { proc { subject }.must_raise(Vedeu::Error::Fatal) }
+        end
 
-      #     it { subject.must_be_instance_of(Vedeu::Views::Lines) }
+        it { subject.must_be_instance_of(described) }
 
-      #     it 'adds the text to the model' do
-      #       described.expects(:add).with(_value, modified_options)
-      #       subject
-      #     end
-      #   end
+        context 'when the value is given' do
+          it { subject.instance_variable_get('@value').must_equal(_value) }
+        end
 
-      #   context 'when the model is a Vedeu::Views::Line' do
-      #     let(:model)    { Vedeu::Views::Line.new }
-      #     let(:instance) { Vedeu::DSL::Line.new(model) }
+        context 'when the value is not given' do
+          let(:_value) {}
 
-      #     it { subject.must_be_instance_of(Vedeu::Views::Streams) }
+          it { subject.instance_variable_get('@value').must_equal('') }
+        end
 
-      #     it 'adds the text to the model' do
-      #       described.expects(:add).with(_value, modified_options)
-      #       subject
-      #     end
-      #   end
 
-      #   context 'when the model is a Vedeu::Views::Stream' do
-      #     let(:parent)   { Vedeu::Views::Line.new }
-      #     let(:model)    { Vedeu::Views::Stream.new(parent: parent) }
-      #     let(:instance) { Vedeu::DSL::Stream.new(model) }
-
-      #     it { subject.must_be_instance_of(Vedeu::Views::Streams) }
-
-      #     it 'adds the text to the model' do
-      #       described.expects(:add).with(_value, modified_options)
-      #       subject
-      #     end
-      #   end
-
-      #   context 'alias methods' do
-      #     context '#align' do
-      #       let(:anchor) { :align }
-
-      #       subject { instance.align(_value, options) }
-
-      #       it 'adds the text to the model' do
-      #         described.expects(:add).with(_value, modified_options)
-      #         subject
-      #       end
-      #     end
-
-      #     context '#center' do
-      #       let(:anchor) { :center }
-
-      #       subject { instance.center(_value, options) }
-
-      #       it 'adds the text to the model' do
-      #         described.expects(:add).with(_value, modified_options)
-      #         subject
-      #       end
-      #     end
-
-      #     context '#centre' do
-      #       let(:anchor) { :centre }
-
-      #       subject { instance.centre(_value, options) }
-
-      #       it 'adds the text to the model' do
-      #         described.expects(:add).with(_value, modified_options)
-      #         subject
-      #       end
-      #     end
-
-      #     context '#left' do
-      #       let(:anchor) { :left }
-
-      #       subject { instance.left(_value, options) }
-
-      #       it 'adds the text to the model' do
-      #         described.expects(:add).with(_value, modified_options)
-      #         subject
-      #       end
-      #     end
-
-      #     context '#right' do
-      #       let(:anchor) { :right }
-
-      #       subject { instance.right(_value, options) }
-
-      #       it 'adds the text to the model' do
-      #         described.expects(:add).with(_value, modified_options)
-      #         subject
-      #       end
-      #     end
-      #   end
-      # end
+        it { subject.instance_variable_get('@options').must_equal(options) }
+      end
 
     end # Text
 
