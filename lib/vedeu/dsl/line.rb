a92787c98d01686dc24a51e350c9421777641088
Gavin Laking <gavinlaking@gmail.com>
2015-12-17 18:33:46 +0000

    Functionality in DSL::Line now in DSL::Elements.

--- lib/vedeu/dsl/line.rb
+++ lib/vedeu/dsl/line.rb
@@ -2,117 +2,11 @@ module Vedeu
 
   module DSL
 
-    # Provides methods to be used to define views.
-    #
-    #   Vedeu.renders do
-    #     view :my_interface do
-    #       lines do
-    #         background '#000000'
-    #         foreground '#ffffff'
-    #         line 'This is white text on a black background.'
-    #         line 'Next is a blank line:'
-    #         line ''
-    #
-    #         streams { stream 'We can define ' }
-    #
-    #         streams do
-    #           foreground '#ff0000'
-    #           stream 'parts of a line '
-    #         end
-    #
-    #         streams { stream 'independently using ' }
-    #
-    #         streams do
-    #           foreground '#00ff00'
-    #           stream 'streams.'
-    #         end
-    #       end
-    #     end
-    #   end
-    #
     class Line
 
       include Vedeu::DSL
       include Vedeu::DSL::Presentation
       include Vedeu::DSL::Elements
-
-      # Specify a single line in a view.
-      #
-      #   Vedeu.renders do
-      #     view :my_interface do
-      #       lines do
-      #         line 'some text...'
-      #         # ... some code
-      #
-      #         line 'some more text...'
-      #         # ... some code
-      #       end
-      #     end
-      #   end
-      #
-      # @raise [Vedeu::Error::RequiresBlock]
-      # @return [Vedeu::Views::Lines]
-      # def line(value = '', &block)
-      #   if block_given?
-      #     content = Vedeu::Views::Line.build(client: client,
-      #                                        parent: model.parent, &block)
-
-      #   elsif value
-      #     content = Vedeu::Views::Line.build(client: client,
-      #                                        parent: model.parent,
-      #                                        value:  [build_stream(value)])
-
-      #   else
-      #     fail Vedeu::Error::RequiresBlock unless block_given?
-
-      #   end
-
-      #   model.parent.add(content)
-      # end
-      # alias_method :line=, :line
-
-      # Define multiple streams (a stream is a subset of a line).
-      # Uses {Vedeu::DSL::Stream} for all directives within the
-      # required block.
-      #
-      #   Vedeu.renders do
-      #     view :my_interface do
-      #       lines do
-      #         line do
-      #           streams do
-      #             # ... some code
-      #           end
-      #
-      #           stream do
-      #             # ... some code
-      #           end
-      #         end
-      #       end
-      #     end
-      #   end
-      #
-      # @param block [Proc]
-      # @raise [Vedeu::Error::RequiresBlock]
-      # @return [Vedeu::Views::Streams<Vedeu::Views::Stream>]
-      # @see Vedeu::DSL::Stream for subdirectives.
-      # def streams(&block)
-      #   fail Vedeu::Error::RequiresBlock unless block_given?
-
-      #   model.add(Vedeu::Views::Stream.build(attributes, &block))
-      # end
-      # alias_method :stream, :streams
-      # alias_method :stream=, :streams
-      # alias_method :streams=, :streams
-
-      # private
-
-      # @param value [String]
-      # @return [Vedeu::Views::Stream]
-      # def build_stream(value)
-      #   Vedeu::Views::Stream.build(client: client,
-      #                              parent: model,
-      #                              value: value)
-      # end
 
     end # Line
 
