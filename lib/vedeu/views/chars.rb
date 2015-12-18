a39f0a0d25716624f59acc50cc66a388c2e30428
Gavin Laking <gavinlaking@gmail.com>
2015-12-18 21:21:42 +0000

    Add custom .coerce method for Vedeu::Views::Chars.

--- lib/vedeu/views/chars.rb
+++ lib/vedeu/views/chars.rb
@@ -8,6 +8,47 @@ module Vedeu
     #
     class Chars < Vedeu::Repositories::Collection
 
+      # @param collection [void]
+      # @param parent [Vedeu::Views::Stream]
+      # @param name [NilClass|Symbol|String]
+      # @raise [Vedeu::Error::InvalidSyntax] When the collection
+      #   cannot be coerced since it is unrecognised or unsupported.
+      # @return [Vedeu::Views::Chars]
+      def self.coerce(collection = [], parent = nil, name = nil)
+        if collection.is_a?(Vedeu::Views::Chars)
+          collection
+
+        elsif collection.is_a?(Array)
+          return new(collection, parent, name) if collection.empty?
+
+          coerced_collection = []
+          collection.each do |element|
+            coerced_collection << element if element.is_a?(Vedeu::Views::Char)
+          end
+
+          new(coerced_collection, parent, name)
+
+        elsif collection.is_a?(Vedeu::Views::Stream)
+
+
+        elsif collection.is_a?(String)
+          return new(collection, parent, name) if collection.empty?
+
+          if parent && parent.attributes
+            new_collection = Vedeu::DSL::Text.new(collection,
+                                                  parent.attributes).chars
+
+            new(new_collection, parent, name)
+          end
+
+        else
+          fail Vedeu::Error::InvalidSyntax,
+               'Cannot coerce for Vedeu::View::Streams, as collection ' \
+               'unrecognised.'.freeze
+
+        end
+      end
+
     end # Chars
 
   end # Views
