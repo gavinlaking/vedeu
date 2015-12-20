8b559ae4767e0628777e2df17dede869b0b23f6e
Gavin Laking <gavinlaking@gmail.com>
2015-12-20 21:47:36 +0000

    Start adding tests for DSL::Attributes.

--- test/lib/vedeu/dsl/attributes_test.rb
+++ test/lib/vedeu/dsl/attributes_test.rb
@@ -5,6 +5,35 @@ module Vedeu
   module DSL
 
     describe Attributes do
+
+      let(:described) { Vedeu::DSL::Attributes }
+      let(:instance)  { described.new(context, model, value, options) }
+      let(:context)   {}
+      let(:model)     {}
+      let(:_value)    {}
+      let(:options)   {
+
+      }
+      # let(:block)     {}
+
+      describe '#initialize' do
+        it { instance.must_be_instance_of(described) }
+        it { instance.instance_variable_get('@context').must_equal(context) }
+        it { instance.instance_variable_get('@model').must_equal(model) }
+        it { instance.instance_variable_get('@value').must_equal(_value) }
+        it { instance.instance_variable_get('@options').must_equal(options) }
+      end
+
+      describe '.build' do
+        subject { described.build(context, model, _value, options) }
+
+        it { subject.must_be_instance_of(Hash) }
+      end
+
+      describe '#attributes' do
+        it { instance.must_respond_to(:attributes) }
+      end
+
     end # Attributes
 
   end # DSL
