ebd895e1751da85cf1304bc97b8c165a105dc911
Gavin Laking <gavinlaking@gmail.com>
2015-12-24 17:56:26 +0000

    Remove unused Output::Text.

--- lib/vedeu/output/all.rb
+++ lib/vedeu/output/all.rb
@@ -8,7 +8,6 @@ end # Vedeu
 
 require 'vedeu/output/compressor_cache'
 require 'vedeu/output/compressor'
-require 'vedeu/output/text'
 require 'vedeu/output/output'
 require 'vedeu/output/viewport'
 require 'vedeu/output/refresh'
