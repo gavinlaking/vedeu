3fcb8af42682be0892929fab57f3c7417f95399a
Gavin Laking <gavinlaking@gmail.com>
2016-01-03 16:00:38 +0000

    Update examples to use all renderers;
    Also, extract the interface definitions to a separate file to make maintenance
    easier. The ./runner.sh script has also been updated to be more informative and
    handle the new output formats (HTML, JSON and plain text).

--- examples/views/runner.sh
+++ examples/views/runner.sh
@@ -2,26 +2,72 @@
 
 set -e
 
-function test_output() {
-  "$@"
-  local status=$?
-  if [ $status -ne 0 ]; then
-    echo "Error with $1" >&2
-  fi
-  return $status
+function expected {
+  printf "\e[32mExpected:\e[39m "
+}
+function actual {
+  printf "\e[31mActual:\e[39m   "
 }
 
-test_output ./line.rb
-test_output diff -q /tmp/line.out ./expected/line.out
-du -b /tmp/line.out | cut -f1
-du -b ./expected/line.out | cut -f1
+./line.rb
+./lines.rb
+./lines_alignment.rb
+./full_screen.rb
 
-test_output ./lines.rb
-test_output diff -q /tmp/lines.out ./expected/lines.out
-du -b /tmp/lines.out | cut -f1
-du -b ./expected/lines.out | cut -f1
+echo -ne "\e[34mline out\e[39m\n"
+echo -ne "$(expected) $(du -b ./expected/vedeu_line.out | cut -f1)\n"
+echo -ne "$(actual) $(du -b /tmp/vedeu_line.out | cut -f1)\n"
+echo -ne "\e[33mhtml\e[39m\n"
+echo -ne "$(expected) $(du -b ./expected/vedeu_line.html | cut -f1)\n"
+echo -ne "$(actual) $(du -b /tmp/vedeu_line.html | cut -f1)\n"
+echo -ne "\e[33mjson\e[39m\n"
+echo -ne "$(expected) $(du -b ./expected/vedeu_line.json | cut -f1)\n"
+echo -ne "$(actual) $(du -b /tmp/vedeu_line.json | cut -f1)\n"
+echo -ne "\e[33mtxt\e[39m\n"
+echo -ne "$(expected) $(du -b ./expected/vedeu_line.txt | cut -f1)\n"
+echo -ne "$(actual) $(du -b /tmp/vedeu_line.txt | cut -f1)\n"
 
-test_output ./lines_alignment.rb
-test_output diff -q /tmp/lines_alignment.out ./expected/lines_alignment.out
-du -b /tmp/lines_alignment.out | cut -f1
-du -b ./expected/lines_alignment.out | cut -f1
+echo -ne "\n"
+
+echo -ne "\e[34mlines out\e[39m\n"
+echo -ne "$(expected) $(du -b ./expected/vedeu_lines.out | cut -f1)\n"
+echo -ne "$(actual) $(du -b /tmp/vedeu_lines.out | cut -f1)\n"
+echo -ne "\e[33mhtml\e[39m\n"
+echo -ne "$(expected) $(du -b ./expected/vedeu_lines.html | cut -f1)\n"
+echo -ne "$(actual) $(du -b /tmp/vedeu_lines.html | cut -f1)\n"
+echo -ne "\e[33mjson\e[39m\n"
+echo -ne "$(expected) $(du -b ./expected/vedeu_lines.json | cut -f1)\n"
+echo -ne "$(actual) $(du -b /tmp/vedeu_lines.json | cut -f1)\n"
+echo -ne "\e[33mtxt\e[39m\n"
+echo -ne "$(expected) $(du -b ./expected/vedeu_lines.txt | cut -f1)\n"
+echo -ne "$(actual) $(du -b /tmp/vedeu_lines.txt | cut -f1)\n"
+
+echo -ne "\n"
+
+echo -ne "\e[34mlines alignment out\e[39m\n"
+echo -ne "$(expected) $(du -b ./expected/vedeu_lines_alignment.out | cut -f1)\n"
+echo -ne "$(actual) $(du -b /tmp/vedeu_lines_alignment.out | cut -f1)\n"
+echo -ne "\e[33mhtml\e[39m\n"
+echo -ne "$(expected) $(du -b ./expected/vedeu_lines_alignment.html | cut -f1)\n"
+echo -ne "$(actual) $(du -b /tmp/vedeu_lines_alignment.html | cut -f1)\n"
+echo -ne "\e[33mjson\e[39m\n"
+echo -ne "$(expected) $(du -b ./expected/vedeu_lines_alignment.json | cut -f1)\n"
+echo -ne "$(actual) $(du -b /tmp/vedeu_lines_alignment.json | cut -f1)\n"
+echo -ne "\e[33mtxt\e[39m\n"
+echo -ne "$(expected) $(du -b ./expected/vedeu_lines_alignment.txt | cut -f1)\n"
+echo -ne "$(actual) $(du -b /tmp/vedeu_lines_alignment.txt | cut -f1)\n"
+
+echo -ne "\n"
+
+echo -ne "\e[34mfull screen out\e[39m\n"
+echo -ne "$(expected) $(du -b ./expected/vedeu_full_screen.out | cut -f1)\n"
+echo -ne "$(actual) $(du -b /tmp/vedeu_full_screen.out | cut -f1)\n"
+echo -ne "\e[33mhtml\e[39m\n"
+echo -ne "$(expected) $(du -b ./expected/vedeu_full_screen.html | cut -f1)\n"
+echo -ne "$(actual) $(du -b /tmp/vedeu_full_screen.html | cut -f1)\n"
+echo -ne "\e[33mjson\e[39m\n"
+echo -ne "$(expected) $(du -b ./expected/vedeu_full_screen.json | cut -f1)\n"
+echo -ne "$(actual) $(du -b /tmp/vedeu_full_screen.json | cut -f1)\n"
+echo -ne "\e[33mtxt\e[39m\n"
+echo -ne "$(expected) $(du -b ./expected/vedeu_full_screen.txt | cut -f1)\n"
+echo -ne "$(actual) $(du -b /tmp/vedeu_full_screen.txt | cut -f1)\n"
