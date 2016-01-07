ccf449055e42754abdf556fec8e2f041ed8daa8b
Gavin Laking <gavinlaking@gmail.com>
2016-01-07 19:23:45 +0000

    Add missing dsl_app_009.rb;
    Fixed the bugs which were preventing this displaying.
    Also add a very simple test runner to help with the testing of these integrations.

---
+++ integrations/test_runner.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# die on error
+set -e
+
+# change to directory that contains this script
+cd "$(dirname "$0")"
+
+./dsl_app_001.rb
+./dsl_app_002.rb
+./dsl_app_003.rb
+./dsl_app_004.rb
+./dsl_app_005.rb
+./dsl_app_006.rb
+./dsl_app_007.rb
+./dsl_app_008.rb
+./dsl_app_009.rb
+./dsl_app_010.rb
