c6f3aea0312ac4d468cc01a4420901c8303652a3
Gavin Laking <gavinlaking@gmail.com>
2016-01-07 20:39:10 +0000

    Add placeholder integration test files.

---
+++ integrations/dsl_app_020.rb
@@ -0,0 +1,56 @@
+#!/usr/bin/env ruby
+
+# frozen_string_literal: true
+
+require 'bundler/setup'
+require 'vedeu'
+
+class DSLApp
+
+  Vedeu.bind(:_initialize_) { Vedeu.trigger(:_refresh_) }
+
+  Vedeu.configure do
+    debug!
+    log '/tmp/vedeu_views_dsl.log'
+    renderers [
+                Vedeu::Renderers::Terminal.new(
+                  filename:   '/tmp/dsl_app_020.out',
+                  write_file: true),
+                # Vedeu::Renderers::JSON.new(filename: '/tmp/dsl_app_020.json'),
+                # Vedeu::Renderers::HTML.new(filename: '/tmp/dsl_app_020.html'),
+                # Vedeu::Renderers::Text.new(filename: '/tmp/dsl_app_020.txt'),
+              ]
+    run_once!
+    standalone!
+  end
+
+  load './support/test_interface.rb'
+
+  Vedeu.render do
+    view(:test_interface) do
+    end
+  end
+
+  def self.actual
+    File.read('/tmp/dsl_app_020.out')
+  end
+
+  def self.expected
+    File.read(File.expand_path('../expected/dsl_app_020.out', __FILE__))
+  end
+
+  def self.start(argv = ARGV)
+    Vedeu::Launcher.execute!(argv)
+  end
+
+end # DSLApp
+
+DSLApp.start
+
+if DSLApp.expected == DSLApp.actual
+  puts "#{__FILE__} \e[32mPassed.\e[39m"
+  exit 0;
+else
+  puts "#{__FILE__} \e[31mFailed.\e[39m"
+  exit 1;
+end
