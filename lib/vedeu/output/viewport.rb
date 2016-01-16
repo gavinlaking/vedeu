17bfe6138c62324dc974132858c79a0cd03f76b7
Gavin Laking <gavinlaking@gmail.com>
2016-01-16 00:42:19 +0000

    Delegate #bx and #by to #geometry.

--- lib/vedeu/output/viewport.rb
+++ lib/vedeu/output/viewport.rb
@@ -24,7 +24,9 @@ module Vedeu
 
       def_delegators :geometry,
                      :bordered_height,
-                     :bordered_width
+                     :bordered_width,
+                     :bx,
+                     :by
 
       def_delegators :cursor,
                      :ox,
@@ -66,16 +68,6 @@ module Vedeu
       attr_reader :view
 
       private
-
-      # @return [Fixnum]
-      def bx
-        @bx ||= geometry.bx
-      end
-
-      # @return [Fixnum]
-      def by
-        @by ||= geometry.by
-      end
 
       # Using the current cursor's x position, return a range of
       # visible columns.
