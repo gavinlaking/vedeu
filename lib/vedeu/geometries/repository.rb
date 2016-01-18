112fca74eb9c236abb5f2e1b25ab292e5f44c230
Gavin Laking <gavinlaking@gmail.com>
2016-01-18 00:19:00 +0000

    Move some event bindings.

--- lib/vedeu/geometries/repository.rb
+++ lib/vedeu/geometries/repository.rb
@@ -35,29 +35,9 @@ module Vedeu
     Vedeu.geometries.by_name(name).maximise
   end
 
-  # See {file:docs/events/view.md#\_movement_refresh_}
-  Vedeu.bind(:_movement_refresh_) do |name|
-    Vedeu.trigger(:_clear_)
-    Vedeu.trigger(:_refresh_)
-    Vedeu.trigger(:_clear_view_, name)
-    Vedeu.trigger(:_refresh_view_, name)
-  end
-
   # See {file:docs/events/view.md#\_unmaximise_}
   Vedeu.bind(:_unmaximise_) do |name|
     Vedeu.geometries.by_name(name).unmaximise
-  end
-
-  [:down, :left, :right, :up].each do |direction|
-    Vedeu.bind(:"_view_#{direction}_") do |name, offset|
-      Vedeu::Geometries::Move.move(direction: direction,
-                                   name:      name,
-                                   offset:    offset)
-    end
-  end
-
-  [:down, :left, :right, :up].each do |direction|
-    Vedeu.bind_alias(:"_geometry_#{direction}_", :"_view_#{direction}_")
   end
 
   # :nocov:
