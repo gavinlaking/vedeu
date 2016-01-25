e8a6294807b6112151ebc32d64ff0348d3d6c6c6
Gavin Laking <gavinlaking@gmail.com>
2016-01-25 22:34:11 +0000

    Move Models::Page.coerce to Coercers::Page.coerce.

--- lib/vedeu/coercers/page.rb
+++ lib/vedeu/coercers/page.rb
@@ -11,13 +11,29 @@ module Vedeu
     #
     class Page < Vedeu::Coercers::Coercer
 
-      # @return [void]
+      # @param value
+      #   [Vedeu::Models::Page|Vedeu::Models::Row|Array<void>|void]
+      # @macro raise_invalid_syntax
+      # @return [Vedeu::Models::Page]
       def coerce
         if coerced?
           value
 
-        else
+        elsif value.is_a?(Vedeu::Models::Row)
+          klass.new([value])
 
+        elsif value.is_a?(Array) && value.empty?
+          klass.new([Vedeu::Models::Row.coerce(value)])
+
+        elsif value.is_a?(Array) || value.is_a?(Vedeu::Buffers::View)
+          values = value.map { |v| Vedeu::Models::Row.coerce(v) }
+
+          klass.new(values)
+
+        else
+          fail Vedeu::Error::InvalidSyntax,
+               "Cannot coerce as value is not an Array, #{klass} " \
+               "or Vedeu::Models::Row. Is a '#{value.class.name}'."
 
         end
       end
--- test/lib/vedeu/coercers/page_test.rb
+++ test/lib/vedeu/coercers/page_test.rb
@@ -33,6 +33,116 @@ module Vedeu
         end
       end
 
+      describe '.coerce' do
+        let(:_value) {}
+
+        subject { described.coerce(_value) }
+
+        it { proc { subject }.must_raise(Vedeu::Error::InvalidSyntax) }
+
+        context 'when the value is a Vedeu::Models::Row' do
+          let(:_value)   { Vedeu::Models::Row.new }
+          let(:expected) { klass.new([_value]) }
+
+          it { subject.must_equal(expected) }
+        end
+
+        context 'when the value is an Array' do
+          context 'and the value is empty' do
+            let(:_value)   { [] }
+            let(:expected) {
+              klass.coerce(Vedeu::Models::Row.coerce(_value))
+            }
+
+            it { subject.must_be_instance_of(klass) }
+            it { subject.must_equal(expected) }
+          end
+
+          context 'and the value is not empty, the content is' do
+            context 'is an empty Array' do
+              let(:_value)   { [[]] }
+              let(:expected) { klass.coerce(Vedeu::Models::Row.coerce([])) }
+
+              it { subject.must_be_instance_of(klass) }
+              it { subject.must_equal(expected) }
+            end
+
+            context 'an array of Vedeu::Models::Row objects' do
+              let(:_value) {
+                [
+                  Vedeu::Models::Row.new([:hydrogen, :helium]),
+                  Vedeu::Models::Row.new([:lithium, :beryllium]),
+                ]
+              }
+              let(:expected) { klass.new(_value) }
+
+              it { subject.must_equal(expected) }
+            end
+
+            context 'a mix of Vedeu::Models::Row objects and other objects' do
+              let(:_value) {
+                [
+                  Vedeu::Models::Row.new([:hydrogen, :helium]),
+                  [:lithium],
+                  Vedeu::Models::Row.new([:beryllium, :boron]),
+                ]
+              }
+              let(:expected) {
+                klass.new([
+                  Vedeu::Models::Row.new([:hydrogen, :helium]),
+                  Vedeu::Models::Row.new([:lithium]),
+                  Vedeu::Models::Row.new([:beryllium, :boron]),
+                ])
+              }
+
+              it { subject.must_equal(expected) }
+            end
+
+            context 'a mix of Vedeu::Models::Row objects, other objects and ' \
+                    'nils' do
+              let(:_value) {
+                [
+                  Vedeu::Models::Row.new([:hydrogen, :helium]),
+                  nil,
+                  [:lithium],
+                  Vedeu::Models::Row.new([:beryllium]),
+                  [
+                    Vedeu::Cells::Char.new(value: 'b'),
+                    Vedeu::Cells::Char.new(value: 'o'),
+                    Vedeu::Cells::Char.new(value: 'r'),
+                    Vedeu::Cells::Char.new(value: 'o'),
+                    Vedeu::Cells::Char.new(value: 'n'),
+                  ],
+                  [nil, :carbon],
+                  :nitrogen,
+                ]
+              }
+              let(:expected) {
+                klass.new([
+                  Vedeu::Models::Row.new([:hydrogen, :helium]),
+                  Vedeu::Models::Row.new([]),
+                  Vedeu::Models::Row.new([:lithium]),
+                  Vedeu::Models::Row.new([:beryllium]),
+                  Vedeu::Models::Row.new([
+                    Vedeu::Cells::Char.new(value: 'b'),
+                    Vedeu::Cells::Char.new(value: 'o'),
+                    Vedeu::Cells::Char.new(value: 'r'),
+                    Vedeu::Cells::Char.new(value: 'o'),
+                    Vedeu::Cells::Char.new(value: 'n'),
+                  ]),
+                  Vedeu::Models::Row.new([:carbon]),
+                  Vedeu::Models::Row.new([:nitrogen]),
+                ])
+              }
+
+              it { subject.must_equal(expected) }
+            end
+
+          end
+
+        end
+      end
+
     end # Page
 
   end # Coercers
