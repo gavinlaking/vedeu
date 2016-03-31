ffcc495c7e6a0f33db746f4217b02b553a779e03
Gavin Laking <gavinlaking@gmail.com>
2016-03-31 21:46:15 +0100

    Implement signal traps in a more compatible way;
    Previously, traps were not necessary Windows OS compatible, this commit takes steps
    to improve that situation.
    
    Fixes #373.

--- lib/vedeu/runtime/traps.rb
+++ lib/vedeu/runtime/traps.rb
@@ -6,23 +6,76 @@ module Vedeu
 
     # Vedeu can respond to various signals which are handled here.
     #
+    # @api private
+    #
     module Traps
 
       # :nocov:
 
-      Signal.trap('INT') do
-        exit(1)
+      # @params keys [Array<Symbol>]
+      # @macro param_block
+      # @macro raise_requires_block
+      # @return [Array<Symbol>]
+      def self.add_signal_trap(*keys, &block)
+        raise Vedeu::Error::RequiresBlock unless block_given?
+
+        # fail Signal.list.inspect
+
+        keys.each do |key|
+          Signal.trap(key.to_s, &block) if Signal.list.key?(key.to_s)
+        end
       end
 
-      Signal.trap('TERM') do
-        exit(1)
-      end
+      # Resize terminal
+      add_signal_trap(:WINCH) { Vedeu.trigger(:_resize_) }
 
-      Signal.trap('TTIN') {}
-      Signal.trap('USR1') {}
-      Signal.trap('USR2') {}
+      # Stop runtime (failure)
+      add_signal_trap(:INT, :TERM) { exit(1) }
 
-      Signal.trap('SIGWINCH') { Vedeu.trigger(:_resize_) }
+      # Read from terminal
+      add_signal_trap(:TTIN) {}
+
+      # User
+      add_signal_trap(:USR1, :USR2) {}
+
+      # Signals from Ubuntu Linux 16.04.
+      #
+      # {
+      #   "EXIT"   => 0,
+      #   "HUP"    => 1,
+      #   "INT"    => 2,
+      #   "QUIT"   => 3,
+      #   "ILL"    => 4,
+      #   "TRAP"   => 5,
+      #   "ABRT"   => 6,
+      #   "IOT"    => 6,
+      #   "BUS"    => 7,
+      #   "FPE"    => 8,
+      #   "KILL"   => 9,
+      #   "USR1"   => 10,
+      #   "SEGV"   => 11,
+      #   "USR2"   => 12,
+      #   "PIPE"   => 13,
+      #   "ALRM"   => 14,
+      #   "TERM"   => 15,
+      #   "CHLD"   => 17,
+      #   "CLD"    => 17,
+      #   "STOP"   => 19,
+      #   "CONT"   => 18,
+      #   "TSTP"   => 20,
+      #   "TTIN"   => 21,
+      #   "TTOU"   => 22,
+      #   "URG"    => 23,
+      #   "XCPU"   => 24,
+      #   "XFSZ"   => 25,
+      #   "VTALRM" => 26,
+      #   "PROF"   => 27,
+      #   "WINCH"  => 28,
+      #   "IO"     => 29,
+      #   "POLL"   => 29,
+      #   "PWR"    => 30,
+      #   "SYS"    => 31,
+      # }
 
       # :nocov:
 
